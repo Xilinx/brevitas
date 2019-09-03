437f6000be41b67fc093129d24e6da2b0b53c350
Yaman Umuroglu <yamanu@xilinx.com>
2019-09-03 15:29:40 +0100

    [ONNX] get rid of quantization_annotation dict, use attributes
    it seems like we can't count on the uniqueName()s returned during
    symbolic export to stay the same in the ONNX graph, so opting for
    providing the quantization info as a node attribute for now.

--- brevitas/onnx/onnx_custom_ops.py
+++ brevitas/onnx/onnx_custom_ops.py
@@ -1,16 +1,11 @@
 import torch
 from torch.autograd import Function
-from brevitas.onnx import quantization_annotation
-
-# TODO find a better way to carry the quantization annotations --
-# consider putting them in as attributes, then using an ONNX pass to
-# move them into the graph.quantization_annotation
 
 class QuantizedLinearPlaceholderFunction(Function):
     @staticmethod
     def symbolic(g, W, x, bw, out_features):
-        quantization_annotation[W.uniqueName()] = str(bw)
-        return g.op('MatMul', W, x, domain_s = "finn")
+        ret = g.op('MatMul', W, x, domain_s = "finn", weight_qnt_s = str(bw))
+        return ret
 
     @staticmethod
     def forward(ctx, W, x, bw, out_features):
@@ -19,9 +14,8 @@ class QuantizedLinearPlaceholderFunction(Function):
 class QuantizedHardTanhPlaceholderFunction(Function):
     @staticmethod
     def symbolic(g, input):
-        ret = g.op('QuantizedHardTanh', input, domain_s = "finn")
-        # TODO fix bitwidth
-        quantization_annotation[ret.uniqueName()] = "1"
+        # TODO fix quantization info
+        ret = g.op('QuantizedHardTanh', input, domain_s = "finn", activation_qnt_s = "1")
         return ret
 
     @staticmethod
