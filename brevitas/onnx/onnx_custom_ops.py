121c0a177da1b2b5661a0be03cae0ae127f186aa
Yaman Umuroglu <yamanu@xilinx.com>
2019-09-03 17:32:07 +0100

    [ONNX] pass quantization type as argument to placeholder ops

--- brevitas/onnx/onnx_custom_ops.py
+++ brevitas/onnx/onnx_custom_ops.py
@@ -3,21 +3,20 @@ from torch.autograd import Function
 
 class QuantizedLinearPlaceholderFunction(Function):
     @staticmethod
-    def symbolic(g, W, x, bw, out_features):
-        ret = g.op('MatMul', W, x, domain_s = "finn", weight_qnt_s = str(bw))
+    def symbolic(g, W, x, qnt_type, out_features):
+        ret = g.op('MatMul', W, x, domain_s = "finn", weight_qnt_s = qnt_type)
         return ret
 
     @staticmethod
-    def forward(ctx, W, x, bw, out_features):
+    def forward(ctx, W, x, qnt_type, out_features):
         return torch.empty(1, out_features, dtype = torch.float)
 
 class QuantizedHardTanhPlaceholderFunction(Function):
     @staticmethod
-    def symbolic(g, input):
-        # TODO fix quantization info
-        ret = g.op('QuantizedHardTanh', input, domain_s = "finn", activation_qnt_s = "1")
+    def symbolic(g, input, qnt_type):
+        ret = g.op('QuantizedHardTanh', input, domain_s = "finn", activation_qnt_s = qnt_type)
         return ret
 
     @staticmethod
-    def forward(ctx, input):
+    def forward(ctx, input, qnt_type):
         return input.clamp(0)
