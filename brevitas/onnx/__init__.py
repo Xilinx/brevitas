e36c2056aca0157fa007701529a3aa4270d974ad
Yaman Umuroglu <yamanu@xilinx.com>
2019-09-04 15:49:45 +0100

    [ONNX] add export_finn_onnx function
    this does the necessary init/deinit as well as some cleanup on
    the exported ONNX model

--- brevitas/onnx/__init__.py
+++ brevitas/onnx/__init__.py
@@ -1,5 +1,10 @@
-def prepare_for_onnx_export(pytorch_module, enable_export = True):
-    """Traverse children of given module to prepare them for ONNX export.
+import torch
+import torch.onnx
+import onnx
+import onnx.optimizer as opt
+
+def _prepare_for_finn_onnx_export(module, enable_export = True):
+    """Traverse children of given module to prepare them for FINN-ONNX export.
 
     This sets the export_mode property of all child modules to enable_export
     where applicable (e.g. subclasses of QuantLayer), which changes their
@@ -8,9 +13,32 @@ def prepare_for_onnx_export(pytorch_module, enable_export = True):
     """
 
     try:
-        pytorch_module.export_mode = enable_export
+        module.export_mode = enable_export
     except AttributeError:
         # module does not have the prepare_for_export function, skip
         pass
-    for c in pytorch_module.children():
-        prepare_for_onnx_export(c, enable_export)
+    for c in module.children():
+        _prepare_for_finn_onnx_export(c, enable_export)
+
+def export_finn_onnx(module, input_shape, export_path):
+    """Export given module with Brevitas layers to FINN-ONNX with some cleanup."""
+
+    with torch.no_grad():
+        # TODO maybe consider a deepcopy of the module first?
+        module = module.eval()
+        _prepare_for_finn_onnx_export(module, enable_export = True)
+        torch.onnx.export(
+            module, torch.empty(input_shape, dtype=torch.float), export_path
+        )
+        # restore the model to non-export mode to keep it clean
+        _prepare_for_finn_onnx_export(module, enable_export = False)
+        # do some cleanup on the exported ONNX model
+        model = onnx.load(export_path)
+        onnx_passes = [
+            # use initializers instead of Constant nodes for fixed params
+            "extract_constant_to_initializer",
+            # remove unused graph inputs (e.g. zero_hw_sentinel) & initializers
+            "eliminate_unused_initializer"
+        ]
+        model = opt.optimize(model, onnx_passes)
+        onnx.save(model, export_path)
