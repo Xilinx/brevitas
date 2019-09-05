bee366ac27d672d98a90bf651fc88be71dbe8978
Yaman Umuroglu <yamanu@xilinx.com>
2019-09-05 23:00:33 +0100

    [ONNX] add a transformation to move qnt info into annotations

--- brevitas/onnx/__init__.py
+++ brevitas/onnx/__init__.py
@@ -2,6 +2,7 @@ import torch
 import torch.onnx
 import onnx
 import onnx.optimizer as opt
+import copy
 
 def _prepare_for_finn_onnx_export(module, enable_export = True):
     """Traverse children of given module to prepare them for FINN-ONNX export.
@@ -19,6 +20,37 @@ def _prepare_for_finn_onnx_export(module, enable_export = True):
         pass
     for c in module.children():
         _prepare_for_finn_onnx_export(c, enable_export)
+
+def _move_quant_attributes_into_annotations(model):
+    """Move quantization info in attributes into quantization_annotation"""
+
+    model = copy.deepcopy(model)
+    for n in model.graph.node:
+        for a in n.attribute:
+            mark_for_removal = False
+            if a.name == "weight_qnt":
+                # assume first input is weight, make sure it has an initializer
+                w_tensor_name = n.input[0]
+                assert w_tensor_name in [x.name for x in model.graph.initializer]
+                tq = onnx.StringStringEntryProto(key = a.name, value = a.s)
+                ta = onnx.TensorAnnotation(
+                    tensor_name = w_tensor_name,
+                    quant_parameter_tensor_names = [tq]
+                )
+                model.graph.quantization_annotation.append(ta)
+                mark_for_removal = True
+            elif a.name == "activation_qnt":
+                a_tensor_name = n.output[0]
+                tq = onnx.StringStringEntryProto(key = a.name, value = a.s)
+                ta = onnx.TensorAnnotation(
+                    tensor_name = a_tensor_name,
+                    quant_parameter_tensor_names = [tq]
+                )
+                model.graph.quantization_annotation.append(ta)
+                mark_for_removal = True
+            if mark_for_removal:
+                n.attribute.remove(a)
+    return model
 
 def export_finn_onnx(module, input_shape, export_path):
     """Export given module with Brevitas layers to FINN-ONNX with some cleanup."""
@@ -41,4 +73,5 @@ def export_finn_onnx(module, input_shape, export_path):
             "eliminate_unused_initializer"
         ]
         model = opt.optimize(model, onnx_passes)
+        model = _move_quant_attributes_into_annotations(model)
         onnx.save(model, export_path)
