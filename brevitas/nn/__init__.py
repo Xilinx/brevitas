8d18346b57ee9b43c722db2d71278b7107dfcec1
Nick Fraser <icanlosh@gmail.com>
2019-09-30 22:36:14 +0100

    Added QuantBatchNorm2d to the initialisation list in brevitas.nn.

--- brevitas/nn/__init__.py
+++ brevitas/nn/__init__.py
@@ -3,3 +3,4 @@ from .quant_activation import QuantReLU, QuantSigmoid, QuantTanh, QuantHardTanh
 from .quant_avg_pool import QuantAvgPool2d
 from .quant_linear import QuantLinear
 from .quant_conv import QuantConv2d, PaddingType
+from .quant_bn import QuantBatchNorm2d
