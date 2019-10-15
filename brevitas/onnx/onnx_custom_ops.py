ded535349106cb0bde72860464a4bca681511f8e
Yaman Umuroglu <yamanu@xilinx.com>
2019-10-15 16:31:57 +0100

    [ONNX] export QuantLinear scale factor as Mul if present

--- brevitas/onnx/onnx_custom_ops.py
+++ brevitas/onnx/onnx_custom_ops.py
@@ -3,12 +3,16 @@ from torch.autograd import Function
 
 class QuantizedLinearPlaceholderFunction(Function):
     @staticmethod
-    def symbolic(g, W, x, qnt_type, out_features):
+    def symbolic(g, W, x, scale_factor, qnt_type, out_features):
         ret = g.op('MatMul', W, x, domain_s = "finn", weight_qnt_s = qnt_type)
+        if scale_factor is not None:
+            # TODO add info about scaling factor constraints as attributes here
+            # (e.g. power of two, channel-wise or tensor-wise, ...)
+            ret = g.op('Mul', ret, scale_factor)
         return ret
 
     @staticmethod
-    def forward(ctx, W, x, qnt_type, out_features):
+    def forward(ctx, W, x, scale_factor, qnt_type, out_features):
         return torch.empty(1, out_features, dtype = torch.float)
 
 class QuantizedHardTanhPlaceholderFunction(Function):
