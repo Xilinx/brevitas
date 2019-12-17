f704370adfe6c04229ee21ca59fc1f258de563bd
Giuseppe Franco <giuseppefranco4@gmail.com>
2019-12-17 16:33:33 +0000

    BF: added Quant1DConv to init file

--- brevitas/nn/__init__.py
+++ brevitas/nn/__init__.py
@@ -6,3 +6,4 @@ from .quant_conv import QuantConv2d, PaddingType
 from .quant_bn import BatchNorm2dToQuantScaleBias
 from .quant_scale_bias import ScaleBias, QuantScaleBias
 from .hadamard_classifier import HadamardClassifier
+from .quant_conv1d import QuantConv1d
