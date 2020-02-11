ad0b46a2661199bf0f829b9af27b58540b2d1a94
Giuseppe5 <giuseppefranco4@gmail.com>
2020-02-11 16:44:49 +0000

    New parametrized tests

--- test/test_scaling.py
+++ test/test_scaling.py
@@ -3,47 +3,18 @@ from hypothesis import strategies as st
 from common import *
 from brevitas.core.scaling import *
 from brevitas.core import ZERO_HW_SENTINEL_VALUE
-import itertools
-
+import pytest
 
 from brevitas.core.stats import StatsOp
 
+# Constants
 OUTCHANNEL = 20
 SCALING_MIN_VAL = 0.0
 
-
-# Generate configurations for StandaloneScaling. It takes as input isscalar bool, which indicates whether the parameter
-# shape is a scalar or not. Isscalar doesn't refer to the dimension of scaling_init.
-def generator_standalonescaling(isscalar):
-    maxDim = [2, 3, 4]
-    restrict_scaling_type_comb = [RestrictValueType.FP, RestrictValueType.LOG_FP, RestrictValueType.POWER_OF_TWO]
-    is_parameter_bool = [True, False]
-    if isscalar:
-        combinations = [restrict_scaling_type_comb, is_parameter_bool]
-        for restrict_scaling_type, is_parameter in list(itertools.product(*combinations)):
-            yield (restrict_scaling_type, is_parameter)
-    else:
-        combinations = [maxDim, restrict_scaling_type_comb, is_parameter_bool]
-        for dim, restrict_scaling_type, is_parameter in list(itertools.product(*combinations)):
-            yield (dim, restrict_scaling_type, is_parameter)
-
-
-# Generate configurations for StatsScaling. It takes as input isscalar bool, which indicates whether the stats shape is
-# a scalar or not
-def generator_statsscaling(isscalar):
-    maxDim = [2, 3, 4]
-    restrict_scaling_type_comb = [RestrictValueType.FP, RestrictValueType.LOG_FP, RestrictValueType.POWER_OF_TWO]
-    if isscalar:
-        for restrict_scaling_type in restrict_scaling_type_comb:
-            yield restrict_scaling_type
-    else:
-        combinations = [maxDim, restrict_scaling_type_comb]
-        for dim, restrict_scaling_type in list(itertools.product(*combinations)):
-            for posix in range(0, dim):
-                stats_shape = [1] * dim
-                stats_shape[posix] = OUTCHANNEL
-                stats_shape = tuple(stats_shape)
-                yield (stats_shape, restrict_scaling_type)
+# Pytest Parametrize options
+restrict_scaling_type_options = [(RestrictValueType.FP), (RestrictValueType.LOG_FP), (RestrictValueType.POWER_OF_TWO)]
+is_parameter_bool_options = [(True), (False)]
+max_dim_options = [(2), (3), (4)]
 
 
 # Perform test for standalone scaling. It takes the scaling Tensor (which can be either a scalar or a real Tensor), and
@@ -99,31 +70,32 @@ def perform_test_statsscaling(scaling, shape, restrict_scaling_type):
 
 # Test the case where both scaling_init and the parameter shape are scalar
 @given(scaling_init=float_st_p)
-def test_standlonescaling_scalar_scalar(scaling_init):
+@pytest.mark.parametrize('restrict_scaling_type', restrict_scaling_type_options)
+@pytest.mark.parametrize('is_parameter', is_parameter_bool_options)
+def test_standlonescaling_scalar_scalar(scaling_init, restrict_scaling_type, is_parameter):
     scaling_init = torch.tensor(scaling_init)
     parameter_shape = ()
-    isscalar = True
-    for restrict_scaling_type, is_parameter in generator_standalonescaling(isscalar):
-        assert perform_test_standalonescaling(scaling_init, parameter_shape, restrict_scaling_type, is_parameter)
+    assert perform_test_standalonescaling(scaling_init, parameter_shape, restrict_scaling_type, is_parameter)
 
 
 # Test the case where scaling_init is scalar and parameter_shape is a Tensor.
 # In this case, the parameter shape has 2,3, or 4 dimensions. All the dimensions are set to 1, with the exception of one
 # dimension. All possible combinations are tested.
 @given(scaling_init=float_st_p)
-def test_standlonescaling_scalar_tensor(scaling_init):
+@pytest.mark.parametrize('restrict_scaling_type', restrict_scaling_type_options)
+@pytest.mark.parametrize('is_parameter', is_parameter_bool_options)
+@pytest.mark.parametrize('dim', max_dim_options)
+def test_standlonescaling_scalar_tensor(scaling_init, restrict_scaling_type, is_parameter, dim):
     scaling_init = torch.tensor(scaling_init)
-    isscalar = False
-    for dim, restrict_scaling_type, is_parameter in generator_standalonescaling(isscalar):
-        for posix in range(0, dim):
-            parameter_shape = [1] * dim
-            parameter_shape[posix] = OUTCHANNEL
-            parameter_shape = tuple(parameter_shape)
+    for posix in range(0, dim):
+        parameter_shape = [1] * dim
+        parameter_shape[posix] = OUTCHANNEL
+        parameter_shape = tuple(parameter_shape)
 
-            # Skip case not supported by StandaloneScaling which would throw error
-            if len(parameter_shape) > 1 and not is_parameter:
-                continue
-            assert perform_test_standalonescaling(scaling_init, parameter_shape, restrict_scaling_type, is_parameter)
+        # Skip case not supported by StandaloneScaling which would throw error
+        if len(parameter_shape) > 1 and not is_parameter:
+            continue
+        assert perform_test_standalonescaling(scaling_init, parameter_shape, restrict_scaling_type, is_parameter)
 
 
 # Test the case where both scaling_init and the parameter shape are tensor
@@ -133,36 +105,41 @@ def test_standlonescaling_scalar_tensor(scaling_init):
 # To allow this, we pass the 'data' strategy to the function, and then we draw elements interactively inside the
 # function.
 @given(scaling_init=st.lists(float_st_p, min_size=OUTCHANNEL, max_size=OUTCHANNEL))
-def test_standlonescaling_tensor_tensor(scaling_init):
-    isscalar = False
-    for dim, restrict_scaling_type, is_parameter in generator_standalonescaling(isscalar):
-        for posix in range(0, dim):
-            parameter_shape = [1] * dim
-            parameter_shape[posix] = OUTCHANNEL
-            parameter_shape = tuple(parameter_shape)
-            scaling_init_tensor = torch.ones(OUTCHANNEL)
+@pytest.mark.parametrize('restrict_scaling_type', restrict_scaling_type_options)
+@pytest.mark.parametrize('is_parameter', is_parameter_bool_options)
+@pytest.mark.parametrize('dim', max_dim_options)
+def test_standlonescaling_tensor_tensor(scaling_init, restrict_scaling_type, is_parameter, dim):
+    for posix in range(0, dim):
+        parameter_shape = [1] * dim
+        parameter_shape[posix] = OUTCHANNEL
+        parameter_shape = tuple(parameter_shape)
 
-            # Skip case not supported by StandaloneScaling which would throw error
-            if len(parameter_shape) > 1 and not is_parameter:
-                continue
-            scaling_init_tensor = torch.tensor(scaling_init).view(parameter_shape)
-            assert perform_test_standalonescaling(scaling_init_tensor, parameter_shape, restrict_scaling_type, is_parameter)
+        # Skip case not supported by StandaloneScaling which would throw error
+        if len(parameter_shape) > 1 and not is_parameter:
+            continue
+        scaling_init_tensor = torch.tensor(scaling_init).view(parameter_shape)
+        assert perform_test_standalonescaling(scaling_init_tensor, parameter_shape, restrict_scaling_type, is_parameter)
 
 
 # Test the case where scaling_init is a scalar
 @given(scaling_init=float_st_p)
-def test_statsscaling_scalar(scaling_init):
+@pytest.mark.parametrize('restrict_scaling_type', restrict_scaling_type_options)
+def test_statsscaling_scalar(scaling_init, restrict_scaling_type):
     scaling_init = torch.tensor(scaling_init)
-    for restrict_scaling_type in generator_statsscaling(True):
-        assert perform_test_statsscaling(scaling_init, SCALING_SCALAR_SHAPE, restrict_scaling_type)
+    assert perform_test_statsscaling(scaling_init, SCALING_SCALAR_SHAPE, restrict_scaling_type)
 
 
 # Test the case where scaling_init is a Tensor
 # In this case, the stats shape has 2,3, or 4 dimensions. All the dimensions are set to 1, with the exception of one
 # dimension. All possible combinations are tested.
 @given(scaling_init=st.lists(float_st_p, min_size=OUTCHANNEL, max_size=OUTCHANNEL))
-def test_statsscaling_tensor(scaling_init):
-    for shape, restrict_scaling_type in generator_statsscaling(False):
+@pytest.mark.parametrize('restrict_scaling_type', restrict_scaling_type_options)
+@pytest.mark.parametrize('dim', max_dim_options)
+def test_statsscaling_tensor(scaling_init, restrict_scaling_type, dim):
+    for posix in range(0, dim):
+        shape = [1] * dim
+        shape[posix] = OUTCHANNEL
+        shape = tuple(shape)
         scaling_init_tensor = torch.tensor(scaling_init).view(shape)
         assert perform_test_statsscaling(scaling_init_tensor, shape, restrict_scaling_type)
 
