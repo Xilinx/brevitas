90e2655318ba9329a2cab427208e8f655fb1ab3e
Alessandro Pappalardo <alessand@xilinx.com>
2020-03-23 16:38:50 +0000

    CI/CD: add a toggle in Nox test runner for Pytorch JIT
    Signed-off-by: Alessandro Pappalardo <alessand@xilinx.com>

--- noxfile.py
+++ noxfile.py
@@ -12,15 +12,21 @@ REQUIREMENTS_PATH = os.path.join('.', 'requirements')
 
 TESTS_YML = yaml.load(open(os.path.join(GITHUB_WORKFLOWS_PATH, 'tests.yml')), Loader=yaml.FullLoader)
 TESTS_MATRIX = TESTS_YML['jobs']['build']['strategy']['matrix']
+
 CONDA_PYTHON_VERSIONS = tuple(TESTS_MATRIX['conda_python_version'])
 PYTORCH_VERSIONS = tuple(TESTS_MATRIX['pytorch_version'])
+JIT_ENABLED = tuple(TESTS_MATRIX['jit_enabled'])
+
 CONDA_PYTHON_IDS = tuple([f'conda_python_{i}' for i in CONDA_PYTHON_VERSIONS])
 PYTORCH_IDS = tuple([f'pytorch_{i}' for i in PYTORCH_VERSIONS])
+JIT_IDS = tuple([f'jit_{i}'.lower() for i in JIT_ENABLED])
 
 
 @nox.session(venv_backend="conda", python=CONDA_PYTHON_VERSIONS)
 @nox.parametrize("pytorch", PYTORCH_VERSIONS, ids=PYTORCH_IDS)
-def tests_cpu(session, pytorch):
+@nox.parametrize("jit_enabled", JIT_ENABLED, ids=JIT_IDS)
+def tests_cpu(session, pytorch, jit_enabled):
+    session.env['PYTORCH_JIT'] = '{}'.format(int(jit_enabled))
     if version.parse(pytorch) >= version.parse(PYTORCH_CPU_VIRTUAL_PKG):
         session.conda_install('-c', 'pytorch', f'pytorch=={pytorch}', 'cpuonly')
     else:
