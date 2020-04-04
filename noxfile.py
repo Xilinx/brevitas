1089d433e28c06f6433f569123f839addef51129
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2020-04-04 17:25:55 +0100

    CI/CD: Generate Github Actions yml from a base template

--- noxfile.py
+++ noxfile.py
@@ -1,31 +1,23 @@
 import os
 
-import yaml
 import nox
 from packaging import version
 from platform import system
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), os.path.join('.', '.github', 'workflows')))
+from gen_github_actions import *
 
 
 PYTORCH_CPU_VIRTUAL_PKG = '1.2.0'
-
-GITHUB_WORKFLOWS_PATH = os.path.join('.', '.github', 'workflows')
-REQUIREMENTS_PATH = os.path.join('.', 'requirements')
-
-TESTS_YML = yaml.load(open(os.path.join(GITHUB_WORKFLOWS_PATH, 'tests.yml')), Loader=yaml.FullLoader)
-TESTS_MATRIX = TESTS_YML['jobs']['build']['strategy']['matrix']
-
-CONDA_PYTHON_VERSIONS = tuple(TESTS_MATRIX['conda_python_version'])
-PYTORCH_VERSIONS = tuple(TESTS_MATRIX['pytorch_version'])
-JIT_STATUS = tuple(TESTS_MATRIX['jit_status'])
-
 CONDA_PYTHON_IDS = tuple([f'conda_python_{i}' for i in CONDA_PYTHON_VERSIONS])
 PYTORCH_IDS = tuple([f'pytorch_{i}' for i in PYTORCH_VERSIONS])
-JIT_IDS = tuple([f'{i}'.lower() for i in JIT_STATUS])
+JIT_IDS = tuple([f'{i}'.lower() for i in JIT_STATUSES])
 
 
 @nox.session(venv_backend="conda", python=CONDA_PYTHON_VERSIONS)
 @nox.parametrize("pytorch", PYTORCH_VERSIONS, ids=PYTORCH_IDS)
-@nox.parametrize("jit_status", JIT_STATUS, ids=JIT_IDS)
+@nox.parametrize("jit_status", JIT_STATUSES, ids=JIT_IDS)
 def tests_cpu(session, pytorch, jit_status):
     session.env['PYTORCH_JIT'] = '{}'.format(int(jit_status == 'jit_enabled'))
     if version.parse(pytorch) >= version.parse(PYTORCH_CPU_VIRTUAL_PKG):
@@ -47,19 +39,23 @@ def tests_install_develop(session, pytorch):
     if system() == 'Windows':
         env = session.env
         nox.command.run(['python', '-c', 'import brevitas'], env=env, path=os.path.dirname(session.bin))
-        nox.command.run(['python', '-c', 'import brevitas.function.ops_ste'], env=env, path=os.path.dirname(session.bin))
+        nox.command.run(['python', '-c', 'import brevitas.function.ops_ste'], env=env,
+                        path=os.path.dirname(session.bin))
     else:
         session.run('python', '-c', 'import brevitas')
         session.run('python', '-c', 'import brevitas.function.ops_ste')
+
 
 @nox.session(python=False)
 @nox.parametrize("pytorch", PYTORCH_VERSIONS, ids=PYTORCH_IDS)
 @nox.parametrize("python", CONDA_PYTHON_VERSIONS, ids=CONDA_PYTHON_IDS)
 def dry_run_pytorch_only_deps(session, pytorch, python):
     if version.parse(pytorch) >= version.parse(PYTORCH_CPU_VIRTUAL_PKG):
-        session.run('conda', 'create', '-n', 'dry_run', '--only-deps', '-d', '-c', 'pytorch', f'pytorch=={pytorch}', 'cpuonly', f'python={python}')
+        session.run('conda', 'create', '-n', 'dry_run', '--only-deps', '-d', '-c', 'pytorch', f'pytorch=={pytorch}',
+                    'cpuonly', f'python={python}')
     else:
-        session.run('conda', 'create', '-n', 'dry_run', '--only-deps', '-d', '-c', 'pytorch', f'pytorch-cpu=={pytorch}', f'python={python}')
+        session.run('conda', 'create', '-n', 'dry_run', '--only-deps', '-d', '-c', 'pytorch', f'pytorch-cpu=={pytorch}',
+                    f'python={python}')
 
 
 @nox.session(python=False)
@@ -67,6 +63,8 @@ def dry_run_pytorch_only_deps(session, pytorch, python):
 @nox.parametrize("python", CONDA_PYTHON_VERSIONS, ids=CONDA_PYTHON_IDS)
 def dry_run_pytorch_no_deps(session, pytorch, python):
     if version.parse(pytorch) >= version.parse(PYTORCH_CPU_VIRTUAL_PKG):
-        session.run('conda', 'create', '-n', 'dry_run', '--no-deps', '-d', '-c', 'pytorch', f'pytorch=={pytorch}', 'cpuonly', f'python={python}')
+        session.run('conda', 'create', '-n', 'dry_run', '--no-deps', '-d', '-c', 'pytorch', f'pytorch=={pytorch}',
+                    'cpuonly', f'python={python}')
     else:
-        session.run('conda', 'create', '-n', 'dry_run', '--no-deps', '-d', '-c', 'pytorch', f'pytorch-cpu=={pytorch}', f'python={python}')
+        session.run('conda', 'create', '-n', 'dry_run', '--no-deps', '-d', '-c', 'pytorch', f'pytorch-cpu=={pytorch}',
+                    f'python={python}')
