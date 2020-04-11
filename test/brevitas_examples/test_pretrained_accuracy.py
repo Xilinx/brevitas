924c7216c215eb73562d6fbc27b04019d3b41c81
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2020-04-11 21:20:14 +0100

    Tests: Add accuracy tests for pretrained BNN-PYNQ's FC topologies on MNIST

---
+++ test/brevitas_examples/test_pretrained_accuracy.py
@@ -0,0 +1,26 @@
+import logging
+from urllib import request
+
+import pytest
+from brevitas_examples.bnn_pynq.bnn_pynq_train import main
+from brevitas_examples.bnn_pynq.models import get_model_cfg
+
+
+@pytest.mark.parametrize("model", ["TFC", "SFC", "LFC"])
+@pytest.mark.parametrize("weight_bit_width", [1, 2])
+@pytest.mark.parametrize("act_bit_width", [1, 2])
+def test_bnn_pynq_fc_pretrained_accuracy(caplog, model, weight_bit_width, act_bit_width):
+    if model == "LFC" and weight_bit_width == 2 and act_bit_width == 2:
+        pytest.skip("No pretrained LFC_W2A2 present.")
+    if weight_bit_width > act_bit_width:
+        pytest.skip("No weight_bit_width > act_bit_width cases.")
+
+    caplog.set_level(logging.INFO)
+    network = f"{model}_{weight_bit_width}W{act_bit_width}A"
+    cfg = get_model_cfg(network)
+    eval_log_url = cfg.get('MODEL', 'EVAL_LOG')
+    main(['--pretrained', '--network', network, '--evaluate', '--gpus', 'None'])
+    with request.urlopen(eval_log_url) as r:
+        log_list = [l[l.index('Prec@1'):] for l in caplog.text.splitlines()]
+        reference_prec_list = [l[l.index('Prec@1'):] for l in r.read().decode('utf-8').splitlines()]
+        assert log_list == reference_prec_list
