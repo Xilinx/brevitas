a4df0b0e4bf78091141ca333d9719529b007ed5e
Yaman Umuroglu <yamanu@xilinx.com>
2020-05-12 23:42:49 +0000

    [ONNX] handle MultiThreshold scale/bias separately for BIPOLAR
    it makes the FINN streamlining flow much easier to keep the scale
    and bias for bipolar qnt. in the attributes
    we should really consider exporting those always as attributes,
    only separating out as mul/add nodes when desired

--- brevitas/onnx/onnx_custom_ops.py
+++ brevitas/onnx/onnx_custom_ops.py
@@ -42,13 +42,17 @@ class QuantizedConv2dPlaceholderFunction(Function):
 class QuantizedHardTanhPlaceholderFunction(Function):
     @staticmethod
     def symbolic(g, input, qnt_type, thres, bias, scale):
-        ret = g.op('MultiThreshold', input, thres, domain_s = "finn",
-                    out_dtype_s = qnt_type)
-        if bias is not None:
-            ret = g.op('Add', ret, bias)
-        if scale is not None:
-            ret = g.op('Mul', ret, scale)
-        return ret
+        if qnt_type == "BIPOLAR":
+          return g.op('MultiThreshold', input, thres, domain_s = "finn",
+                      out_dtype_s = qnt_type, out_scale_f = 2.0, out_bias_f = -1.0)
+        else:
+          ret = g.op('MultiThreshold', input, thres, domain_s = "finn",
+                      out_dtype_s = qnt_type)
+          if bias is not None:
+              ret = g.op('Add', ret, bias)
+          if scale is not None:
+              ret = g.op('Mul', ret, scale)
+          return ret
 
     @staticmethod
     def forward(ctx, input, qnt_type, thres, bias, scale):
