4512e3142b1144ed671373c47f1d466df0655d6b
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2020-05-24 23:37:14 +0100

    Examples (BNN-PYNQ): Fix relative import

--- brevitas_examples/bnn_pynq/bnn_pynq_train.py
+++ brevitas_examples/bnn_pynq/bnn_pynq_train.py
@@ -25,7 +25,7 @@ import os
 import sys
 
 import torch
-from .trainer import Trainer
+from brevitas_examples.bnn_pynq.trainer import Trainer
 
 # Pytorch precision
 torch.set_printoptions(precision=10)
