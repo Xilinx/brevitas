dae946ad17c487a75d57f5e25f504bd67b019376
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2020-08-10 11:42:59 +0100

    Tests (pyxir/dpuv1): change test model to conv + linear layer
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- test/pyxir/dpuv1/test_export.py
+++ test/pyxir/dpuv1/test_export.py
@@ -9,8 +9,8 @@ from brevitas.quant_tensor import QuantTensor
 KERNEL_SIZE = 3
 IN_CHANNELS = 10
 OUT_CHANNELS = 20
-IN_SIZE = (1, IN_CHANNELS, 50, 50)
-
+IN_SIZE = (1, IN_CHANNELS, 10, 10)
+FC_IN_SIZE = 1280
 
 class DPUv1WeightQuantInjector(Injector):
     quant_type = 'INT'
@@ -44,33 +44,32 @@ class DPUv1OutputQuantInjector(Injector):
     narrow_range = False
 
 
+class Model(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv = QuantConv2d(
+            kernel_size=KERNEL_SIZE,
+            in_channels=IN_CHANNELS,
+            out_channels=OUT_CHANNELS,
+            weight_quant=DPUv1WeightQuantInjector,
+            bias_quant=DPUv1BiasQuantInjector,
+            output_quant=DPUv1OutputQuantInjector,
+            return_quant_tensor=False)
+        self.linear = nn.Linear(FC_IN_SIZE, OUT_CHANNELS)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(int(x.size(0)), -1)
+        x = self.linear(x)
+        return x
+
+
 def test_simple_export():
     x = QuantTensor(
         torch.randn(IN_SIZE),
         scale=torch.tensor(2.0 ** (-7)),
         bit_width=torch.tensor(8),
         signed=True)
-    conv1 = QuantConv2d(
-        kernel_size=KERNEL_SIZE,
-        in_channels=IN_CHANNELS,
-        out_channels=OUT_CHANNELS,
-        weight_quant=DPUv1WeightQuantInjector,
-        bias_quant=DPUv1BiasQuantInjector,
-        output_quant=DPUv1OutputQuantInjector,
-        return_quant_tensor=True)
-    act1 = QuantReLU(
-        act_quant=DPUv1OutputQuantInjector,
-        return_quant_tensor=True)
-    conv2 = QuantConv2d(
-        kernel_size=KERNEL_SIZE,
-        in_channels=OUT_CHANNELS,
-        out_channels=OUT_CHANNELS,
-        weight_quant=DPUv1WeightQuantInjector,
-        bias_quant=DPUv1BiasQuantInjector,
-        output_quant=DPUv1OutputQuantInjector,
-        return_quant_tensor=True)
-    act2 = QuantReLU(
-        act_quant=DPUv1OutputQuantInjector,
-        return_quant_tensor=True)
-    mod = nn.Sequential(*[conv1, act1, conv2, act2])
-    export_dpuv1_onnx(mod, input_shape=IN_SIZE, input_t=x, export_path='dpuv1_2layers.onnx')
+    mod = Model()
+    export_dpuv1_onnx(mod, input_shape=IN_SIZE, input_t=x, export_path='dpuv1_conv_linear.onnx')
