a28b542d5fd35602072293299d6c256f5419d376
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2020-08-11 17:50:07 +0100

    ONNX (pyxir/dpuv1): Handle disabled bias in Conv layers
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- brevitas/onnx/pyxir/function.py
+++ brevitas/onnx/pyxir/function.py
@@ -23,21 +23,38 @@ class QuantizedConv2dPlaceholderFunction(Function):
             stride,
             groups,
             dilation):
-        ret = g.op(
-            'Conv', x,
-            int_weight,
-            int_bias,
-            domain_s="pyxir",
-            vai_quant_s=['vai_quant_in', 'vai_quant_out', 'vai_quant_weights', 'vai_quant_biases'],
-            vai_quant_in_i=[input_bit_width, input_scale],
-            vai_quant_out_i=[output_bit_width, output_scale],
-            vai_quant_weights_i=[weight_bit_width, weight_scale],
-            vai_quant_biases_i=[bias_bit_width, bias_scale],
-            kernel_shape_i=kernel_size,
-            pads_i=padding,
-            strides_i=stride,
-            group_i=groups,
-            dilations_i=dilation)
+        vai_quant_s = ['vai_quant_in', 'vai_quant_out', 'vai_quant_weights']
+        if int_bias is not None:
+            vai_quant_s += ['vai_quant_biases']
+            ret = g.op(
+                'Conv', x,
+                int_weight,
+                int_bias,
+                domain_s="pyxir",
+                vai_quant_s=vai_quant_s,
+                vai_quant_in_i=[input_bit_width, input_scale],
+                vai_quant_out_i=[output_bit_width, output_scale],
+                vai_quant_weights_i=[weight_bit_width, weight_scale],
+                vai_quant_biases_i=[bias_bit_width, bias_scale],
+                kernel_shape_i=kernel_size,
+                pads_i=padding,
+                strides_i=stride,
+                group_i=groups,
+                dilations_i=dilation)
+        else:
+            ret = g.op(
+                'Conv', x,
+                int_weight,
+                domain_s="pyxir",
+                vai_quant_s=vai_quant_s,
+                vai_quant_in_i=[input_bit_width, input_scale],
+                vai_quant_out_i=[output_bit_width, output_scale],
+                vai_quant_weights_i=[weight_bit_width, weight_scale],
+                kernel_shape_i=kernel_size,
+                pads_i=padding,
+                strides_i=stride,
+                group_i=groups,
+                dilations_i=dilation)
         return ret
 
     @staticmethod
