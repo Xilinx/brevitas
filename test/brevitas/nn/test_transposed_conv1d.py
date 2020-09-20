719661fc1d9d19b33620f8b027b22c4c026e25b7
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2020-09-20 18:08:00 +0100

    Layers: add QuantConvTranspose2d layer, reorganize conv files

--- test/brevitas/nn/test_transposed_conv1d.py
+++ test/brevitas/nn/test_transposed_conv1d.py
@@ -1,5 +1,5 @@
 import torch
-import brevitas.nn.quant_convtranspose1d as quant_convtranspose1d
+from brevitas.nn import QuantConvTranspose1d
 from generate_quant_input import generate_quant_input
 from brevitas.core.quant import QuantType
 import random
@@ -29,36 +29,14 @@ random.seed(42)
 np.random.seed(42)
 torch.manual_seed(42)
 
+
 class Test1DTranspConv:
 
     @check_expected_pyt_120_fail
     def test_float_quant(self):
         shape = (BATCH, IN_CHANNEL, HEIGHT)
         input_quant_int, input_quant = generate_quant_input(shape, BIT, SCALE, True, True)
-        ConvTranspose1d = quant_convtranspose1d.QuantConvTranspose1d(in_channels=IN_CHANNEL,
-                                          out_channels=OUT_CHANNEL,
-                                          kernel_size=KERNEL,
-                                          stride=STRIDE,
-                                          weight_quant_type=QuantType.INT,
-                                          weight_bit_width=BIT,
-                                          bias=False)
-
-        results_float_quantized = ConvTranspose1d(input_quant)
-        weight_int = ConvTranspose1d.int_weight()
-        bias = ConvTranspose1d.bias
-        output_padding = ConvTranspose1d.compute_output_padding(input_quant_int, None)
-        results_int_quantized = ConvTranspose1d.conv_transpose1d_zeros_pad(
-            input_quant_int, weight_int.float(), bias, output_padding)
-        totalScale = SCALE * ConvTranspose1d.quant_weight_scale()
-        result_rescaled = results_int_quantized * totalScale
-        # print(torch.norm(results_float_quantized- result_rescaled))
-        assert (torch.allclose(results_float_quantized, result_rescaled, atol= ATOL, rtol= RTOL))
-
-    @check_expected_pyt_120_fail
-    def test_int(self):
-        shape = (BATCH, IN_CHANNEL, HEIGHT)
-        input_quant_int, input_quant = generate_quant_input(shape, BIT, SCALE, True, True)
-        ConvTranspose1d = quant_convtranspose1d.QuantConvTranspose1d(
+        mod = QuantConvTranspose1d(
             in_channels=IN_CHANNEL,
             out_channels=OUT_CHANNEL,
             kernel_size=KERNEL,
@@ -66,13 +44,37 @@ class Test1DTranspConv:
             weight_quant_type=QuantType.INT,
             weight_bit_width=BIT,
             bias=False)
-        results_float_quantized = ConvTranspose1d(input_quant)
-        weight_int = ConvTranspose1d.int_weight()
-        bias = ConvTranspose1d.bias
-        output_padding = ConvTranspose1d.compute_output_padding(input_quant_int, None)
-        results_int_quantized = ConvTranspose1d.conv_transpose1d_zeros_pad(
+
+        results_float_quantized = mod(input_quant)
+        weight_int = mod.int_weight()
+        bias = mod.bias
+        output_padding = mod.compute_output_padding(input_quant_int, None)
+        results_int_quantized = mod.conv_transpose1d_zeros_pad(
             input_quant_int, weight_int.float(), bias, output_padding)
-        totalScale = SCALE * ConvTranspose1d.quant_weight_scale()
-        result_rescaled = torch.round(results_float_quantized / totalScale)
+        total_scale = SCALE * mod.quant_weight_scale()
+        result_rescaled = results_int_quantized * total_scale
+        assert (torch.allclose(results_float_quantized, result_rescaled, atol=ATOL, rtol=RTOL))
+
+    @check_expected_pyt_120_fail
+    def test_int(self):
+        shape = (BATCH, IN_CHANNEL, HEIGHT)
+        input_quant_int, input_quant = generate_quant_input(shape, BIT, SCALE, True, True)
+        mod = QuantConvTranspose1d(
+            in_channels=IN_CHANNEL,
+            out_channels=OUT_CHANNEL,
+            kernel_size=KERNEL,
+            stride=STRIDE,
+            weight_quant_type=QuantType.INT,
+            weight_bit_width=BIT,
+            bias=False)
+
+        results_float_quantized = mod(input_quant)
+        weight_int = mod.int_weight()
+        bias = mod.bias
+        output_padding = mod.compute_output_padding(input_quant_int, None)
+        results_int_quantized = mod.conv_transpose1d_zeros_pad(
+            input_quant_int, weight_int.float(), bias, output_padding)
+        total_scale = SCALE * mod.quant_weight_scale()
+        result_rescaled = torch.round(results_float_quantized / total_scale)
         assert (torch.allclose(results_int_quantized, result_rescaled, atol=ATOL, rtol=RTOL))
 
