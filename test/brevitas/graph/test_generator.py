cbb876eaf9651e53e8d81bb0162b8928ddb484e2
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2020-09-29 20:53:47 +0100

    Tests: run graph tests under no_grad

--- test/brevitas/graph/test_generator.py
+++ test/brevitas/graph/test_generator.py
@@ -9,7 +9,7 @@ from brevitas_examples.bnn_pynq import lfc_1w1a
 
 SEED = 123456
 IMAGENET_SIZE = (1, 3, 224, 224)
-INCEPTION_IMAGENET_SIZE = (2, 3, 299, 299)
+INCEPTION_SIZE = (2, 3, 299, 299)
 LARGER_IMAGE_SIZE = (2, 3, 340, 340)
 MNIST_SIZE = (1, 1, 28, 28)
 
@@ -35,28 +35,28 @@ def test_generator_torchvision(model_name: str, train: bool, pretrained: bool):
     model = getattr(models, model_name)(pretrained=pretrained)
     model = model.train(train)
     torch.manual_seed(SEED)
-    input = torch.randn(INCEPTION_IMAGENET_SIZE if model_name == 'inception_v3' else IMAGENET_SIZE)
+    input = torch.randn(INCEPTION_SIZE if model_name == 'inception_v3' else IMAGENET_SIZE)
     torch.manual_seed(SEED)
-    trace = Tracer(input).trace_model(model)
-    identity_model = ModuleGenerator().gen_model(trace)
-    identity_model.load_state_dict(model.state_dict())
-
-    model.train(train)
-    identity_model.train(train)
-
-    # Define a new input tensor to check we are not just memorizing stuff
-    torch.manual_seed(SEED + 1)
-    inp2 = torch.randn(INCEPTION_IMAGENET_SIZE if model_name == 'inception_v3' else IMAGENET_SIZE)
-    torch.manual_seed(SEED)
-    out = model(inp2)
-    torch.manual_seed(SEED)
-    q_out = identity_model(inp2)
-    if isinstance(q_out, (tuple, list)):
-        assert isinstance(out, (tuple, list))
-        for reference, o in zip(out, q_out):
-            assert o.isclose(reference).all().item()
-    else:
-        assert q_out.isclose(out).all().item()
+    with torch.no_grad():
+        trace = Tracer(input).trace_model(model)
+        identity_model = ModuleGenerator().gen_model(trace)
+        identity_model.load_state_dict(model.state_dict())
+        del trace  # not needed anymore
+        model.train(train)
+        identity_model.train(train)
+        # Define a new input tensor to check we are not just memorizing stuff
+        torch.manual_seed(SEED + 1)
+        inp2 = torch.randn(INCEPTION_SIZE if model_name == 'inception_v3' else IMAGENET_SIZE)
+        torch.manual_seed(SEED)
+        out = model(inp2)
+        torch.manual_seed(SEED)
+        q_out = identity_model(inp2)
+        if isinstance(q_out, (tuple, list)):
+            assert isinstance(out, (tuple, list))
+            for reference, o in zip(out, q_out):
+                assert o.isclose(reference).all().item()
+        else:
+            assert q_out.isclose(out).all().item()
 
 
 # TODO fix tracing and generating a graph containing ScriptModules
@@ -64,20 +64,16 @@ def test_generator_torchvision(model_name: str, train: bool, pretrained: bool):
 def test_generator_bnn_pynq(train=False):
     model = lfc_1w1a(pretrained=False)
     model = model.train(train)
-
     torch.manual_seed(SEED)
     input = torch.randn(MNIST_SIZE)
-
-    model(input)
-    trace = Tracer(input).trace_model(model, wrap_torchscript=True)
-    quant_model = ModuleGenerator().gen_model(trace)
-    quant_model.load_state_dict(model.state_dict())
-    quant_model.train(train)
-    for inst in quant_model.schedule:
-        print(inst)
-    torch.manual_seed(SEED)
-    out = model(input)
-    torch.manual_seed(SEED)
-    q_out = quant_model(input)
-
-    assert out.isclose(q_out).all().item()
+    with torch.no_grad():
+        model(input)
+        trace = Tracer(input).trace_model(model, wrap_torchscript=True)
+        quant_model = ModuleGenerator().gen_model(trace)
+        quant_model.load_state_dict(model.state_dict())
+        quant_model.train(train)
+        torch.manual_seed(SEED)
+        out = model(input)
+        torch.manual_seed(SEED)
+        q_out = quant_model(input)
+        assert out.isclose(q_out).all().item()
--- test/brevitas/graph/test_rewriter.py
+++ test/brevitas/graph/test_rewriter.py
@@ -26,23 +26,22 @@ MODELS = [
 def test_rewriter_merge_bn(model_name: str, pretrained: bool):
     model = getattr(models, model_name)(pretrained=pretrained)
     model = model.train(False)
-
     torch.manual_seed(SEED)
     input = torch.randn(INPUT_SIZE)
-    trace = Tracer(input).trace_model(model)
-
-    gen_model = ModuleGenerator().gen_model(trace)
-    gen_model.load_state_dict(model.state_dict())
-    gen_model.train(False)
-    torch.manual_seed(SEED)
-    out_gen_model = gen_model(input)
-    gen_model_bn_fused = MergeBatchNorm2d().apply(gen_model)
-    torch.manual_seed(SEED)
-    out_gen_model_bn_fused = gen_model_bn_fused(input)
-    is_close = out_gen_model_bn_fused.isclose(out_gen_model, atol=ATOL).all().item()
-    for m in gen_model_bn_fused.modules():
-        assert not isinstance(m, nn.BatchNorm2d)
-    assert is_close
+    with torch.no_grad():
+        trace = Tracer(input).trace_model(model)
+        gen_model = ModuleGenerator().gen_model(trace)
+        gen_model.load_state_dict(model.state_dict())
+        gen_model.train(False)
+        torch.manual_seed(SEED)
+        out_gen_model = gen_model(input)
+        gen_model_bn_fused = MergeBatchNorm2d().apply(gen_model)
+        torch.manual_seed(SEED)
+        out_gen_model_bn_fused = gen_model_bn_fused(input)
+        is_close = out_gen_model_bn_fused.isclose(out_gen_model, atol=ATOL).all().item()
+        for m in gen_model_bn_fused.modules():
+            assert not isinstance(m, nn.BatchNorm2d)
+        assert is_close
 
 
 @pytest.mark.parametrize("train", [True, False])
@@ -50,18 +49,17 @@ def test_rewriter_merge_bn(model_name: str, pretrained: bool):
 def test_rewriter_duplicate_shared_relu(model_name: str, train: bool):
     model = getattr(models, model_name)(pretrained=False)
     model = model.train(train)
-
     torch.manual_seed(SEED)
     input = torch.randn(INPUT_SIZE)
-    trace = Tracer(input).trace_model(model)
-
-    gen_model = ModuleGenerator().gen_model(trace)
-    gen_model.load_state_dict(model.state_dict())
-    gen_model.train(train)
-    torch.manual_seed(SEED)
-    out_gen_model = gen_model(input)
-    gen_model_duplicate_relu = DuplicateSharedStatelessModule().apply(gen_model)
-    torch.manual_seed(SEED)
-    out_gen_model_duplicate_relu = gen_model_duplicate_relu(input)
-    is_close = out_gen_model_duplicate_relu.isclose(out_gen_model, atol=ATOL).all().item()
-    assert is_close
+    with torch.no_grad():
+        trace = Tracer(input).trace_model(model)
+        gen_model = ModuleGenerator().gen_model(trace)
+        gen_model.load_state_dict(model.state_dict())
+        gen_model.train(train)
+        torch.manual_seed(SEED)
+        out_gen_model = gen_model(input)
+        gen_model_duplicate_relu = DuplicateSharedStatelessModule().apply(gen_model)
+        torch.manual_seed(SEED)
+        out_gen_model_duplicate_relu = gen_model_duplicate_relu(input)
+        is_close = out_gen_model_duplicate_relu.isclose(out_gen_model, atol=ATOL).all().item()
+        assert is_close
