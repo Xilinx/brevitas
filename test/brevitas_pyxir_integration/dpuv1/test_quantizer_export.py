481273f0dc7cf8fd7cab4d0849c706a0bbd6ec85
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2020-10-07 18:05:47 +0100

    Test: fix import
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- test/brevitas_pyxir_integration/dpuv1/test_quantizer_export.py
+++ test/brevitas_pyxir_integration/dpuv1/test_quantizer_export.py
@@ -2,7 +2,7 @@ import pytest
 import torch
 from torchvision import models
 
-from brevitas.onnx import export_dpuv1_onnx
+from brevitas.onnx import export_dpuv2_onnx
 from brevitas.graph.quantizer import quantize, BatchNormHandling
 from brevitas import config
 config.IGNORE_MISSING_KEYS = True
@@ -23,11 +23,11 @@ def test_rewriter_export(model_name: str):
     input = torch.randn(IN_SIZE)
     gen_model = quantize(
         model, input,
-        power_of_two_scaling=True,
-        per_output_channel=False,
+        restrict_scaling_type='POWER_OF_TWO',
+        scaling_per_output_channel=False,
         weight_bit_width=8,
         act_bit_width=8,
         bias_bit_width=8,
         bn_handling=BatchNormHandling.MERGE_AND_QUANTIZE)
     out_file = f'{model_name}.onnx'
-    export_dpuv1_onnx(gen_model, input_shape=IN_SIZE, input_t=input, export_path=out_file)
+    export_dpuv2_onnx(gen_model, input_shape=IN_SIZE, input_t=input, export_path=out_file)
