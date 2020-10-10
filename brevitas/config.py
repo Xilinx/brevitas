0968e66fb1bc438fe02cb8428b14429752b4e44a
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2020-10-10 20:50:31 +0100

    Config: fix import jit status on latest pytorch
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- brevitas/config.py
+++ brevitas/config.py
@@ -1,6 +1,9 @@
 import os
 from distutils.util import strtobool
-from torch.jit import _enabled
+try:
+    from torch.jit import _enabled
+except ImportError:
+    from torch.jit._state import _enabled
 
 def env_to_bool(name, default):
     return bool(strtobool(os.environ.get(name, "{}".format(default))))
