842a2241885eec243d8977fae5d66bb8ba5553b6
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2020-11-11 11:21:05 +0000

    ONNX (standard): remove constant_to_initializer transform
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- src/brevitas/export/onnx/standard/manager.py
+++ src/brevitas/export/onnx/standard/manager.py
@@ -34,8 +34,6 @@ class StdONNXManager(ONNXBaseManager):
         StdONNXQuantMaxPool2d]
 
     onnx_passes = [
-        # use initializers instead of Constant nodes for fixed params
-        "extract_constant_to_initializer",
         # remove unused graph inputs & initializers
         "eliminate_unused_initializer"]
 
