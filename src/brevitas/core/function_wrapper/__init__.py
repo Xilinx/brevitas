32ff2bcfa20a6f13070adab493527f0591198d98
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-01-07 17:19:06 +0000

    Core (function_wrapper): add InplaceNoOp module
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- src/brevitas/core/function_wrapper/__init__.py
+++ src/brevitas/core/function_wrapper/__init__.py
@@ -1,5 +1,5 @@
 from .clamp import ClampMin, TensorClamp, ScalarClamp
-from .misc import LogTwo, InplaceLogTwo, PowerOfTwo, Identity
+from .misc import LogTwo, InplaceLogTwo, PowerOfTwo, Identity, InplaceNoOp
 from .ops_ste import CeilSte, ScalarClampMinSte, FloorSte, RoundSte, RoundToZeroSte, TensorClampSte
 from .shape import OverBatchOverOutputChannelView, OverBatchOverTensorView, OverOutputChannelView
 from .shape import OverTensorView, StatsInputViewShapeImpl
