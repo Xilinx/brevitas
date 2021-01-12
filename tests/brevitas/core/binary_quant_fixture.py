86b9b96dbbf803298eb195ee21de99a8f1199789
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-01-12 11:47:29 +0000

    Tests (core/quant/binary): add binary quant fixtures
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

---
+++ tests/brevitas/core/binary_quant_fixture.py
@@ -0,0 +1,58 @@
+import pytest_cases
+from pytest_cases import fixture_union
+from brevitas.core.quant import BinaryQuant, ClampedBinaryQuant
+
+__all__ = [
+    'binary_quant',
+    'clamped_binary_quant',
+    'delayed_binary_quant',
+    'delayed_clamped_binary_quant',
+    'binary_quant_impl_all',
+    'binary_quant_all', # noqa
+    'delayed_binary_quant_all', # noqa
+]
+
+
+@pytest_cases.fixture()
+@pytest_cases.parametrize('impl', [BinaryQuant, ClampedBinaryQuant])
+def binary_quant_impl_all(impl):
+    """
+    Classes implementing variants of binary quantization
+    """
+    return impl
+
+
+@pytest_cases.fixture()
+def binary_quant(scaling_impl_all):
+    """
+    Binary quant with all variants of scaling
+    """
+    return BinaryQuant(scaling_impl=scaling_impl_all)
+
+
+@pytest_cases.fixture()
+def clamped_binary_quant(scaling_impl_all):
+    """
+    ClampedBinaryQuant with all variants of scaling
+    """
+    return ClampedBinaryQuant(scaling_impl=scaling_impl_all)
+
+
+@pytest_cases.fixture()
+def delayed_binary_quant(scaling_impl_all, quant_delay_steps):
+    """
+    Delayed BinaryQuant with all variants of scaling
+    """
+    return BinaryQuant(scaling_impl=scaling_impl_all, quant_delay_steps=quant_delay_steps)
+
+
+@pytest_cases.fixture()
+def delayed_clamped_binary_quant(scaling_impl_all, quant_delay_steps):
+    """
+    ClampedBinaryQuant with all variants of scaling
+    """
+    return ClampedBinaryQuant(scaling_impl=scaling_impl_all, quant_delay_steps=quant_delay_steps)
+
+
+fixture_union('binary_quant_all', ['binary_quant', 'clamped_binary_quant'])
+fixture_union('delayed_binary_quant_all', ['delayed_binary_quant', 'delayed_clamped_binary_quant'])
