7f09db214a4f54efc593f2b2031d9d5d111e3532
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-01-21 18:32:39 +0000

    Export (pytorch): add support for QuantMaxPool1d/2d
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

---
+++ src/brevitas/export/pytorch/handler/pool.py
@@ -0,0 +1,56 @@
+from abc import ABC
+from typing import Union
+
+import torch.nn.quantized.functional as qF
+
+from brevitas.nn import QuantMaxPool1d, QuantMaxPool2d
+from .base import PytorchQuantLayerHandler
+
+
+class PytorchQuantMaxPoolNd(PytorchQuantLayerHandler, ABC):
+
+    @classmethod
+    def validate(cls, module):
+        # nothing to do here, pytorch's quant max pool is standard max pool
+        pass
+
+    @classmethod
+    def explicit_output_dtype(cls) -> bool:
+        return False
+
+    @classmethod
+    def prepare_qf_kwargs(cls, module: Union[QuantMaxPool1d, QuantMaxPool2d]):
+        return {
+            'kernel_size': module.kernel_size,
+            'stride': module.stride,
+            'padding': module.padding,
+            'dilation': module.dilation,
+            'ceil_mode': module.ceil_mode,
+            'return_indices': module.return_indices}
+
+    def prepare_for_export(self, module):
+        self.qf_impl, self.qf_kwargs = self.prepare_qf(module)
+        self.return_quant_tensor = module.return_quant_tensor
+
+    def forward(self, inp):
+        out = self.qf_impl(inp, **self.qf_kwargs)
+        # We are being tolerant here to non quantized tensors
+        if out.is_quantized and not self.return_quant_tensor:
+            out = out.dequantize()
+        return out
+
+
+class PytorchQuantMaxPool1d(PytorchQuantMaxPoolNd):
+    handled_layer = QuantMaxPool1d
+
+    @classmethod
+    def prepare_qf(cls, module: QuantMaxPool1d):
+        return qF.max_pool1d, cls.prepare_qf_kwargs(module)
+
+
+class PytorchQuantMaxPool2d(PytorchQuantMaxPoolNd):
+    handled_layer = QuantMaxPool2d
+
+    @classmethod
+    def prepare_qf(cls, module: QuantMaxPool1d):
+        return qF.max_pool2d, cls.prepare_qf_kwargs(module)
--- tests/brevitas/export/test_pytorch_qf_export.py
+++ tests/brevitas/export/test_pytorch_qf_export.py
@@ -1,7 +1,7 @@
 import torch
 import pytest
 
-from brevitas.nn import QuantConv2d, QuantLinear, QuantIdentity, QuantReLU
+from brevitas.nn import QuantConv2d, QuantLinear, QuantIdentity, QuantReLU, QuantMaxPool2d
 from brevitas.inject.defaults import ShiftedUint8ActPerTensorFloat, Int8WeightPerTensorFloat
 from brevitas.inject.defaults import Uint8ActPerTensorFloat
 from brevitas.export.pytorch.manager import PytorchQuantManager
@@ -26,7 +26,8 @@ def test_pytorch_quant_linear_export():
                 bias=False,
                 weight_quant=Int8WeightPerTensorFloat,
                 input_quant=ShiftedUint8ActPerTensorFloat,
-                output_quant=ShiftedUint8ActPerTensorFloat)
+                output_quant=ShiftedUint8ActPerTensorFloat,
+                return_quant_tensor=False)
             self.linear.weight.data.uniform_(-0.01, 0.01)
 
         def forward(self, x):
@@ -51,7 +52,8 @@ def test_quant_act_export():
         def __init__(self):
             super().__init__()
             self.act1 = QuantIdentity(
-                act_quant=ShiftedUint8ActPerTensorFloat, return_quant_tensor=True)
+                act_quant=ShiftedUint8ActPerTensorFloat,
+                return_quant_tensor=True)
             self.act2 = QuantReLU(act_quant=Uint8ActPerTensorFloat)
 
         def forward(self, x):
@@ -65,4 +67,33 @@ def test_quant_act_export():
     pytorch_qf_model = PytorchQuantManager.export(model, inp)
     pytorch_out = pytorch_qf_model(inp)
     atol = model.act2.quant_output_scale().item() * TOLERANCE
+    assert pytorch_out.isclose(brevitas_out, rtol=0.0, atol=atol).all()
+
+
+def test_quant_max_pool2d_export():
+    IN_SIZE = (1, 1, IN_CH, IN_CH)
+    KERNEL_SIZE = 3
+
+    class Model(torch.nn.Module):
+
+        def __init__(self):
+            super().__init__()
+            self.act = QuantIdentity(
+                act_quant=ShiftedUint8ActPerTensorFloat,
+                return_quant_tensor=True)
+            self.pool = QuantMaxPool2d(
+                kernel_size=KERNEL_SIZE, stride=KERNEL_SIZE,
+                return_quant_tensor=False)
+
+        def forward(self, x):
+            return self.pool(self.act(x))
+
+    inp = torch.randn(IN_SIZE)
+    model = Model()
+    model(inp)  # collect scale factors
+    model.eval()
+    brevitas_out = model(inp)
+    pytorch_qf_model = PytorchQuantManager.export(model, inp)
+    pytorch_out = pytorch_qf_model(inp)
+    atol = model.act.quant_output_scale().item() * TOLERANCE
     assert pytorch_out.isclose(brevitas_out, rtol=0.0, atol=atol).all()
