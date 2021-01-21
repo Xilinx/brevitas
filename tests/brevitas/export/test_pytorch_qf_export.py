3c6d4ceed8fd1b2de6f95d4b4c6e89a177c5bdc6
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-01-21 14:57:49 +0000

    Tests (export/pytorch): update tests with fail mark where needed
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- tests/brevitas/export/test_pytorch_qf_export.py
+++ tests/brevitas/export/test_pytorch_qf_export.py
@@ -1,47 +1,18 @@
 import torch
+import pytest
 
-from brevitas.nn import QuantConv2d, QuantLinear
-from brevitas.inject.defaults import Uint8ActPerTensorFloat, Int8WeightPerTensorFloat
+from brevitas.nn import QuantConv2d, QuantLinear, QuantIdentity, QuantReLU
+from brevitas.inject.defaults import ShiftedUint8ActPerTensorFloat, Int8WeightPerTensorFloat
+from brevitas.inject.defaults import Uint8ActPerTensorFloat
 from brevitas.export.pytorch.manager import PytorchQuantManager
 
-OUT_CH = 3
-IN_CH = 2
+OUT_CH = 50
+IN_CH = 40
+TOLERANCE = 1.1
 
 
-def test_pytorch_quant_conv_export():
-    IN_SIZE = (1, IN_CH, 7, 7)
-    KERNEL_SIZE = 3
-
-    class Model(torch.nn.Module):
-
-        def __init__(self):
-            super().__init__()
-            self.conv1 = QuantConv2d(
-                out_channels=OUT_CH,
-                in_channels=IN_CH,
-                kernel_size=KERNEL_SIZE,
-                bias=False,
-                weight_quant=Int8WeightPerTensorFloat,
-                input_quant=Uint8ActPerTensorFloat,
-                output_quant=Uint8ActPerTensorFloat,
-                return_quant_tensor=True)
-            self.conv1.weight.data.uniform_(0.0, 0.01)
-
-        def forward(self, x):
-            return self.conv1(x)
-
-    model = Model().eval()
-    inp = torch.randn(IN_SIZE)
-    brevitas_out = model(inp)
-    pytorch_qf_model = PytorchQuantManager.export(model)
-    pytorch_out = pytorch_qf_model(inp)
-    pytorch_out = pytorch_out.dequantize().cpu()
-    atol = brevitas_out.scale.item() * 2.1  # slightly larger than 2 scale factor
-    brevitas_out = brevitas_out.value
-    print(brevitas_out - pytorch_out)
-    assert pytorch_out.isclose(brevitas_out, rtol=0.0, atol=atol).all()
-
-
+# see https://github.com/pytorch/pytorch/projects/17#card-53197198
+@pytest.mark.xfail(strict=True)
 def test_pytorch_quant_linear_export():
     IN_SIZE = (IN_CH, IN_CH)
 
@@ -54,19 +25,44 @@ def test_pytorch_quant_linear_export():
                 out_features=OUT_CH,
                 bias=False,
                 weight_quant=Int8WeightPerTensorFloat,
-                input_quant=Uint8ActPerTensorFloat,
-                output_quant=Uint8ActPerTensorFloat,
-                return_quant_tensor=True)
-            self.linear.weight.data.uniform_(0.0, 0.01)
+                input_quant=ShiftedUint8ActPerTensorFloat,
+                output_quant=ShiftedUint8ActPerTensorFloat)
+            self.linear.weight.data.uniform_(-0.01, 0.01)
 
         def forward(self, x):
             return self.linear(x)
 
-    model = Model().eval()
     inp = torch.randn(IN_SIZE)
+    model = Model()
+    model(inp)  # collect scale factors
+    model.eval()
     brevitas_out = model(inp)
-    pytorch_qf_model = PytorchQuantManager.export(model)
+    pytorch_qf_model = PytorchQuantManager.export(model, inp)
     pytorch_out = pytorch_qf_model(inp)
-    atol = brevitas_out.scale.item() * 1.1  # slightly larger than 1 scale factor
-    brevitas_out = brevitas_out.value
-    assert pytorch_out.dequantize().cpu().isclose(brevitas_out, rtol=0.0, atol=atol).all()
+    atol = model.linear.quant_output_scale().item() * TOLERANCE
+    assert pytorch_out.isclose(brevitas_out, rtol=0.0, atol=atol).all()
+
+
+def test_quant_act_export():
+    IN_SIZE = (IN_CH, IN_CH)
+
+    class Model(torch.nn.Module):
+
+        def __init__(self):
+            super().__init__()
+            self.act1 = QuantIdentity(
+                act_quant=ShiftedUint8ActPerTensorFloat, return_quant_tensor=True)
+            self.act2 = QuantReLU(act_quant=Uint8ActPerTensorFloat)
+
+        def forward(self, x):
+            return self.act2(self.act1(x))
+
+    inp = torch.randn(IN_SIZE)
+    model = Model()
+    model(inp)  # collect scale factors
+    model.eval()
+    brevitas_out = model(inp)
+    pytorch_qf_model = PytorchQuantManager.export(model, inp)
+    pytorch_out = pytorch_qf_model(inp)
+    atol = model.act2.quant_output_scale().item() * TOLERANCE
+    assert pytorch_out.isclose(brevitas_out, rtol=0.0, atol=atol).all()
