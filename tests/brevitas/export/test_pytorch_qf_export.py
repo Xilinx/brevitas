892b0823dfb7e045c9ee35e4780a5fc75442766f
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-01-22 13:58:55 +0000

    Tests (export/pytorch): add test for QuantConv2d export
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- tests/brevitas/export/test_pytorch_qf_export.py
+++ tests/brevitas/export/test_pytorch_qf_export.py
@@ -11,6 +11,39 @@ IN_CH = 40
 TOLERANCE = 1.1
 
 
+def test_pytorch_quant_conv_export():
+    IN_SIZE = (1, 1, IN_CH, IN_CH)
+    KERNEL_SIZE = (3, 3)
+
+    class Model(torch.nn.Module):
+
+        def __init__(self):
+            super().__init__()
+            self.conv = QuantConv2d(
+                out_channels=OUT_CH,
+                in_channels=IN_CH,
+                kernel_size=KERNEL_SIZE,
+                bias=False,
+                weight_quant=Int8WeightPerTensorFloat,
+                input_quant=ShiftedUint8ActPerTensorFloat,
+                output_quant=ShiftedUint8ActPerTensorFloat,
+                return_quant_tensor=False)
+            self.linear.weight.data.uniform_(-0.01, 0.01)
+
+        def forward(self, x):
+            return self.linear(x)
+
+    inp = torch.randn(IN_SIZE)
+    model = Model()
+    model(inp)  # collect scale factors
+    model.eval()
+    brevitas_out = model(inp)
+    pytorch_qf_model = PytorchQuantManager.export(model, inp)
+    pytorch_out = pytorch_qf_model(inp)
+    atol = model.linear.quant_output_scale().item() * TOLERANCE
+    assert pytorch_out.isclose(brevitas_out, rtol=0.0, atol=atol).all()
+
+
 def test_pytorch_quant_linear_export():
     IN_SIZE = (IN_CH, IN_CH)
 
