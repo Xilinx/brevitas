d308831a80c41199e0576de35f0b032873dbce19
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-01-25 11:39:42 +0000

    Tests: add basic tests for weight bit-width losses
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

---
+++ tests/brevitas/loss/test_weighted_bit_width.py
@@ -0,0 +1,34 @@
+import torch
+from brevitas.inject.defaults import Int8ActPerTensorFloat
+from brevitas.loss import WeightBitWidthWeightedBySize, ActivationBitWidthWeightedBySize
+from brevitas.loss import QuantLayerOutputBitWidthWeightedByOps, MEGA
+from brevitas.nn import QuantIdentity, QuantLinear
+
+
+def test_weight_bit_width_weighted_by_size():
+    model = QuantLinear(
+        out_features=6, in_features=5, bias=False,
+        weight_bit_width_impl_type='parameter', weight_bit_width=4)
+    loss = WeightBitWidthWeightedBySize(model)
+    out = model(torch.randn(2, 5, 5))
+    assert loss.tot_num_elements == 30
+    assert loss.retrieve() == 4.0
+
+
+def test_act_bit_width_weighted_by_size():
+    model = QuantIdentity(bit_width_impl_type='parameter', bit_width=3)
+    loss = ActivationBitWidthWeightedBySize(model)
+    out = model(torch.randn(2, 5, 5))
+    assert loss.tot_num_elements == 25
+    assert loss.retrieve() == 3.0
+
+
+def test_output_bit_weighted_by_ops():
+    model = QuantLinear(
+        out_features=6, in_features=5, bias=False,
+        input_quant=Int8ActPerTensorFloat, weight_bit_width_impl_type='parameter',
+        return_quant_tensor=True)
+    loss = QuantLayerOutputBitWidthWeightedByOps(model)
+    out = model(torch.randn(2, 4, 5))
+    assert loss.tot_num_elements == 24 * 10 / MEGA
+    assert loss.retrieve() == out.bit_width
