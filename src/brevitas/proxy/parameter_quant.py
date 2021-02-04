Bugfix (Proxy): fix sign flag in max_uint_value
Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

src/brevitas/proxy/parameter_quant.py
@@ -106,7 +106,7 @@ class ParameterQuantProxyFromInjector(QuantProxyFromInjector, ParameterQuantProx
             self.tensor_quant = self.quant_injector.tensor_quant
 
     def max_uint_value(self, bit_width):
-        return max_int(True, self.is_narrow_range, bit_width)
+        return max_int(False, self.is_narrow_range, bit_width)
 
     def add_tracked_parameter(self, parameter: torch.nn.Parameter) -> None:
         if self.tracked_parameter_list is None: