f6dabdb9b9a5a7b07256c38cfd30bea5abc14b2d
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-02-19 02:04:38 +0000

    Fix (solver): remove restrict_bit_width_impl solver from bias solver
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- src/brevitas/quant/solver/bias.py
+++ src/brevitas/quant/solver/bias.py
@@ -106,7 +106,6 @@ class BiasQuantSolver(
         SolveBitWidthImplFromEnum,
         SolveTensorQuantFloatToIntImplFromEnum,
         SolveRestrictScalingImplFromEnum,
-        SolveRestrictBitWidthImplFromEnum,
         SolveIntScalingImplFromEnum,
         SolveParameterScalingImplFromEnum,
         SolveParameterTensorClampImplFromEnum,
