12827688a8bd42aba68ae265dca1e29aad4ca369
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-02-27 15:21:22 +0000

    CI: switch nox backend from conda to virtualenv
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- noxfile.py
+++ noxfile.py
@@ -6,59 +6,67 @@ from platform import system
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), os.path.join('.', '.github', 'workflows')))
-from gen_github_actions import *
+from gen_github_actions import PYTORCH_VERSIONS, PYTHON_VERSIONS, JIT_STATUSES
 
-PYTORCH_CPU_VIRTUAL_PKG = '1.2.0'
+IS_OSX = system() == 'Darwin'
+PYTORCH_STABLE_WHEEL_SRC = 'https://download.pytorch.org/whl/torch_stable.html'
+PYTORCH_110_CPU_WHEEL_SRC = 'https://download.pytorch.org/whl/cpu/torch_stable.html'
 PYTORCH_PILLOW_FIXED = '1.4.0'
-CONDA_PYTHON_IDS = tuple([f'conda_python_{i}' for i in CONDA_PYTHON_VERSIONS])
+OLDER_PILLOW_VERSION = '6.2.0'
 PYTORCH_IDS = tuple([f'pytorch_{i}' for i in PYTORCH_VERSIONS])
 JIT_IDS = tuple([f'{i}'.lower() for i in JIT_STATUSES])
+
+TORCHVISION_VERSION_DICT = {
+    '1.1.0': '0.3.0',
+    '1.2.0': '0.4.0',
+    '1.3.0': '0.4.1',
+    '1.3.1': '0.4.2',
+    '1.4.0': '0.5.0',
+    '1.5.0': '0.6.0',
+    '1.5.1': '0.6.1',
+    '1.6.0': '0.7.0',
+    '1.7.0': '0.8.0',
+    '1.7.1': '0.8.1'
+}
+
+PARSED_TORCHVISION_VERSION_DICT = {
+    version.parse(k): v for k, v in TORCHVISION_VERSION_DICT.items()}
 
 
 # This combination has proven to be problematic and without access to
 # a physical system it's tricky to debug
 def is_torchvision_broken(python_version, pytorch_version):
-    return (system() == 'Darwin' and
+    return (IS_OSX and
             version.parse(python_version) == version.parse('3.6') and
             version.parse(pytorch_version) == version.parse('1.5'))
 
 
-def install_pytorch(pytorch, session, numpy=None):
-    is_cpu_virtual = version.parse(pytorch) >= version.parse(PYTORCH_CPU_VIRTUAL_PKG)
-    if is_cpu_virtual:
-        cmd = ['-c', 'pytorch', f'pytorch=={pytorch}', 'cpuonly']
+def install_pytorch(pytorch, session):
+    if not IS_OSX and version.parse(pytorch) > version.parse('1.1.0'):
+        cmd = [f'torch=={pytorch}+cpu', '-f', PYTORCH_STABLE_WHEEL_SRC]
+    elif not IS_OSX and version.parse(pytorch) <= version.parse('1.1.0'):
+        cmd = [f'torch=={pytorch}', '-f', PYTORCH_110_CPU_WHEEL_SRC]
     else:
-        cmd = ['-c', 'pytorch', f'pytorch-cpu=={pytorch}']
-    session.conda_install(*cmd)
+        cmd = [f'torch=={pytorch}']
+    session.install(*cmd)
 
 
 def install_torchvision(pytorch, session):
-    is_cpu_virtual = version.parse(pytorch) >= version.parse(PYTORCH_CPU_VIRTUAL_PKG)
-    fixed_pillow = version.parse(pytorch) >= version.parse(PYTORCH_PILLOW_FIXED)
-    if is_cpu_virtual:
-        cmd = ['-c', 'pytorch', 'torchvision', 'cpuonly']
+    torchvision = PARSED_TORCHVISION_VERSION_DICT[version.parse(pytorch)]
+    if not IS_OSX and version.parse(pytorch) > version.parse('1.1.0'):
+        cmd = [f'torchvision=={torchvision}+cpu', '-f', PYTORCH_STABLE_WHEEL_SRC]
+    elif not IS_OSX and version.parse(pytorch) <= version.parse('1.1.0'):
+        cmd = [f'torchvision=={torchvision}', '-f', PYTORCH_110_CPU_WHEEL_SRC]
     else:
-        cmd = ['-c', 'pytorch', 'torchvision-cpu']
-    if not fixed_pillow:
-        cmd += ['Pillow==6.2.0']
-    session.conda_install(*cmd)
+        cmd = [f'torchvision=={torchvision}']
+    if version.parse(pytorch) < version.parse(PYTORCH_PILLOW_FIXED):
+        cmd += [f'Pillow=={OLDER_PILLOW_VERSION}']
+    session.install(*cmd)
 
 
-def dry_run_install_pytorch_deps(python, pytorch, session, deps_only):
-    deps = '--only-deps' if deps_only else '--no-deps'
-    is_cpu_virtual = version.parse(pytorch) >= version.parse(PYTORCH_CPU_VIRTUAL_PKG)
-    if is_cpu_virtual:
-        cmd = ['conda', 'create', '-n', 'dry_run', deps, '-d', '-c', 'pytorch', f'pytorch=={pytorch}',
-               'cpuonly', f'python={python}']
-    else:
-        cmd = ['conda', 'create', '-n', 'dry_run', deps, '-d', '-c', 'pytorch', f'pytorch-cpu=={pytorch}',
-               'cpuonly', f'python={python}']
-    session.run(*cmd)
-
-
-@nox.session(venv_backend="conda", python=CONDA_PYTHON_VERSIONS)
-@nox.parametrize("pytorch", PYTORCH_VERSIONS, ids=PYTORCH_IDS)
-@nox.parametrize("jit_status", JIT_STATUSES, ids=JIT_IDS)
+@nox.session(python=PYTHON_VERSIONS)
+@nox.parametrize('pytorch', PYTORCH_VERSIONS, ids=PYTORCH_IDS)
+@nox.parametrize('jit_status', JIT_STATUSES, ids=JIT_IDS)
 def tests_brevitas_cpu(session, pytorch, jit_status):
     session.env['PYTORCH_JIT'] = '{}'.format(int(jit_status == 'jit_enabled'))
     session.env['BREVITAS_JIT'] = '{}'.format(int(jit_status == 'jit_enabled'))
@@ -69,12 +77,10 @@ def tests_brevitas_cpu(session, pytorch, jit_status):
     session.run('pytest', 'tests/brevitas', '-v', '--ignore', 'tests/brevitas/graph')
     # run graph tests
     if not is_torchvision_broken(session.python, pytorch):
-        session.run('pytest', 'tests/brevitas/graph/test_generator.py', '-v')
-        session.run('pytest', 'tests/brevitas/graph/test_tracer.py', '-v')
-        session.run('pytest', 'tests/brevitas/graph/test_rewriter.py', '-v')
+        session.run('pytest', 'tests/brevitas/graph', '-v')
 
 
-@nox.session(venv_backend="conda", python=CONDA_PYTHON_VERSIONS)
+@nox.session(python=PYTHON_VERSIONS)
 @nox.parametrize("pytorch", PYTORCH_VERSIONS, ids=PYTORCH_IDS)
 @nox.parametrize("jit_status", JIT_STATUSES, ids=JIT_IDS)
 def tests_brevitas_examples_cpu(session, pytorch, jit_status):
@@ -85,7 +91,7 @@ def tests_brevitas_examples_cpu(session, pytorch, jit_status):
     session.run('pytest', 'tests/brevitas_examples')
 
 
-@nox.session(venv_backend="conda", python=CONDA_PYTHON_VERSIONS)
+@nox.session(python=PYTHON_VERSIONS)
 @nox.parametrize("pytorch", PYTORCH_VERSIONS, ids=PYTORCH_IDS)
 def tests_brevitas_install_dev(session, pytorch):
     install_pytorch(pytorch, session)
@@ -95,7 +101,7 @@ def tests_brevitas_install_dev(session, pytorch):
     session.run('pytest', '-v', 'tests/brevitas/test_brevitas_import.py')
 
 
-@nox.session(venv_backend="conda", python=CONDA_PYTHON_VERSIONS)
+@nox.session(python=PYTHON_VERSIONS)
 @nox.parametrize("pytorch", PYTORCH_VERSIONS, ids=PYTORCH_IDS)
 def tests_brevitas_examples_install_dev(session, pytorch):
     install_pytorch(pytorch, session)
@@ -104,7 +110,7 @@ def tests_brevitas_examples_install_dev(session, pytorch):
     session.run('pytest', '-v', 'tests/brevitas_examples/test_examples_import.py')
 
 
-@nox.session(venv_backend="conda", python=CONDA_PYTHON_VERSIONS)
+@nox.session(python=PYTHON_VERSIONS)
 @nox.parametrize("pytorch", PYTORCH_VERSIONS, ids=PYTORCH_IDS)
 def tests_brevitas_finn_integration(session, pytorch):
     install_pytorch(pytorch, session)
@@ -114,7 +120,7 @@ def tests_brevitas_finn_integration(session, pytorch):
     session.run('pytest', '-v', 'tests/brevitas_finn', env=env)
 
 
-@nox.session(venv_backend="conda", python=CONDA_PYTHON_VERSIONS)
+@nox.session(python=PYTHON_VERSIONS)
 @nox.parametrize("pytorch", PYTORCH_VERSIONS, ids=PYTORCH_IDS)
 def tests_brevitas_ort_integration(session, pytorch):
     install_pytorch(pytorch, session)
@@ -123,24 +129,10 @@ def tests_brevitas_ort_integration(session, pytorch):
     session.run('pytest', '-v', 'tests/brevitas_ort')
 
 
-@nox.session(venv_backend="conda", python=CONDA_PYTHON_VERSIONS)
-@nox.parametrize("pytorch", PYTORCH_VERSIONS, ids=PYTORCH_IDS)
+@nox.session(python=PYTHON_VERSIONS)
+@nox.parametrize('pytorch', PYTORCH_VERSIONS, ids=PYTORCH_IDS)
 def tests_brevitas_pyxir_integration(session, pytorch):
     install_pytorch(pytorch, session)
     install_torchvision(pytorch, session)
     session.install('--upgrade', '-e', '.[test, pyxir_integration]')
     session.run('pytest', '-v', 'tests/brevitas_pyxir')
-
-
-@nox.session(python=False)
-@nox.parametrize("pytorch", PYTORCH_VERSIONS, ids=PYTORCH_IDS)
-@nox.parametrize("python", CONDA_PYTHON_VERSIONS, ids=CONDA_PYTHON_IDS)
-def dry_run_pytorch_only_deps(session, pytorch, python):
-    dry_run_install_pytorch_deps(python, pytorch, session, deps_only=True)
-
-
-@nox.session(python=False)
-@nox.parametrize("pytorch", PYTORCH_VERSIONS, ids=PYTORCH_IDS)
-@nox.parametrize("python", CONDA_PYTHON_VERSIONS, ids=CONDA_PYTHON_IDS)
-def dry_run_pytorch_no_deps(session, pytorch, python):
-    dry_run_install_pytorch_deps(python, pytorch, session, deps_only=False)
