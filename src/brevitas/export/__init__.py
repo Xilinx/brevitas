2d3e5d4ccc43c1c0b02683297c4f8fccd1f51cf9
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-03-03 12:07:23 +0000

    Feat (export): import managers in __init__.py
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

---
+++ src/brevitas/export/__init__.py
@@ -0,0 +1,5 @@
+from .onnx.finn.manager import FINNManager
+from .onnx.standard.manager import StdONNXManager
+from .onnx.pyxir.dpuv1.manager import DPUv1Manager
+from .onnx.pyxir.dpuv2.manager import DPUv2Manager
+from .pytorch.manager import PytorchQuantManager
