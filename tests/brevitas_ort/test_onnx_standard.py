97cd8c34737e5177337b8e58f3417613d27d5de8
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-03-08 11:22:08 +0000

    Tests (export/onnx/std): test __torch_function__ maxpool export
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- tests/brevitas_ort/test_onnx_standard.py
+++ tests/brevitas_ort/test_onnx_standard.py
@@ -141,3 +141,29 @@ def test_standard_onnx_quant_linear_export():
     model.eval()
     atol = model.linear.quant_output_scale().item() * TOLERANCE
     assert is_brevitas_ort_close(model, inp, export_name, atol=atol)
+
+
+def test_functional_max_pool_export():
+    IN_SIZE = (1, OUT_CH, IN_CH, IN_CH)
+
+    class Model(torch.nn.Module):
+
+        def __init__(self):
+            super().__init__()
+            self.act1 = QuantIdentity(return_quant_tensor=True)
+            self.act2 = QuantIdentity(return_quant_tensor=False)
+
+        def forward(self, x):
+            x = self.act1(x)
+            x = torch.nn.functional.max_pool2d(x, kernel_size=2)
+            x = self.act2(x)
+            x = torch.nn.functional.max_pool2d(x, kernel_size=2)
+            return x
+
+    export_name = 'stdonnx_F_max_pool2d.onnx'
+    inp = gen_linspaced_data(reduce(mul, IN_SIZE)).reshape(IN_SIZE)
+    model = Model()
+    model(torch.from_numpy(inp))  # accumulate scale factors
+    model.eval()
+    atol = model.act2.quant_output_scale().item() * TOLERANCE
+    assert is_brevitas_ort_close(model, inp, export_name, atol=atol)
