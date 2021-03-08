69e828106a53ba0dfaef576082c534980818f712
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-03-08 15:45:50 +0000

    Tests (export/pytorch): test export of __torch_function__ op
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- tests/brevitas/export/test_pytorch_qf_export.py
+++ tests/brevitas/export/test_pytorch_qf_export.py
@@ -140,3 +140,37 @@ def test_quant_max_pool2d_export():
     pytorch_out = pytorch_qf_model(inp)
     atol = model.act.quant_output_scale().item() * TOLERANCE
     assert pytorch_out.isclose(brevitas_out, rtol=0.0, atol=atol).all()
+
+
+def test_func_quant_max_pool2d_export():
+    IN_SIZE = (1, 1, IN_CH, IN_CH)
+    KERNEL_SIZE = 2
+
+    class Model(torch.nn.Module):
+
+        def __init__(self):
+            super().__init__()
+            self.act1 = QuantIdentity(
+                bit_width=7,
+                act_quant=ShiftedUint8ActPerTensorFloat,
+                return_quant_tensor=True)
+            self.act2 = QuantIdentity(
+                bit_width=7,
+                act_quant=ShiftedUint8ActPerTensorFloat,
+                return_quant_tensor=False)
+
+        def forward(self, x):
+            x = self.act1(x)
+            x = torch.nn.functional.max_pool2d(x, KERNEL_SIZE)
+            x = self.act2(x)
+            return x
+
+    inp = torch.randn(IN_SIZE)
+    model = Model()
+    model(inp)  # collect scale factors
+    model.eval()
+    brevitas_out = model(inp)
+    pytorch_qf_model = PytorchQuantManager.export(model, input_t=inp)
+    pytorch_out = pytorch_qf_model(inp)
+    atol = model.act2.quant_output_scale().item() * TOLERANCE
+    assert pytorch_out.isclose(brevitas_out, rtol=0.0, atol=atol).all()
