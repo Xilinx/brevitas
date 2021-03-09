fb406eb139aeaf82c431aeeeaef5d2f8732a3204
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-03-09 20:01:39 +0000

    Bugfix (examples): fix mnist download
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- src/brevitas_examples/bnn_pynq/__init__.py
+++ src/brevitas_examples/bnn_pynq/__init__.py
@@ -1,1 +1,7 @@
 from .models import *
+
+# Fix mnist download on older torchvision versions
+from six.moves import urllib
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+urllib.request.install_opener(opener)
