0f62a0107dd034aa8495922b9c3650ec12488fe0
Alessandro <volcacius@users.noreply.github.com>
2021-03-19 16:39:32 +0000

    Bugfix (examples/quartznet): set restrict act scaling

--- src/brevitas_examples/speech_to_text/quartznet/parts/common.py
+++ src/brevitas_examples/speech_to_text/quartznet/parts/common.py
@@ -31,6 +31,7 @@
 import brevitas.nn as quant_nn
 from brevitas.core.quant import QuantType
 from brevitas.core.scaling import ScalingImplType
+from brevitas.core.restrict_val import RestrictValueType
 from brevitas.core.stats import StatsOp
 global ACT_MIN_VAL, ACT_MAX_VAL
 brevitas_activations = {
@@ -44,6 +45,7 @@ QUANT_TYPE_BIAS = QuantType.FP
 SCALING_MIN_VAL = 2e-16
 ACT_SCALING_IMPL_TYPE = ScalingImplType.PARAMETER
 ACT_SCALING_PER_CHANNEL = False
+ACT_RESTRICT_SCALING_TYPE = RestrictValueType.LOG_FP
 
 WEIGHT_SCALING_IMPL_TYPE = ScalingImplType.STATS
 WEIGHT_SCALING_STATS_OP = StatsOp.MAX
@@ -53,13 +55,13 @@ BIAS_CONFIGS = False
 
 def make_quantization_input(bit_width, absolute_act_val, scaling_per_channel):
     return quant_nn.QuantHardTanh(bit_width=bit_width, scaling_per_channel=scaling_per_channel, quant_type=QUANT_TYPE,
-                                  scaling_impl_type=ACT_SCALING_IMPL_TYPE, scaling_min_val=SCALING_MIN_VAL,
+                                  scaling_impl_type=ACT_SCALING_IMPL_TYPE, scaling_min_val=SCALING_MIN_VAL, restrict_scaling_type=ACT_RESTRICT_SCALING_TYPE,
                                   max_val=absolute_act_val, min_val=-absolute_act_val, return_quant_tensor=False)
 
 
 def make_norm_scale(bit_width, absolute_act_val, scaling_per_channel):
     return quant_nn.QuantHardTanh(bit_width=bit_width, scaling_per_channel=scaling_per_channel, quant_type=QUANT_TYPE,
-                                  scaling_impl_type=ACT_SCALING_IMPL_TYPE, scaling_min_val=SCALING_MIN_VAL,
+                                  scaling_impl_type=ACT_SCALING_IMPL_TYPE, scaling_min_val=SCALING_MIN_VAL, restrict_scaling_type=ACT_RESTRICT_SCALING_TYPE,
                                   max_val=absolute_act_val, min_val=-absolute_act_val, scaling_stats_permute_dims=(1, 0, 2),
                                   return_quant_tensor=True)
 
@@ -67,7 +69,7 @@ def make_norm_scale(bit_width, absolute_act_val, scaling_per_channel):
 def make_jasper_activation(activation, channels, bit_width, absolute_act_val, scaling_per_channel):
     brevitas_activation = brevitas_activations[activation]
     return brevitas_activation(bit_width=bit_width, scaling_per_channel=scaling_per_channel, quant_type=QUANT_TYPE,
-                               scaling_impl_type=ACT_SCALING_IMPL_TYPE, scaling_min_val=SCALING_MIN_VAL,
+                               scaling_impl_type=ACT_SCALING_IMPL_TYPE, scaling_min_val=SCALING_MIN_VAL, restrict_scaling_type=ACT_RESTRICT_SCALING_TYPE,
                                max_val=absolute_act_val, per_channel_broadcastable_shape=(1, channels, 1),
                                scaling_stats_permute_dims=(1, 0, 2), return_quant_tensor=False)
 
