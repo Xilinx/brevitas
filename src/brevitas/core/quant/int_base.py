a3362369bcf9b7c708f4d2ac705c0d95e08e412d
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-04-14 22:55:28 +0100

    Feat (core): add DecoupledIntQuant
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- src/brevitas/core/quant/int_base.py
+++ src/brevitas/core/quant/int_base.py
@@ -131,3 +131,91 @@ class IntQuant(brevitas.jit.ScriptModule):
         y = y * scale
         y = self.delay_wrapper(x, y)
         return y
+
+
+class DecoupledIntQuant(brevitas.jit.ScriptModule):
+    """
+    ScriptModule that implements scale, shifted, uniform integer quantization of an input tensor,
+    according to an input pre-scale, scale, pre-zero-point, zero-point and bit-width.
+
+    Args:
+        narrow_range (bool): Flag that determines whether restrict quantization to a narrow range or not.
+        signed (bool): Flag that determines whether to quantize to a signed range or not.
+        float_to_int_impl (Module): Module that performs the conversion from floating point to
+            integer representation. Default: RoundSte()
+        tensor_clamp_impl (Module): Module that performs clamping. Default: TensorClamp()
+        quant_delay_steps (int): Number of training steps to delay quantization for. Default: 0
+
+    Returns:
+        Tensor: Quantized output in de-quantized format.
+
+    Examples:
+        >>> from brevitas.core.scaling import ConstScaling
+        >>> int_quant = DecoupledIntQuant(narrow_range=True, signed=True)
+        >>> scale, zero_point, bit_width = torch.tensor(0.01), torch.tensor(0.), torch.tensor(4.)
+        >>> pre_scale, pre_zero_point = torch.tensor(0.02), torch.tensor(0.)
+        >>> inp = torch.Tensor([0.042, -0.053, 0.31, -0.44])
+        >>> out = int_quant(pre_scale, pre_zero_point, scale, zero_point, bit_width, inp)
+        >>> out
+        tensor([ 0.0200, -0.0300,  0.0700, -0.0700])
+
+    Note:
+        Maps to quant_type == QuantType.INT == 'INT' == 'int' in higher-level APIs.
+
+    Note:
+        Set env variable BREVITAS_JIT=1 to enable TorchScript compilation of this module.
+    """
+
+    __constants__ = ['signed', 'narrow_range']
+
+    def __init__(
+            self,
+            narrow_range: bool,
+            signed: bool,
+            float_to_int_impl: Module = RoundSte(),
+            tensor_clamp_impl: Module = TensorClamp(),
+            quant_delay_steps: int = 0):
+        super(DecoupledIntQuant, self).__init__()
+        self.float_to_int_impl = float_to_int_impl
+        self.tensor_clamp_impl = tensor_clamp_impl
+        self.signed = signed
+        self.narrow_range = narrow_range
+        self.delay_wrapper = DelayWrapper(quant_delay_steps)
+
+    @brevitas.jit.script_method_110_disabled
+    def to_int(
+            self,
+            pre_scale: Tensor,
+            pre_zero_point: Tensor,
+            bit_width: Tensor,
+            x: Tensor) -> Tensor:
+        y = x / pre_scale
+        y = y + pre_zero_point
+        min_int_val = self.min_int(bit_width)
+        max_int_val = self.max_int(bit_width)
+        y = self.tensor_clamp_impl(y, min_val=min_int_val, max_val=max_int_val)
+        y = self.float_to_int_impl(y)
+        return y
+
+    @brevitas.jit.script_method
+    def min_int(self, bit_width):
+        return min_int(self.signed, self.narrow_range, bit_width)
+
+    @brevitas.jit.script_method
+    def max_int(self, bit_width):
+        return max_int(self.signed, self.narrow_range, bit_width)
+
+    @brevitas.jit.script_method
+    def forward(
+            self,
+            pre_scale: Tensor,
+            pre_zero_point: Tensor,
+            scale: Tensor,
+            zero_point: Tensor,
+            bit_width: Tensor,
+            x: Tensor) -> Tensor:
+        y_int = self.to_int(pre_scale, pre_zero_point, bit_width, x)
+        y = y_int - zero_point
+        y = y * scale
+        y = self.delay_wrapper(x, y)
+        return y
