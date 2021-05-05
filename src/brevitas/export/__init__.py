2d26387904394389d989fe3cc8080df5b5294727
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-05-05 19:25:03 +0100

    Fix (export): wrap export method not export_onnx

--- src/brevitas/export/__init__.py
+++ src/brevitas/export/__init__.py
@@ -10,19 +10,19 @@ from .pytorch.manager import PytorchQuantManager
 
 
 def export_finn_onnx(*args, **kwargs):
-    return FINNManager.export_onnx(*args, **kwargs)
+    return FINNManager.export(*args, **kwargs)
 
 
 def export_dpuv1_onnx(*args, **kwargs):
-    return DPUv1Manager.export_onnx(*args, **kwargs)
+    return DPUv1Manager.export(*args, **kwargs)
 
 
 def export_dpuv2_onnx(*args, **kwargs):
-    return DPUv2Manager.export_onnx(*args, **kwargs)
+    return DPUv2Manager.export(*args, **kwargs)
 
 
 def export_standard_onnx(*args, **kwargs):
-    return StdONNXManager.export_onnx(*args, **kwargs)
+    return StdONNXManager.export(*args, **kwargs)
 
 
 def jit_trace_dpuv1(*args, **kwargs):
