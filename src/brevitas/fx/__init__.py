8168e45cb5d8d81fadb218c57b40e263e4c65abf
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-05-18 15:06:07 +0100

    Feat (fx): add missing imports

--- src/brevitas/fx/__init__.py
+++ src/brevitas/fx/__init__.py
@@ -1,2 +1,2 @@
-from .brevitas_tracer import brevitas_symbolic_trace, brevitas_value_trace
+from .brevitas_tracer import brevitas_symbolic_trace, brevitas_value_trace, value_trace, symbolic_trace
 from .value_tracer import Graph, GraphModule, Proxy, Node
