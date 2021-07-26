8c422a4b8afe018894cd38734b7ed05d292cda8e
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-07-26 12:21:20 +0100

    Feat (nn): add BatchNorm1dToQuantScaleBias
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- src/brevitas/nn/quant_bn.py
+++ src/brevitas/nn/quant_bn.py
@@ -39,6 +39,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+from abc import ABC
 from typing import Optional
 
 import brevitas.config as config
@@ -48,29 +49,7 @@ from .utils import mul_add_from_bn
 from .quant_layer import WeightQuantType, BiasQuantType, ActQuantType
 
 
-class BatchNorm2dToQuantScaleBias(QuantScaleBias):
-
-    def __init__(
-            self,
-            num_features,
-            eps: float = 1e-5,
-            weight_quant: Optional[WeightQuantType] = Int8WeightPerTensorFloat,
-            bias_quant: Optional[BiasQuantType] = None,
-            input_quant: Optional[ActQuantType] = None,
-            output_quant: Optional[ActQuantType] = None,
-            return_quant_tensor: bool = False,
-            **kwargs):
-        super(BatchNorm2dToQuantScaleBias, self).__init__(
-            num_features,
-            bias=True,
-            runtime_shape=(1, -1, 1, 1),
-            weight_quant=weight_quant,
-            bias_quant=bias_quant,
-            input_quant=input_quant,
-            output_quant=output_quant,
-            return_quant_tensor=return_quant_tensor,
-            **kwargs)
-        self.eps = eps
+class _BatchNormToQuantScaleBias(QuantScaleBias, ABC):
 
     def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict,
                               missing_keys, unexpected_keys, error_msgs):
@@ -93,7 +72,7 @@ class BatchNorm2dToQuantScaleBias(QuantScaleBias):
             del state_dict[running_mean_key]
             del state_dict[running_var_key]
             del state_dict[num_batches_tracked_key]
-        super(BatchNorm2dToQuantScaleBias, self)._load_from_state_dict(
+        super(_BatchNormToQuantScaleBias, self)._load_from_state_dict(
             state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs)
         if config.IGNORE_MISSING_KEYS and bias_key in missing_keys:
             missing_keys.remove(bias_key)
@@ -101,3 +80,53 @@ class BatchNorm2dToQuantScaleBias(QuantScaleBias):
             missing_keys.remove(weight_key)
         if num_batches_tracked_key in unexpected_keys:
             unexpected_keys.remove(num_batches_tracked_key)
+
+
+class BatchNorm1dToQuantScaleBias(_BatchNormToQuantScaleBias):
+
+    def __init__(
+            self,
+            num_features,
+            eps: float = 1e-5,
+            weight_quant: Optional[WeightQuantType] = Int8WeightPerTensorFloat,
+            bias_quant: Optional[BiasQuantType] = None,
+            input_quant: Optional[ActQuantType] = None,
+            output_quant: Optional[ActQuantType] = None,
+            return_quant_tensor: bool = False,
+            **kwargs):
+        super(BatchNorm1dToQuantScaleBias, self).__init__(
+            num_features,
+            bias=True,
+            runtime_shape=(1, -1, 1),
+            weight_quant=weight_quant,
+            bias_quant=bias_quant,
+            input_quant=input_quant,
+            output_quant=output_quant,
+            return_quant_tensor=return_quant_tensor,
+            **kwargs)
+        self.eps = eps
+
+
+class BatchNorm2dToQuantScaleBias(_BatchNormToQuantScaleBias):
+
+    def __init__(
+            self,
+            num_features,
+            eps: float = 1e-5,
+            weight_quant: Optional[WeightQuantType] = Int8WeightPerTensorFloat,
+            bias_quant: Optional[BiasQuantType] = None,
+            input_quant: Optional[ActQuantType] = None,
+            output_quant: Optional[ActQuantType] = None,
+            return_quant_tensor: bool = False,
+            **kwargs):
+        super(BatchNorm2dToQuantScaleBias, self).__init__(
+            num_features,
+            bias=True,
+            runtime_shape=(1, -1, 1, 1),
+            weight_quant=weight_quant,
+            bias_quant=bias_quant,
+            input_quant=input_quant,
+            output_quant=output_quant,
+            return_quant_tensor=return_quant_tensor,
+            **kwargs)
+        self.eps = eps
