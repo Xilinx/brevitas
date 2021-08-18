308d4ff545f829dcf18b05889c3f6ef6692d561d
Alessandro Pappalardo <alessand@xilinx.com>
2021-08-18 10:51:41 +0100

    Examples: update imagenet calibration script

--- src/brevitas_examples/imagenet_classification/flexml_imagenet_calibration.py
+++ src/brevitas_examples/imagenet_classification/flexml_imagenet_calibration.py
@@ -14,67 +14,20 @@ import torchvision.datasets as datasets
 from torchvision import models
 
 from brevitas.graph.target.flexml import quantize_flexml
-from brevitas.graph.calibrate import BiasCorrection, FloatModelInference
+from brevitas.graph.calibrate import calibrate
+from brevitas.export.onnx.generic.manager import BrevitasONNXManager
 
 SEED = 123456
-
+INPUT_CHANNELS = 3
+INPUT_FEATURES = 224
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Validation')
 parser.add_argument('--imagenet-dir', help='path to folder containing Imagenet val folder')
-parser.add_argument('--model', type=str, default='resnet18', help='Name of the model')
-parser.add_argument('--pretrained', action='store_true', help='Load pretrained checkpoint')
+parser.add_argument('--model', type=str, default='resnet18', help='Name of the torchvision model')
 parser.add_argument('--workers', default=4, type=int, help='number of data loading workers')
 parser.add_argument('--batch-size', default=256, type=int, help='Minibatch size')
 parser.add_argument('--gpu', default=None, type=int, help='GPU id to use.')
 parser.add_argument('--shuffle', action='store_true', help='Shuffle validation data.')
-
-
-def main():
-    args = parser.parse_args()
-    random.seed(SEED)
-    torch.manual_seed(SEED)
-
-    model = getattr(models, args.model)(pretrained=args.pretrained)
-    if args.gpu is not None:
-        torch.cuda.set_device(args.gpu)
-        model = model.cuda(args.gpu)
-
-    valdir = os.path.join(args.imagenet_dir, 'val')
-    calibrationdir = os.path.join(args.imagenet_dir, 'calibration')
-    mean = [0.485, 0.456, 0.406]
-    std = [0.229, 0.224, 0.225]
-    normalize = transforms.Normalize(mean=mean, std=std)
-    val_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(valdir, transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            normalize,
-        ])),
-        batch_size=args.batch_size, shuffle=args.shuffle, num_workers=args.workers, pin_memory=True)
-    calibration_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(calibrationdir, transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            normalize,
-        ])),
-        batch_size=args.batch_size, shuffle=args.shuffle, num_workers=args.workers, pin_memory=True)
-    model = quantize(calibration_loader, model, args)
-    model = calibrate(calibration_loader, model, args)
-    validate(val_loader, model, args)
-    return
-
-def quantize(calibration_loader, model, args):
-    with torch.no_grad():
-        for images, _ in calibration_loader:
-            if args.gpu is not None:
-                images = images.cuda(args.gpu, non_blocking=True)
-            model = quantize_flexml(model, images)
-            break
-    if args.gpu is not None:
-        model = model.cuda(args.gpu)
-    return model
 
 
 def calibrate(calibration_loader, model, args):
@@ -83,10 +36,7 @@ def calibrate(calibration_loader, model, args):
             if args.gpu is not None:
                 images = images.cuda(args.gpu, non_blocking=True)
             print(f'Calibration iteration {i}')
-            model.train()
-            model = FloatModelInference().apply(model, images)
-            model.eval()
-            model = BiasCorrection().apply(model, images)
+            calibrate(model, images)
     return model
 
 
@@ -112,7 +62,7 @@ def validate(val_loader, model, args):
             top5.update(acc5[0], images.size(0))
             print_accuracy(top1, top5, '{}/{}: '.format(i, num_batches))
         print_accuracy(top1, top5, 'Total:')
-    return
+    return top1, top5
 
 
 class AverageMeter(object):
@@ -154,6 +104,50 @@ def accuracy(output, target, topk=(1,)):
             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul(100.0 / batch_size))
         return res
+
+
+def main():
+    args = parser.parse_args()
+    random.seed(SEED)
+    torch.manual_seed(SEED)
+
+    model = getattr(models, args.model)(pretrained=True)
+    model = quantize_flexml(model, torch.randn(2, INPUT_CHANNELS, INPUT_FEATURES, INPUT_FEATURES))
+    if args.gpu is not None:
+        torch.cuda.set_device(args.gpu)
+        model = model.cuda(args.gpu)
+
+    valdir = os.path.join(args.imagenet_dir, 'val')
+    calibrationdir = os.path.join(args.imagenet_dir, 'calibration')
+
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    normalize = transforms.Normalize(mean=mean, std=std)
+    
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(INPUT_FEATURES),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.batch_size, shuffle=args.shuffle, num_workers=args.workers, pin_memory=True)
+    
+    calibration_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(calibrationdir, transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(INPUT_FEATURES),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.batch_size, shuffle=args.shuffle, num_workers=args.workers, pin_memory=True)
+    
+    model = calibrate(calibration_loader, model, args)
+    top1, top5 = validate(val_loader, model, args)
+    BrevitasONNXManager.export(
+        model.cpu(), 
+        input_t=torch.randn(2, INPUT_CHANNELS, INPUT_FEATURES, INPUT_FEATURES), 
+        export_path=f'{args.model}_flexml_{top1.avg:.2f}.onnx')
 
 
 if __name__ == '__main__':
