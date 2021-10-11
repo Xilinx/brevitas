ac33d898044e08a25fe97ec60398df1221257f98
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-10-11 16:02:53 +0100

    Feat (core): import for DPU rounding
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- src/brevitas/core/function_wrapper/__init__.py
+++ src/brevitas/core/function_wrapper/__init__.py
@@ -1,7 +1,7 @@
 from .clamp import ClampMin, TensorClamp, ScalarClamp
 from .misc import LogTwo, InplaceLogTwo, PowerOfTwo, Identity
 from .ops_ste import CeilSte, ScalarClampMinSte, FloorSte, RoundSte, RoundToZeroSte, TensorClampSte
-from .ops_ste import InplaceTensorClampSte
+from .ops_ste import InplaceTensorClampSte, DPURoundSte
 from .shape import OverBatchOverOutputChannelView, OverBatchOverTensorView, OverOutputChannelView
 from .shape import OverTensorView, StatsInputViewShapeImpl
 
