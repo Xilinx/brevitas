01f58e600a8fdb767cac2604a32d271ba31868bf
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-10-11 15:49:20 +0100

    Feat (utils): add FloatToIntImpl to enum mapping
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- src/brevitas/utils/quant_utils.py
+++ src/brevitas/utils/quant_utils.py
@@ -38,13 +38,15 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from brevitas.proxy.parameter_quant import WeightQuantProxyFromInjector
 from brevitas.core.quant import RescalingIntQuant
-from brevitas.proxy.runtime_quant import ActQuantProxyFromInjector, FusedActivationQuantProxy
 from brevitas.core.bit_width import BitWidthParameter
+from brevitas.core.function_wrapper import *
+from brevitas.inject.enum import FloatToIntImplType
 
 
 def has_learned_weight_bit_width(module):
+    from brevitas.proxy.parameter_quant import WeightQuantProxyFromInjector
+
     if isinstance(module, WeightQuantProxyFromInjector) \
             and isinstance(module.tensor_quant, RescalingIntQuant) \
             and isinstance(module.tensor_quant.msb_clamp_bit_width_impl,
@@ -55,6 +57,8 @@ def has_learned_weight_bit_width(module):
 
 
 def has_learned_activation_bit_width(module):
+    from brevitas.proxy.runtime_quant import ActQuantProxyFromInjector, FusedActivationQuantProxy
+
     if isinstance(module, ActQuantProxyFromInjector) \
             and isinstance(module.fused_activation_quant_proxy, FusedActivationQuantProxy) \
             and isinstance(module.fused_activation_quant_proxy.tensor_quant, RescalingIntQuant) \
@@ -63,3 +67,18 @@ def has_learned_activation_bit_width(module):
         return True
     else:
         return False
+
+
+def float_to_int_impl_to_enum(module):
+    if isinstance(module, RoundSte):
+        return FloatToIntImplType.ROUND
+    elif isinstance(module, RoundToZeroSte):
+        return FloatToIntImplType.ROUND_TO_ZERO
+    elif isinstance(module, FloorSte):
+        return FloatToIntImplType.FLOOR
+    elif isinstance(module, CeilSte):
+        return FloatToIntImplType.CEIL
+    elif isinstance(module, DPURoundSte):
+        return DPURoundSte
+    else:
+        return None
