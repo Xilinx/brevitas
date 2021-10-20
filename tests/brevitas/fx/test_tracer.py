b6d1c46f1b1dedabe532c9cc04531c24ae08fe49
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-10-20 14:05:06 +0100

    Tests (fx): fix import failure on 1.5
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- tests/brevitas/fx/test_tracer.py
+++ tests/brevitas/fx/test_tracer.py
@@ -1,40 +1,40 @@
-from packaging import version
 import pytest
 import torch
 from torch import Tensor
 from torch.nn import Module
-from torchvision import models
+from torchvision.models.resnet import resnet18
+from torchvision.models.mobilenetv2 import mobilenet_v2
+from torchvision.models.alexnet import alexnet
+from torchvision.models.squeezenet import squeezenet1_0
+from torchvision.models.shufflenetv2 import shufflenet_v2_x0_5
+from torchvision.models.mnasnet import mnasnet0_5
+from torchvision.models.densenet import densenet121
 
-import brevitas
-from brevitas.fx import value_trace, symbolic_trace, brevitas_symbolic_trace, brevitas_value_trace
+from brevitas.fx import value_trace, brevitas_symbolic_trace, brevitas_value_trace
 
 SEED = 123456
 INPUT_SIZE = (2, 3, 224, 224)
 INCEPTION_INPUT_SIZE = (2, 3, 299, 299)
 
 
-TV_MODEL_NAMES = [
-    'resnet18',
-    'mobilenet_v2',
-    'alexnet',
-    'squeezenet1_0',
-    'shufflenet_v2_x0_5',
-    'mnasnet0_5',
-    'densenet121']
-
-if brevitas.torch_version >= version.parse('1.4'):
-    TV_MODEL_NAMES += ['inception_v3']
+TV_MODELS = [
+    resnet18,
+    mobilenet_v2,
+    alexnet,
+    squeezenet1_0,
+    shufflenet_v2_x0_5,
+    mnasnet0_5,
+    densenet121]
 
 
 @pytest.mark.parametrize("pretrained", [True, False])
 @pytest.mark.parametrize("train", [True, False])
-@pytest.mark.parametrize("model_name", TV_MODEL_NAMES)
-def test_brevitas_value_tracer(model_name: str, train: bool, pretrained: bool):
-    model = getattr(models, model_name)(pretrained=pretrained)
+@pytest.mark.parametrize("model_impl", TV_MODELS)
+def test_brevitas_value_tracer(model_impl, train: bool, pretrained: bool):
+    model = model_impl(pretrained=pretrained)
     model = model.train(train)
     torch.manual_seed(SEED)
-    inp_size = INCEPTION_INPUT_SIZE if model_name == 'inception_v3' else INPUT_SIZE
-    inp = torch.randn(inp_size)
+    inp = torch.randn(INPUT_SIZE)
     torch.manual_seed(SEED)
     with torch.no_grad():
         graph_model = brevitas_value_trace(model, concrete_args={'x': inp})
@@ -52,13 +52,12 @@ def test_brevitas_value_tracer(model_name: str, train: bool, pretrained: bool):
 
 @pytest.mark.parametrize("pretrained", [True, False])
 @pytest.mark.parametrize("train", [True, False])
-@pytest.mark.parametrize("model_name", TV_MODEL_NAMES)
-def test_brevitas_symbolic_tracer(model_name: str, train: bool, pretrained: bool):
-    model = getattr(models, model_name)(pretrained=pretrained)
+@pytest.mark.parametrize("model_impl", TV_MODELS)
+def test_brevitas_symbolic_tracer(model_impl, train: bool, pretrained: bool):
+    model = model_impl(pretrained=pretrained)
     model = model.train(train)
     torch.manual_seed(SEED)
-    inp_size = INCEPTION_INPUT_SIZE if model_name == 'inception_v3' else INPUT_SIZE
-    inp = torch.randn(inp_size)
+    inp = torch.randn(INPUT_SIZE)
     torch.manual_seed(SEED)
     with torch.no_grad():
         graph_model = brevitas_symbolic_trace(model)
