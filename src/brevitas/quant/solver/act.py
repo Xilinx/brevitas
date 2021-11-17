f18e0788ade18b00c3aee3bc0377cb2dca09ccc5
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2021-11-17 12:57:46 +0000

    Fix (solver): alternative per-channel act syntax

--- src/brevitas/quant/solver/act.py
+++ src/brevitas/quant/solver/act.py
@@ -127,15 +127,15 @@ class SolveActScalingInitFromEnum(ExtendedInjector):
 class SolveActScalingShape(ExtendedInjector):
 
     @value
-    def scaling_per_channel(scaling_per_output_channel):
+    def scaling_per_output_channel(scaling_per_channel):
         """Alternative syntax"""
-        return scaling_per_output_channel
+        return scaling_per_channel
 
     @value
-    def scaling_shape(scaling_per_channel):
+    def scaling_shape(scaling_per_output_channel):
         # this pattern of returning this.something allows to resolve scaling_output_channel_shape
         # only when scaling_per_output_channel is True
-        if scaling_per_channel:
+        if scaling_per_output_channel:
             return this.per_channel_broadcastable_shape
         else:
             return SCALAR_SHAPE
