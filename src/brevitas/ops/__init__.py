dc6827602ede075a3db8b81f3dac04d3e215b864
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2022-01-21 16:42:06 +0000

    Fix (ops): torchscript loading of csrc backend

---
+++ src/brevitas/ops/__init__.py
@@ -0,0 +1,6 @@
+# Match naming of csrc so that e.g. I can import C++ from torch.ops.autograd_ste_ops 
+# and python from brevitas.ops.autograd_ste_ops
+
+# import brevitas.ops.autograd_ste_ops as autograd_ste_ops
+# #is not supported syntax on 3.6, resort to this
+from brevitas.ops import autograd_ste_ops as autograd_ste_ops
