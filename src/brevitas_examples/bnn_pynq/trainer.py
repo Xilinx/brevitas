c366470adce64d61814f1ad33b47d339c90f37dc
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2022-03-24 12:10:46 +0000

    Fix (examples/bnn_pynq): broken MNIST download on latest torchvision

--- src/brevitas_examples/bnn_pynq/trainer.py
+++ src/brevitas_examples/bnn_pynq/trainer.py
@@ -24,8 +24,10 @@ import random
 import os
 import time
 from datetime import datetime
+from packaging.version import parse
 
 import torch
+import torchvision
 import torch.optim as optim
 from torch import nn
 from torch.optim.lr_scheduler import MultiStepLR
@@ -40,19 +42,18 @@ from .models.losses import SqrHingeLoss
 
 class MirrorMNIST(MNIST):
 
-    resources = [
-        ("https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz",
-         "f68b3c2dcbeaaa9fbdd348bbdeb94873"),
-        ("https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz",
-         "d53e105ee54ea40749a09fcbcd1e9432"),
-        ("https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz",
-         "9fb629c4189551a2d022fa330f9573f3"),
-        ("https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz",
-         "ec29112dd5afa0611ce80d1b7f02629c")
-    ]
+    if parse(torchvision.__version__) < parse('0.9.1'):
 
-    # required by torchvision <= 0.4.2
-    urls = [l for l, h in resources]
+        resources = [
+            ("https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz",
+            "f68b3c2dcbeaaa9fbdd348bbdeb94873"),
+            ("https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz",
+            "d53e105ee54ea40749a09fcbcd1e9432"),
+            ("https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz",
+            "9fb629c4189551a2d022fa330f9573f3"),
+            ("https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz",
+            "ec29112dd5afa0611ce80d1b7f02629c")
+        ]
 
 
 def accuracy(output, target, topk=(1,)):
