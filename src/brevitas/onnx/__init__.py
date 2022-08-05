80a3de4ff18953fd306c38a4c6c7f9b5c11a1b29
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2022-08-05 16:13:00 +0100

    Feat (export/onnx): rename generic to qonnx

--- src/brevitas/onnx/__init__.py
+++ src/brevitas/onnx/__init__.py
@@ -1,6 +1,7 @@
 # for retrocompatibility
 
 from brevitas.export import export_finn_onnx  # noqa
+from brevitas.export import export_qonnx  # noqa
 from brevitas.export import export_brevitas_onnx  # noqa
 from brevitas.export import export_standard_qop_onnx  # noqa
 from brevitas.export import export_standard_qcdq_onnx # noqa
