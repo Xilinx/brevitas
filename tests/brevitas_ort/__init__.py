18677cce221c93b9bb64cc2be7ff9e0b0b3f49e9
Giuseppe <giuseppefranco4@gmail.com>
2022-10-25 16:53:28 +0200

    Test (qcdq-qop): Expanded tests with pytest_cases

--- tests/brevitas_ort/__init__.py
+++ tests/brevitas_ort/__init__.py
@@ -1,4 +1,8 @@
-import torch
+try:
+    import torch
 
-# Avoid fast algorithms that might introduce extra error during fake-quantization
-torch.use_deterministic_algorithms(True)
+    # Avoid fast algorithms that might introduce extra error during fake-quantization
+    torch.use_deterministic_algorithms(True)
+except:
+    # Introduced in 1.8.0
+    pass
