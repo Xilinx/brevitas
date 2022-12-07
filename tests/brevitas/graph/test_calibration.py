b1839861ea78566ccfafa81fc8c07b9bd5aa3103
Giuseppe <giuseppefranco4@gmail.com>
2022-12-07 10:30:25 +0000

    Test (graph): Test for bias correction

--- tests/brevitas/graph/test_calibration.py
+++ tests/brevitas/graph/test_calibration.py
@@ -1,11 +1,15 @@
 import torch
 import brevitas.nn as qnn
 import torch.nn as nn
-from brevitas.graph.calibrate import calibration_mode
+from brevitas.graph.calibrate import calibration_mode, bias_correction_mode
 from brevitas.quant import Int8ActPerTensorFixedPoint
 from tests.brevitas.hyp_helper import float_tensor_random_size_st
 from hypothesis import given
 import math
+
+IN_CH = 8
+OUT_CH = 16
+BATCH = 1
 
 def compute_quantile(x, q):
     k = int(math.floor(.01 * q * x.numel() + 0.5))
@@ -20,6 +24,7 @@ def reference_implementation_scale_factors_po2(x, q=99.999, min_val=torch.tensor
     scale =  torch.pow(torch.tensor(2.), quant_float_to_int)/int_scale
     
     return scale
+
 
 @given(inp=float_tensor_random_size_st())
 def test_scale_factors_ptq_calibration_po2(inp):
@@ -64,3 +69,22 @@ def test_calibration_training_state():
     
     assert model.act.act_quant.training == False
     assert model.training == False
+
+
+def test_bias_correction():
+    inp = torch.randn(BATCH, IN_CH)
+    fp_layer = nn.Linear(IN_CH, OUT_CH, bias=False)
+
+    quant_layer = qnn.QuantLinear(IN_CH, OUT_CH, bias=False)
+    quant_layer.weight.data = fp_layer.weight.data
+    fp_layer.eval()
+    quant_layer.eval()
+
+    quant_out = quant_layer(inp)
+    quant_fp = fp_layer(inp)
+
+    with bias_correction_mode(quant_layer):
+        quant_layer(inp)
+
+    assert quant_layer.bias is not None
+    assert torch.allclose(quant_layer.bias, quant_fp-quant_out)
