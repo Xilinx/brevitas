c7b6a2cf84bf2580a3ea995194784636216bd72f
Alessandro Pappalardo <alessand@amd.com>
2022-12-20 09:45:33 +0000

    Setup: clean up noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -10,19 +10,10 @@ from gen_github_actions import PYTORCH_VERSIONS, PYTHON_VERSIONS, JIT_STATUSES
 
 IS_OSX = system() == 'Darwin'
 PYTORCH_STABLE_WHEEL_SRC = 'https://download.pytorch.org/whl/torch_stable.html'
-PYTORCH_110_CPU_WHEEL_SRC = 'https://download.pytorch.org/whl/cpu/torch_stable.html'
-PYTORCH_PILLOW_FIXED = '1.4.0'
-OLDER_PILLOW_VERSION = '6.2.0'
 PYTORCH_IDS = tuple([f'pytorch_{i}' for i in PYTORCH_VERSIONS])
 JIT_IDS = tuple([f'{i}'.lower() for i in JIT_STATUSES])
 
 TORCHVISION_VERSION_DICT = {
-    '1.1.0': '0.3.0',
-    '1.2.0': '0.4.0',
-    '1.3.0': '0.4.1',
-    '1.3.1': '0.4.2',
-    '1.4.0': '0.5.0',
-    '1.5.0': '0.6.0',
     '1.5.1': '0.6.1',
     '1.6.0': '0.7.0',
     '1.7.0': '0.8.1',
@@ -40,19 +31,9 @@ PARSED_TORCHVISION_VERSION_DICT = {
     version.parse(k): v for k, v in TORCHVISION_VERSION_DICT.items()}
 
 
-# This combination has proven to be problematic and without access to
-# a physical system it's tricky to debug
-def is_torchvision_broken(python_version, pytorch_version):
-    return (IS_OSX and
-            version.parse(python_version) == version.parse('3.6') and
-            version.parse(pytorch_version) == version.parse('1.5'))
-
-
 def install_pytorch(pytorch, session):
-    if not IS_OSX and version.parse(pytorch) > version.parse('1.1.0'):
+    if not IS_OSX:
         cmd = [f'torch=={pytorch}+cpu', '-f', PYTORCH_STABLE_WHEEL_SRC]
-    elif not IS_OSX and version.parse(pytorch) <= version.parse('1.1.0'):
-        cmd = [f'torch=={pytorch}', '-f', PYTORCH_110_CPU_WHEEL_SRC]
     else:
         cmd = [f'torch=={pytorch}']
     session.install(*cmd)
@@ -60,20 +41,13 @@ def install_pytorch(pytorch, session):
 
 def install_torchvision(pytorch, session):
     torchvision = PARSED_TORCHVISION_VERSION_DICT[version.parse(pytorch)]
-    if not IS_OSX and version.parse(pytorch) > version.parse('1.1.0'):
+    if not IS_OSX:
         cmd = [
             f'torch=={pytorch}+cpu',  # make sure correct pytorch version is kept
             f'torchvision=={torchvision}+cpu',
             '-f', PYTORCH_STABLE_WHEEL_SRC]
-    elif not IS_OSX and version.parse(pytorch) <= version.parse('1.1.0'):
-        cmd = [
-            f'torch=={pytorch}',  # make sure correct pytorch version is kept
-            f'torchvision=={torchvision}',
-            '-f', PYTORCH_110_CPU_WHEEL_SRC]
     else:
         cmd = [f'torch=={pytorch}', f'torchvision=={torchvision}']
-    if version.parse(pytorch) < version.parse(PYTORCH_PILLOW_FIXED):
-        cmd += [f'Pillow=={OLDER_PILLOW_VERSION}']
     session.install(*cmd)
 
 
@@ -87,9 +61,8 @@ def tests_brevitas_cpu(session, pytorch, jit_status):
     session.install( '--upgrade', '.[test, export]')
     # run non graph tests
     session.run('pytest', 'tests/brevitas', '-v', '--ignore', 'tests/brevitas/graph')
-    # run graph tests
-    if not is_torchvision_broken(session.python, pytorch):
-        session.run('pytest', 'tests/brevitas/graph', '-v')
+    # run graph tests separately
+    session.run('pytest', 'tests/brevitas/graph', '-v')
 
 
 @nox.session(python=PYTHON_VERSIONS)
