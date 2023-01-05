15f4ba6dd621334f0a11f8c0209170073c26ac2a
Alessandro Pappalardo <alessand@amd.com>
2023-01-05 10:56:08 +0000

    Reorganize torch qcdq tests around fixtures

--- tests/brevitas/export/test_torch_qcdq.py
+++ tests/brevitas/export/test_torch_qcdq.py
@@ -1,51 +1,47 @@
 import pytest
 
-import torch
-from pytest_cases import parametrize_with_cases, get_case_id
 from tests.marker import requires_pt_ge
 from brevitas.export import export_torch_qcdq
 
-from .export_cases import TorchQuantWBIOLCases
-from .export_cases import FEATURES, IN_CH, TOLERANCE, IN_MEAN, IN_SCALE
+from .quant_module_fixture import *
 
 
-@parametrize_with_cases('model', cases=TorchQuantWBIOLCases.case_quant_wbiol_qcdq)
 @requires_pt_ge('1.9.1')
-def test_pytorch_qcdq_export(model, current_cases):
-    cases_generator_func = current_cases['model'][1]
-    case_id = get_case_id(cases_generator_func)
-    impl = case_id.split('-')[-1] # Inverse list of definition, 'impl' is -1.
-    quantizer = case_id.split('-')[-2]
-    input_bit_width = case_id.split('-')[-3]
-    weight_bit_width = case_id.split('-')[-4]
-    output_bit_width = case_id.split('-')[-5]
-    bias_bit_width = case_id.split('-')[-6]
-    bias_quantizer = case_id.split('-')[-7]
-    
-    if 'asymmetric_act' in quantizer and ('i10' in input_bit_width or 'o10' in output_bit_width):
+def test_pytorch_qcdq_export(
+        quant_module,
+        quant_module_impl,
+        weight_act_quantizers,
+        input_bit_width,
+        weight_bit_width,
+        output_bit_width,
+        bias_bit_width,
+        bias_quantizer):
+
+    weight_act_quantizers_name, _ = weight_act_quantizers
+    bias_quantizer_name, _ = bias_quantizer
+
+    if 'asymmetric_act' in weight_act_quantizers_name and (input_bit_width > 8 or output_bit_width > 8):
         pytest.skip("Unsigned zero point supported on 8b or less.")
-    if 'asymmetric_weight' in quantizer and 'w10' in weight_bit_width:
+    if 'asymmetric_weight' in weight_act_quantizers_name and weight_bit_width > 8:
         pytest.skip("Unsigned zero point supported on 8b or less.")
-    if 'internal_scale' in bias_quantizer and 'b32' in bias_bit_width:
+    if 'internal_scale' in bias_quantizer_name and bias_bit_width == 32:
         pytest.skip("This combination is prone to numerical errors as the scale gets too small.")
 
-    if impl in ('QuantLinear'):
+    if quant_module_impl == QuantLinear:
         in_size = (1, IN_CH)
-    elif impl in ('QuantConv1d', 'QuantConvTranspose1d'):
+    elif quant_module_impl == QuantConv1d or quant_module_impl == QuantConvTranspose1d:
         in_size = (1, IN_CH, FEATURES)
     else:
         in_size = (1, IN_CH, FEATURES, FEATURES)
 
     inp = torch.randn(in_size)
-    model(inp) # Collect scale factors
-    model.eval()
-    export_path = f'torch_qcdq_{case_id}.pt'
+    quant_module(inp)  # Collect scale factors
+    quant_module.eval()
     inp = torch.randn(in_size) * IN_SCALE + IN_MEAN  # redefine inp for testing
-    out = model(inp)
-    pytorch_qcdq_model = export_torch_qcdq(model, args=inp)
+    out = quant_module(inp)
+    pytorch_qcdq_model = export_torch_qcdq(quant_module, args=inp)
     torchscript_out = pytorch_qcdq_model(inp)
     torchscript_out_value = torchscript_out[0]
     tolerance = TOLERANCE * out.scale
     del pytorch_qcdq_model
-    del model
     assert torch.allclose(out, torchscript_out_value, atol=tolerance)
