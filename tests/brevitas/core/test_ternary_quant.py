acb0db676a27fe0d08b56eea935bcc97bd1701aa
Giuseppe Franco <giuseppefranco4@gmail.com>
2023-01-26 16:23:34 +0100

    Missing update

--- tests/brevitas/core/test_ternary_quant.py
+++ tests/brevitas/core/test_ternary_quant.py
@@ -14,7 +14,7 @@ from tests.brevitas.core.shared_quant_fixture import *  # noqa
 from tests.brevitas.core.ternary_quant_fixture import *  # noqa
 from tests.brevitas.hyp_helper import float_tensor_random_shape_st
 from tests.brevitas.hyp_helper import scalar_float_p_tensor_st
-from tests.marker import requires_jit_disabled
+from tests.marker import jit_disabled_for_mock
 
 
 def is_ternary_output_value_correct(scale: Tensor, output: Tensor):
@@ -33,7 +33,7 @@ class TestTernaryUnit:
         inp=float_tensor_random_shape_st(),
         scale_init=scalar_float_p_tensor_st(),
         threshold=st.floats(min_value=0.0, max_value=1.0))
-    @requires_jit_disabled() # Mock conflicts with JIT
+    @jit_disabled_for_mock()
     def test_ternary_quant(self, inp, scale_init, threshold):
         scaling_impl = mock.Mock(return_value=scale_init)
         ternary_quant = TernaryQuant(scaling_impl, threshold)
