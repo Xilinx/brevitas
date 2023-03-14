1231ad131f8ed5239bc35cbbc0d2ed9d35ba11d9
icolbert <Ian.Colbert@amd.com>
2023-03-14 15:42:42 -0700

    Minor cosmetic changes

--- tests/brevitas/core/test_bit_width.py
+++ tests/brevitas/core/test_bit_width.py
@@ -128,7 +128,13 @@ class TestBitWidthParameter:
         else:
             assert value == state_dict_value
 
-    def test_load_from_stateful_const(self, bit_width_parameter, bit_width_stateful_const, bit_width_init, min_bit_width_init, bit_width_init_two, override_pretrained):
+    def test_load_from_stateful_const(self,
+                                      bit_width_parameter,
+                                      bit_width_stateful_const,
+                                      bit_width_init,
+                                      min_bit_width_init,
+                                      bit_width_init_two,
+                                      override_pretrained):
         """
         Test state dictionary from BitWidthStatefulConst is read correctly
         """
