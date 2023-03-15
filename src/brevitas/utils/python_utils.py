4992bba5280d872b5157408a7ffbeec547d8d2b4
Giuseppe Franco <giuseppefranco4@gmail.com>
2023-03-15 12:20:03 +0000

    Feat (python_utils): add islambda function

--- src/brevitas/utils/python_utils.py
+++ src/brevitas/utils/python_utils.py
@@ -31,3 +31,8 @@ def patch(module, attr, new_value):
         yield getattr(module, attr)
     finally:
         setattr(module, attr, old_value)
+
+
+def islambda(v):
+  LAMBDA = lambda:0
+  return isinstance(v, type(LAMBDA)) and v.__name__ == LAMBDA.__name__
