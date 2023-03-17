3bcbb4d0323348598b5f9de34a98b5f323f3dfba
icolbert <Ian.Colbert@amd.com>
2023-03-17 07:33:09 -0700

    Adding patch to work around shared memory in macos tests

--- tests/brevitas/core/test_bit_width.py
+++ tests/brevitas/core/test_bit_width.py
@@ -6,6 +6,7 @@ from hypothesis import given
 import pytest
 import pytest_cases
 import torch
+import copy
 
 from brevitas import config
 from brevitas.core.bit_width import BitWidthParameter
@@ -141,6 +142,8 @@ class TestBitWidthParameter:
         if (bit_width_init_two < min_bit_width_init) and not override_pretrained:
             pytest.xfail('bit_width cannot be smaller than min_bit_width')
         override_value = bit_width_parameter.bit_width_offset
+        bit_width_parameter = copy.deepcopy(bit_width_parameter)
+        bit_width_stateful_const = copy.deepcopy(bit_width_stateful_const)
         bit_width_parameter.load_state_dict(bit_width_stateful_const.state_dict())
         bit_width_parameter_tensor = bit_width_parameter()
         if override_pretrained:
