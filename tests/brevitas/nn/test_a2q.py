029500d4f63cd90968b8d47af96b8f247a44a9cb
icolbert <Ian.Colbert@amd.com>
2023-03-29 20:32:34 -0700

    Adding tests for A2Q

---
+++ tests/brevitas/nn/test_a2q.py
@@ -0,0 +1,93 @@
+# Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
+import pytest_cases
+from pytest_cases import get_case_id
+import torch
+
+from brevitas.quant_tensor import QuantTensor
+from brevitas.nn.utils import calculate_min_accumulator_bit_width
+
+from .nn_quantizers_fixture import case_model_a2q
+
+
+def parse_args(args):
+    kwargs = {}
+    for arg in args:
+        if '$' not in arg:
+            continue
+        k, v = arg.split('$')
+        try:
+            v = eval(v)
+        except:
+            pass
+        kwargs[k] = v
+    return kwargs
+
+@pytest_cases.parametrize_with_cases('model_input', cases=case_model_a2q)
+def test_quant_wbiol_a2q(model_input, current_cases):
+    """This test verifies that the accumulator-aware weight quantization constraints the l1-norm of
+    the weights enough use the user-specified accumulator bit-width."""
+    model, input = model_input
+
+    cases_generator_func = current_cases['model_input'][1]
+    case_id = get_case_id(cases_generator_func)
+    args = case_id.split('-')[1:] # Exclude first argument
+    kwargs = parse_args(args)
+
+    # A2Q needs to have a quantized input, which can be done by input quantizer or returning
+    # a quantized tensor from the preceding layer
+    is_input_quant_tensor = kwargs['io_quant'] is not None or kwargs['input_quantized']
+    assert is_input_quant_tensor, "All A2Q models require quantized inputs."
+
+    # testing the forward pass
+    output = model(input)
+
+    # bit-width and sign need to come from the quant tensor of the preceding layer if no io_quant
+    if kwargs['io_quant'] is None:
+        input_bit_width = input.bit_width
+        input_is_signed = input.signed
+    # else bit-width and sign come from the io_quant
+    else:
+        input_bit_width = model.conv.quant_input_bit_width()
+        input_is_signed = model.conv.is_quant_input_signed
+
+    # if the input is not quantized already, the wrap it in a QuantTensor
+    if not kwargs['input_quantized']:
+        input = QuantTensor(
+            input,
+            None, # not used by weight_quant
+            None, # not used by weight_quant
+            input_bit_width,
+            input_is_signed,
+            None # note used by weight_quant
+        )
+    # the tensor quantizer requires a QuantTensor with specified bit-width and sign
+    quant_weight = model.conv.quant_weight(input)
+    quant_weight = quant_weight.int().float()
+    if kwargs['model_type'] == 'QuantLinear': # shape = (out_features, in_features)
+        quant_weight_per_channel_l1_norm = quant_weight.norm(p=1, dim=1)
+    elif kwargs['model_type'] == 'QuantConv1d': # shape = (out_channels, in_channels, kernel_size)
+        quant_weight_per_channel_l1_norm = quant_weight.norm(p=1, dim=(1,2))
+    elif kwargs['model_type'] == 'QuantConv2d': # shape = (out_channels, in_channels, kernel_size, kernel_size)
+        quant_weight_per_channel_l1_norm = quant_weight.norm(p=1, dim=(1,2,3))
+    elif kwargs['model_type'] == 'QuantConvTranspose1d': # shape = (in_channels, out_channels, kernel_size)
+        quant_weight_per_channel_l1_norm = quant_weight.norm(p=1, dim=(0,2))
+    elif kwargs['model_type'] == 'QuantConvTranspose2d': # shape = (in_channels, out_channels, kernel_size)
+        quant_weight_per_channel_l1_norm = quant_weight.norm(p=1, dim=(0,2,3))
+    else:
+        raise NotImplementedError(f"Check for {kwargs['model_type']} is not yet implemented.")
+
+    # using the closed-form bounds on accumulator bit-width
+    cur_acc_bit_width = calculate_min_accumulator_bit_width(input_bit_width, input_is_signed, quant_weight_per_channel_l1_norm.max())
+    exp_acc_bit_width = kwargs['accumulator_bit_width']
+    assert cur_acc_bit_width <= exp_acc_bit_width, \
+        f"Model does not satisfy accumulator bit-width bounds. Expected {exp_acc_bit_width}, got {cur_acc_bit_width}"
+
+    # might as well also check again if the output is a quant tensor if it is specified to be
+    if kwargs['return_quant_tensor']:
+        assert isinstance(output, QuantTensor)
+        assert output.scale is not None
+        assert output.bit_width is not None
+    else:
+        assert isinstance(output, torch.Tensor)
