fa2cc265f9366b4ed638e1f004bbd1e25e3f0b0a
Ian Colbert <ian.colbert@amd.com>
2023-04-23 20:22:47 +0100

    Adding training and qonnx export

--- src/brevitas_examples/super_resolution/train_model.py
+++ src/brevitas_examples/super_resolution/train_model.py
@@ -5,30 +5,31 @@ import argparse
 import configparser
 import os
 import random
+import math
 
 import torch
-import torch.backends.cudnn as cudnn
-import torch.nn.parallel
-import torch.optim
-import torch.utils.data
-import torch.utils.data.distributed
+import torch.nn as nn
+import torch.optim as optim
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
+from torch.utils.data import DataLoader
 from torch.utils.data.dataset import TensorDataset
 
-from brevitas_examples.super_resolution.models import model_with_cfg
+from brevitas_examples.super_resolution.models import get_model_by_name
 
 SEED = 123456
 
 parser = argparse.ArgumentParser(description='PyTorch BSD300 Validation')
 parser.add_argument('--data-dir', help='path to folder containing BSD300 val folder')
-parser.add_argument('--model', type=str, default='quant_espcn_v1_4b', help='Name of the model')
-parser.add_argument('--pretrained', action='store_true', help='Load pretrained checkpoint')
-parser.add_argument('--workers', default=4, type=int, help='number of data loading workers')
-parser.add_argument('--batch-size', default=256, type=int, help='Minibatch size')
+parser.add_argument('--model', type=str, default='quant_espcn_x3_v1_4b', help='Name of the model')
+parser.add_argument('--workers', default=0, type=int, help='number of data loading workers')
+parser.add_argument('--batch-size', default=16, type=int, help='Minibatch size')
 parser.add_argument('--gpu', default=None, type=int, help='GPU id to use.')
-parser.add_argument('--shuffle', action='store_true', help='Shuffle validation data.')
+parser.add_argument('--learning-rate', default=1e-3, help='Learning rate')
+parser.add_argument('--upscale-factor', default=3, help='Upscaling factor')
+parser.add_argument('--total-epochs', default=3, help='Total number of training epochs')
+parser.add_argument('--weight-decay', default=1e-5, help='Weight decay')
 
 
 def main():
@@ -36,30 +37,51 @@ def main():
     random.seed(SEED)
     torch.manual_seed(SEED)
 
-    model, cfg = model_with_cfg(args.model, args.pretrained)
+    model = get_model_by_name(args.model)
 
     if args.gpu is not None:
         torch.cuda.set_device(args.gpu)
         model = model.cuda(args.gpu)
-        cudnn.benchmark = True
 
-    print(model)
-
-    upsampling_factor = 3
-    x = torch.randn(1000, 1, 256, 256)
-    y = torch.randn(1000, 1, 256 * upsampling_factor, 256 * upsampling_factor)
+    # NOTE - toy dataset to be replaced with bsd300x3
+    transform = transforms.Resize(510 // args.upscale_factor)
+    y = torch.randn(50, 1, 510, 510)
+    x = transform(y)
     val_dataset = TensorDataset(x, y)
+    train_dataset = TensorDataset(x, y)
 
-    valdir = os.path.join(args.data_dir, 'val')
-    val_loader = torch.utils.data.DataLoader(
+    val_loader = DataLoader(
         val_dataset,
         batch_size=args.batch_size,
-        shuffle=args.shuffle,
+        shuffle=False,
+        num_workers=args.workers,
+        pin_memory=True)
+    
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
         num_workers=args.workers,
         pin_memory=True)
 
+    train(train_loader, model, args)
     validate(val_loader, model, args)
-    return
+    export(val_loader, model, args)
+
+
+def train(train_loader, model, args):
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(
+        model.parameters(),
+        lr=args.learning_rate,
+        weight_decay=args.weight_decay)
+    for ep in range(args.total_epochs):
+        for (images, targets) in train_loader:
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
 
 
 def validate(val_loader, model, args):
@@ -76,7 +98,11 @@ def validate(val_loader, model, args):
     avg_loss = tot_loss / len(val_loader.dataset)
     psnr = 10. * math.log10(1. / avg_loss)
     print(f"Average peak signal-to-noise ratio = {psnr:.3f}")
-    return
+
+
+def export(val_loader, model, args):
+    # TODO - qonnx export
+    pass
 
 
 if __name__ == '__main__':
