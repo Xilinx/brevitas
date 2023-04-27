d3cf12dde198fbcc0c144bae4a339a14a4e34265
icolbert <Ian.Colbert@amd.com>
2023-04-27 16:44:26 -0700

    Updating super resolution model benchmarks

--- src/brevitas_examples/super_resolution/models/__init__.py
+++ src/brevitas_examples/super_resolution/models/__init__.py
@@ -1,16 +1,20 @@
 # Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 
-import os
+import torch.nn as nn
 
 from .espcn import *
 
 model_impl = {
-    'quant_espcn_x3_v1_4b': quant_espcn_x3_v1_4b,
-    'quant_espcn_x3_v2_4b': quant_espcn_x3_v2_4b,
-    'quant_espcn_x3_v3_4b': quant_espcn_x3_v3_4b}
+    'float_espcn_x3': float_espcn_x3,
+    'quant_espcn_x3_w8a8': quant_espcn_x3_w8a8,
+    'quant_espcn_x3_w4a4': quant_espcn_x3_w4a4,
+    'quant_espcn_x3_finn_a2q_w4a4_14b': quant_espcn_x3_finn_a2q_w4a4_14b,
+    'quant_espcn_x3_finn_a2q_w4a4_32b': quant_espcn_x3_finn_a2q_w4a4_32b}
 
 
-def get_model_by_name(name):
+def get_model_by_name(name) -> nn.Module:
+    if name not in model_impl.keys():
+        raise NotImplementedError(f"{name} does not exist.")
     model = model_impl[name]()
     return model
--- src/brevitas_examples/super_resolution/models/espcn.py
+++ src/brevitas_examples/super_resolution/models/espcn.py
@@ -2,20 +2,23 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import torch
-import torch.nn as nn
-import torch.nn.init as init
 from torch import Tensor
+import torch.nn as nn
 
 import brevitas.nn as qnn
 
-from .common import QuantNearestNeighborConvolution
-from .common import CommonUintActQuant
 from .common import CommonIntWeightPerChannelQuant
+from .common import CommonUintActQuant
+from .common import Int8AccumulatorAwareWeightQuant
+from .common import Int8WeightPerTensorFloat
+from .common import QuantNearestNeighborConvolution
 
 __all__ = [
-    "quant_espcn_x3_v1_4b",
-    "quant_espcn_x3_v2_4b",
-    "quant_espcn_x3_v3_4b"]
+    "float_espcn_x3",
+    "quant_espcn_x3_w8a8",
+    "quant_espcn_x3_w4a4",
+    "quant_espcn_x3_finn_a2q_w4a4_14b",
+    "quant_espcn_x3_finn_a2q_w4a4_32b"]
 
 IO_BIT_WIDTH = 8
 
@@ -27,19 +30,43 @@ def weight_init(layer):
             layer.bias.data.zero_()
 
 
-class QuantESPCN(nn.Module):
-    """ Quantized Efficient Sub-Pixel Convolution Network (ESPCN) """
-    def __init__(
-        self,
-        upscale_factor: int = 3,
-        num_channels: int = 3,
-        weight_bit_width: int = 4,
-        act_bit_width: int = 4,
-        weight_quant = CommonIntWeightPerChannelQuant):
-        super(QuantESPCN, self).__init__()
+class ESPCN(nn.Module):
+    """Efficient Sub-Pixel Convolution Network (ESPCN) as proposed by Shi et al. (2016)
+    in `Real-time single image and video super-resolution using an efficient sub-pixel
+    convolutional neural network.`"""
 
+    def __init__(self, in_channels: int, upscale_factor: int):
+        super(ESPCN, self).__init__()
+        out_channels = in_channels * pow(upscale_factor, 2)
+        self.conv1 = nn.Conv2d(in_channels, 64, (5, 5), (1, 1), (2, 2))
+        self.conv2 = nn.Conv2d(64, 64, (3, 3), (1, 1), (1, 1))
+        self.conv3 = nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1))
+        self.conv4 = nn.Conv2d(32, out_channels, (3, 3), (1, 1), (1, 1), bias=False)
+        self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+        self.apply(weight_init)
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.conv3(x))
+        x = self.pixel_shuffle(self.conv4(x))
+        return x
+
+
+class QuantESPCN(ESPCN):
+    """Quantized ESPCN derived from floating-point ESPCN class"""
+
+    def __init__(
+            self,
+            in_channels: int,
+            upscale_factor: int,
+            act_bit_width: int,
+            weight_bit_width: int,
+            weight_quant):
+        super().__init__(in_channels, upscale_factor)
+        out_channels = in_channels * pow(upscale_factor, 2)
         self.conv1 = qnn.QuantConv2d(
-            in_channels=num_channels,
+            in_channels=in_channels,
             out_channels=64,
             kernel_size=5,
             stride=1,
@@ -70,6 +97,88 @@ class QuantESPCN(nn.Module):
             input_quant=CommonUintActQuant,
             weight_bit_width=weight_bit_width,
             weight_quant=weight_quant)
+        self.conv4 = qnn.QuantConv2d(
+            in_channels=32,
+            out_channels=out_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias=False,
+            input_bit_width=act_bit_width,
+            input_quant=CommonUintActQuant,
+            weight_bit_width=weight_bit_width,
+            weight_quant=weight_quant)
+
+
+def float_espcn_x3() -> ESPCN:
+    """Floating-point implementation of the efficient sub-pixel convolution network"""
+    return ESPCN(1, 3)
+
+
+def quant_espcn_x3_w8a8():
+    """4-bit integer quantized ESPCN model for BSD300 using common
+    integer weight quantization with per-tensor scales"""
+    return QuantESPCN(1, 3, 8, 8, Int8WeightPerTensorFloat)
+
+
+def quant_espcn_x3_w4a4():
+    """4-bit integer quantized ESPCN model for BSD300 using common
+    integer weight quantization with per-tensor scales"""
+    return QuantESPCN(1, 3, 4, 4, Int8WeightPerTensorFloat)
+
+
+class QuantESPCNV2(nn.Module):
+    """FINN-Friendly Quantized Efficient Sub-Pixel Convolution Network (ESPCN) as
+    proposed in Colbert et al. (2023) - `Quantized Neural Networks for Low-Precision
+    Accumulation with Guaranteed Overflow Avoidance`."""
+
+    def __init__(
+            self,
+            upscale_factor: int = 3,
+            num_channels: int = 3,
+            weight_bit_width: int = 4,
+            act_bit_width: int = 4,
+            acc_bit_width: int = 32,
+            weight_quant=CommonIntWeightPerChannelQuant):
+        super(QuantESPCNV2, self).__init__()
+
+        self.conv1 = qnn.QuantConv2d(
+            in_channels=num_channels,
+            out_channels=64,
+            kernel_size=5,
+            stride=1,
+            padding=2,
+            bias=True,
+            input_bit_width=IO_BIT_WIDTH,
+            input_quant=CommonUintActQuant,
+            weight_bit_width=weight_bit_width,
+            weight_accumulator_bit_width=acc_bit_width,
+            weight_quant=weight_quant)
+        self.conv2 = qnn.QuantConv2d(
+            in_channels=64,
+            out_channels=64,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias=True,
+            input_bit_width=act_bit_width,
+            input_quant=CommonUintActQuant,
+            weight_bit_width=weight_bit_width,
+            weight_accumulator_bit_width=acc_bit_width,
+            weight_quant=weight_quant)
+        self.conv3 = qnn.QuantConv2d(
+            in_channels=64,
+            out_channels=32,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            input_bit_width=act_bit_width,
+            input_quant=CommonUintActQuant,
+            weight_bit_width=weight_bit_width,
+            weight_accumulator_bit_width=acc_bit_width,
+            weight_quant=weight_quant)
+        # NOTE - not applying accumulator constraint to the final
+        # convolution layer
         self.conv4 = QuantNearestNeighborConvolution(
             in_channels=32,
             out_channels=num_channels,
@@ -88,14 +197,12 @@ class QuantESPCN(nn.Module):
         self.bn3 = nn.BatchNorm2d(32)
 
         self.relu = nn.ReLU(inplace=True)
-        self.out = qnn.QuantReLU(
-            act_quant=CommonUintActQuant,
-            bit_width=IO_BIT_WIDTH)
+        self.out = qnn.QuantReLU(act_quant=CommonUintActQuant, bit_width=IO_BIT_WIDTH)
 
         self.apply(weight_init)
 
     def forward(self, inp: Tensor):
-        x = torch.relu(inp) # Adding for finn-onnx compatability
+        x = torch.relu(inp)  # Adding for finn-onnx compatability
         x = self.relu(self.bn1(self.conv1(x)))
         x = self.relu(self.bn2(self.conv2(x)))
         x = self.relu(self.bn3(self.conv3(x)))
@@ -103,34 +210,25 @@ class QuantESPCN(nn.Module):
         return x
 
 
-def quant_espcn_v1(
-    num_channels: int,
-    upscale_factor: int,
-    weight_bit_width: int,
-    act_bit_width: int,
-    weight_quant) -> QuantESPCN:
-    model = QuantESPCN(
-        upscale_factor=upscale_factor,
-        num_channels=num_channels,
-        weight_bit_width=weight_bit_width,
-        act_bit_width=act_bit_width,
-        weight_quant=weight_quant)
-    return model
+def quant_espcn_x3_finn_a2q_w4a4_14b():
+    """4-bit integer quantized FINN-friendly ESPCN model for BSD300 using
+    integer accumulator-aware weight quantizer for 14-bit accumulator"""
+    return QuantESPCNV2(
+        upscale_factor=3,
+        num_channels=1,
+        act_bit_width=4,
+        acc_bit_width=14,
+        weight_bit_width=4,
+        weight_quant=Int8AccumulatorAwareWeightQuant)
 
 
-def quant_espcn_x3_v1_4b():
-    """4-bit integer quantized ESPCN model for BSD300 using common
-    integer weight quantization with per-channel scales"""
-    return quant_espcn_v1(1, 3, 4, 4, CommonIntWeightPerChannelQuant)
-
-
-def quant_espcn_x3_v2_4b():
-    """4-bit integer quantized ESPCN model for BSD300 using integer
-    weight normalization-based quantizer with L2-norm"""
-    return quant_espcn_v1(1, 3, 4, 4, Int8WeightNormL2PerChannelFixedPoint) 
-
-
-def quant_espcn_x3_v3_4b():
-    """4-bit integer quantized ESPCN model for BSD300 using integer
-    accumulator-aware weight quantizer"""
-    return quant_espcn_v1(1, 3, 4, 4, Int8AccumulatorAwareWeightQuant)
+def quant_espcn_x3_finn_a2q_w4a4_32b():
+    """4-bit integer quantized FINN-friendly ESPCN model for BSD300 using
+    integer accumulator-aware weight quantizer for 32-bit accumulator"""
+    return QuantESPCNV2(
+        upscale_factor=3,
+        num_channels=1,
+        act_bit_width=4,
+        acc_bit_width=32,
+        weight_bit_width=4,
+        weight_quant=Int8AccumulatorAwareWeightQuant)
