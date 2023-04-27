ff898c3df21aa24b31c4b85be50363dcf7752428
icolbert <Ian.Colbert@amd.com>
2023-04-27 16:43:02 -0700

    Creating utils for evaluating trained A2Q models

---
+++ src/brevitas_examples/super_resolution/utils/evaluate.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
+from torch import Tensor
+import torch.nn as nn
+
+import brevitas.nn as qnn
+from brevitas.nn.quant_layer import QuantWeightBiasInputOutputLayer as QuantWBIOL
+from brevitas.nn.utils import calculate_min_accumulator_bit_width
+
+
+def _calc_min_acc_bit_width(module: QuantWBIOL) -> Tensor:
+    # bit-width and sign need to come from the quant tensor of the preceding layer if no io_quant
+    input_bit_width = module.quant_input_bit_width()
+    input_is_signed = module.is_quant_input_signed
+
+    # the tensor quantizer requires a QuantTensor with specified bit-width and sign
+    quant_weight = module.quant_weight()
+    quant_weight = quant_weight.int().float()
+    if isinstance(module,
+                  qnn.QuantConv2d):  # shape = (out_channels, in_channels, kernel_size, kernel_size)
+        quant_weight_per_channel_l1_norm = quant_weight.norm(p=1, dim=(1, 2, 3))
+
+    # using the closed-form bounds on accumulator bit-width
+    cur_acc_bit_width = calculate_min_accumulator_bit_width(
+        input_bit_width, input_is_signed, quant_weight_per_channel_l1_norm.max())
+    return cur_acc_bit_width
+
+
+def evaluate_accumulator_bit_widths(model: nn.Module):
+    stats = dict()
+    for name, module in model.named_modules():
+        # ESPCN only has quantized conv2d nodes. Will check for more layer types
+        # in the future with other example models/architectures.
+        if isinstance(module, qnn.QuantConv2d):
+            acc_bit_width = _calc_min_acc_bit_width(module)
+            stats[name] = acc_bit_width.item()
+    return stats
