7724df187585675159c18a2d4c5b41cdea646b04
icolbert <Ian.Colbert@amd.com>
2023-04-27 16:43:52 -0700

    Updating shared quantizers

--- src/brevitas_examples/super_resolution/models/common.py
+++ src/brevitas_examples/super_resolution/models/common.py
@@ -3,19 +3,19 @@
 
 from typing import Optional
 
-import brevitas.nn as qnn
-import torch.nn as nn
 from torch import Tensor
+import torch.nn as nn
 
 from brevitas.core.restrict_val import RestrictValueType
+import brevitas.nn as qnn
 from brevitas.quant import Int8ActPerTensorFloat
 from brevitas.quant import Int8WeightPerTensorFloat
 from brevitas.quant import Uint8ActPerTensorFloat
-from brevitas.quant.fixed_point import Int8WeightNormL2PerChannelFixedPoint
 from brevitas.quant.fixed_point import Int8AccumulatorAwareWeightQuant
+from brevitas.quant.fixed_point import Int8WeightNormL2PerChannelFixedPoint
 
 
-class CommonIntWeightPerChannelQuant(CommonIntWeightPerTensorQuant):
+class CommonIntWeightPerChannelQuant(Int8WeightPerTensorFloat):
     """
     Common per-channel weight quantizer with bit-width set to None so that it's forced to be
     specified by each layer.
@@ -43,31 +43,26 @@ class CommonUintActQuant(Uint8ActPerTensorFloat):
 
 class QuantNearestNeighborConvolution(nn.Module):
     """Quantized nearest neighbor resize convolution"""
+
     def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        kernel_size: Optional[int] = 3,
-        stride: Optional[int] = 1,
-        padding: Optional[int] = 1,
-        upscale_factor: Optional[int] = 3,
-        signed_act: Optional[bool] = False,
-        bias: Optional[bool] = True,
-        weight_quant = CommonIntWeightPerChannelQuant,
-        act_bit_width: Optional[int] = 8,
-        weight_bit_width: Optional[int] = 8):
+            self,
+            in_channels: int,
+            out_channels: int,
+            kernel_size: Optional[int] = 3,
+            stride: Optional[int] = 1,
+            padding: Optional[int] = 1,
+            upscale_factor: Optional[int] = 3,
+            signed_act: Optional[bool] = False,
+            bias: Optional[bool] = True,
+            weight_quant=CommonIntWeightPerChannelQuant,
+            act_bit_width: Optional[int] = 8,
+            weight_bit_width: Optional[int] = 8):
         super().__init__()
 
         act_quant = CommonIntActQuant if signed_act else CommonUintActQuant
 
         self.upscale_factor = upscale_factor
-        self.input_quant = qnn.QuantIdentity(
-            act_quant=act_quant,
-            return_quant_tensor=True,
-            bit_width=act_bit_width)
-        self.interp = qnn.QuantUpsamplingNearest2d(
-            scale_factor=upscale_factor,
-            return_quant_tensor=True)
+        self.interp = nn.UpsamplingNearest2d(scale_factor=upscale_factor)
         self.conv = qnn.QuantConv2d(
             in_channels=in_channels,
             out_channels=out_channels,
@@ -75,8 +70,10 @@ class QuantNearestNeighborConvolution(nn.Module):
             padding=padding,
             stride=stride,
             bias=bias,
+            input_bit_width=act_bit_width,
+            input_quant=act_quant,
             weight_bit_width=weight_bit_width,
             weight_quant=weight_quant)
 
     def forward(self, inp: Tensor) -> Tensor:
-        return self.conv(self.interp(self.input_quant(inp)))
+        return self.conv(self.interp(inp))
