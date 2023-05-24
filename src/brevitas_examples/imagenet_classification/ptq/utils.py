df7d9d51ae9017be9a51f09166d4af903de340c7
Giuseppe Franco <giuseppefranco4@gmail.com>
2023-05-24 16:34:06 +0200

    Feat (examples/ptq): parallel benchmark execution (#596)
    * Feat (ptq): parallel benchmark exection
    
    * Update utils.py

--- src/brevitas_examples/imagenet_classification/ptq/utils.py
+++ src/brevitas_examples/imagenet_classification/ptq/utils.py
@@ -1,9 +1,15 @@
 # Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 
+from time import sleep
+import warnings
+
+import torch
 import torchvision
 
 from brevitas_examples.imagenet_classification import models
+
+TIME_BETWEEN_RETRIES = 2 * 60  # in seconds
 
 
 def get_model_config(model_name):
@@ -49,3 +55,48 @@ def add_bool_arg(parser, name, default, help, str_true=False):
         group.add_argument('--' + name, dest=dest, action='store_true', help='Enable ' + help)
     group.add_argument('--no-' + name, dest=dest, action='store_false', help='Disable ' + help)
     parser.set_defaults(**{dest: default})
+
+
+def get_gpu_index(idx):
+    gpu_world_size = torch.cuda.device_count()
+    if gpu_world_size == 0:
+        # If no GPU is available, execute on CPU
+        return None
+
+    gpu_id = idx % gpu_world_size
+    try:
+        # If GPUtil is instealled, fetch the next available GPU
+        return get_next_available_gpu(gpu_id)
+    except ModuleNotFoundError:
+        warnings.warn("GPUtil not installed, multiple jobs might end up on the same GPU")
+        # If GPUtil is not installed, cycle through GPUs
+        return gpu_id
+
+
+def get_next_available_gpu(gpu_id):
+    import GPUtil
+    GPUs = GPUtil.getGPUs()
+    available_gpus = GPUtil.getAvailability(
+        GPUs, maxLoad=0.5, maxMemory=0.5, includeNan=False, excludeID=[], excludeUUID=[])
+    if available_gpus[gpu_id] == 0:
+        next_available_gpu = GPUtil.getAvailable(
+            order='memory',
+            limit=3,
+            maxLoad=0.5,
+            maxMemory=0.5,
+            includeNan=False,
+            excludeID=[],
+            excludeUUID=[])
+        while len(next_available_gpu) == 0:
+            sleep(TIME_BETWEEN_RETRIES)
+            next_available_gpu = GPUtil.getAvailable(
+                order='memory',
+                limit=3,
+                maxLoad=0.5,
+                maxMemory=0.5,
+                includeNan=False,
+                excludeID=[],
+                excludeUUID=[])
+
+        gpu_id = next_available_gpu[0]
+    return gpu_id
