992d328cb8ad7cb089857344a9b8649c41d54e9b
Alessandro Pappalardo <alessand@xilinx.com>
2023-05-25 01:03:33 +0100

    Fixes for the backport of make_fx
    Signed-off-by: Alessandro Pappalardo <alessand@xilinx.com>

--- src/brevitas/backport/fx/config.py
+++ src/brevitas/backport/fx/config.py
@@ -41,27 +41,9 @@ POSSIBILITY OF SUCH DAMAGE.
 Forked as-is from PyTorch 2.0.1
 """
 
-from brevitas.backport.fx.proxy import Proxy
+# Whether to disable showing progress on compilation passes
+# Need to add a new config otherwise wil get a circular import if dynamo config is imported here
+disable_progress = True
 
-from ._compatibility import compatibility
-
-
-@compatibility(is_backward_compatible=False)
-def annotate(val, type):
-    # val could be either a regular value (not tracing)
-    # or fx.Proxy (tracing)
-    if isinstance(val, Proxy):
-        if val.node.type:
-            raise RuntimeError(
-                f"Tried to annotate a value that already had a type on it!"
-                f" Existing type is {val.node.type} "
-                f"and new type is {type}. "
-                f"This could happen if you tried to annotate a function parameter "
-                f"value (in which case you should use the type slot "
-                f"on the function signature) or you called "
-                f"annotate on the same value twice")
-        else:
-            val.node.type = type
-        return val
-    else:
-        return val
+# If True this also shows the node names in each pass, for small models this is great but larger models it's quite noisy
+verbose_progress = False
--- src/brevitas/backport/fx/traceback.py
+++ src/brevitas/backport/fx/traceback.py
@@ -41,27 +41,75 @@ POSSIBILITY OF SUCH DAMAGE.
 Forked as-is from PyTorch 2.0.1
 """
 
-from brevitas.backport.fx.proxy import Proxy
+from contextlib import contextmanager
+import traceback
+from typing import Any, Dict, List
 
 from ._compatibility import compatibility
 
+__all__ = [
+    'preserve_node_meta',
+    'has_preserved_node_meta',
+    'set_stack_trace',
+    'format_stack',
+    'set_current_meta',
+    'get_current_meta']
+
+current_meta: Dict[str, Any] = {}
+should_preserve_node_meta = False
+
 
 @compatibility(is_backward_compatible=False)
-def annotate(val, type):
-    # val could be either a regular value (not tracing)
-    # or fx.Proxy (tracing)
-    if isinstance(val, Proxy):
-        if val.node.type:
-            raise RuntimeError(
-                f"Tried to annotate a value that already had a type on it!"
-                f" Existing type is {val.node.type} "
-                f"and new type is {type}. "
-                f"This could happen if you tried to annotate a function parameter "
-                f"value (in which case you should use the type slot "
-                f"on the function signature) or you called "
-                f"annotate on the same value twice")
-        else:
-            val.node.type = type
-        return val
+@contextmanager
+def preserve_node_meta():
+    global should_preserve_node_meta
+
+    saved_should_preserve_node_meta = should_preserve_node_meta
+    try:
+        should_preserve_node_meta = True
+        yield
+    finally:
+        should_preserve_node_meta = saved_should_preserve_node_meta
+
+
+@compatibility(is_backward_compatible=False)
+def set_stack_trace(stack: List[str]):
+    global current_meta
+
+    if should_preserve_node_meta and stack:
+        current_meta["stack_trace"] = "".join(stack)
+
+
+@compatibility(is_backward_compatible=False)
+def format_stack() -> List[str]:
+    if should_preserve_node_meta:
+        return [current_meta.get("stack_trace", "")]
     else:
-        return val
+        # fallback to traceback.format_stack()
+        return traceback.format_list(traceback.extract_stack()[:-1])
+
+
+@compatibility(is_backward_compatible=False)
+def has_preserved_node_meta() -> bool:
+    return should_preserve_node_meta
+
+
+@compatibility(is_backward_compatible=False)
+@contextmanager
+def set_current_meta(meta: Dict[str, Any]):
+    global current_meta
+
+    if should_preserve_node_meta and meta:
+        saved_meta = current_meta
+        try:
+            current_meta = meta
+            yield
+        finally:
+            current_meta = saved_meta
+    else:
+        yield
+
+
+@compatibility(is_backward_compatible=False)
+def get_current_meta() -> Dict[str, Any]:
+    return current_meta.copy()
