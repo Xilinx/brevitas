4129b96c4b8fb9b7316ee66f2b21c96ee8dde1d2
Alessandro Pappalardo <alessand@xilinx.com>
2023-06-05 15:57:44 +0100

    Test act equalization support

---
+++ src/brevitas_examples/llm/llm_quant/equalize.py
@@ -0,0 +1,27 @@
+import torch
+
+from brevitas.graph.equalize import activation_equalization_mode
+from brevitas_examples.llm.llm_quant.run_utils import apply_layer_ptq_fn
+
+
+@torch.no_grad()
+def activation_equalization_iter(curr_layer, inps, outs, cached_values, alpha):
+    curr_layer = curr_layer.cuda()
+    with activation_equalization_mode(curr_layer, alpha, add_mul_node=True, layerwise=True):
+        for j in range(len(inps)):
+            inp = inps[j].unsqueeze(0).cuda()
+            curr_out = curr_layer(inp, **cached_values)[0]
+            outs[j] = curr_out
+    curr_layer.cpu()
+    return outs
+
+
+@torch.no_grad()
+def apply_act_equalization(model, dataloader, nsamples, seqlen=2048, alpha=0.5):
+    apply_layer_ptq_fn(
+        model,
+        dataloader,
+        nsamples,
+        inference_fn=activation_equalization_iter,
+        seqlen=seqlen,
+        alpha=alpha)
--- src/brevitas_examples/llm/run_quantization.py
+++ src/brevitas_examples/llm/run_quantization.py
@@ -8,6 +8,7 @@ from transformers import pipeline
 from brevitas_examples.llm.llm_quant.bias_corr import apply_bias_correction
 from brevitas_examples.llm.llm_quant.calibrate import apply_calibration
 from brevitas_examples.llm.llm_quant.data import get_c4
+from brevitas_examples.llm.llm_quant.equalize import apply_act_equalization
 from brevitas_examples.llm.llm_quant.eval import model_eval
 from brevitas_examples.llm.llm_quant.gptq import apply_gptq
 from brevitas_examples.llm.llm_quant.quantize import quantize_model
@@ -87,6 +88,10 @@ def main():
     parser.add_argument(
         '--act-calibration', action='store_true', help='Apply activation calibration.')
     parser.add_argument('--bias-corr', action='store_true', help='Apply bias correction.')
+    parser.add_argument(
+        '--act-equalization',
+        action='store_true',
+        help='Apply activation equalization (SmoothQuant).')
 
     args = parser.parse_args()
     set_seed(args.seed)
@@ -96,6 +101,16 @@ def main():
     model = AutoModelForCausalLM.from_pretrained(args.model, **kwargs)
     print("Model loaded.")
     model.eval()
+
+    print("Data loading...")
+    calibration_loader, val_data = get_c4(
+        nsamples=args.nsamples, seed=args.seed, model=args.model, seqlen=args.seqlen)
+    print("Data loaded.")
+
+    if args.act_equalization:
+        print("Apply act equalization (SmoothQuant)...")
+        apply_act_equalization(model, calibration_loader, args.nsamples)
+        print("Act equalization applied.")
 
     print("Applying model quantization...")
     quantize_model(
@@ -112,11 +127,6 @@ def main():
         input_scale_type=args.input_scale_type,
         input_quant_granularity=args.input_quant_granularity)
     print("Model quantization applied.")
-
-    print("Data loading...")
-    calibration_loader, val_data = get_c4(
-        nsamples=args.nsamples, seed=args.seed, model=args.model, seqlen=args.seqlen)
-    print("Data loaded.")
 
     if args.act_calibration:
         print("Apply act calibration...")
