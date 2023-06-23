e730b4fcec7bc9d8bf51a93d21f6c952e2264b37
Alessandro Pappalardo <alessand@xilinx.com>
2023-06-23 10:55:45 +0100

    Clean up weight eq support

--- src/brevitas_examples/llm/llm_quant/equalize.py
+++ src/brevitas_examples/llm/llm_quant/equalize.py
@@ -5,7 +5,9 @@ Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
 
 import torch
 
+from brevitas.fx.brevitas_tracer import value_trace
 from brevitas.graph.equalize import activation_equalization_mode
+from brevitas.graph.equalize import EqualizeGraph
 from brevitas_examples.llm.llm_quant.run_utils import apply_layer_ptq_fn
 
 
@@ -30,3 +32,9 @@ def apply_act_equalization(model, dataloader, nsamples, seqlen=2048, alpha=0.5):
         inference_fn=activation_equalization_iter,
         seqlen=seqlen,
         alpha=alpha)
+
+
+@torch.no_grad()
+def apply_weight_equalization(model, ref_kwargs, scale_computation_type='range'):
+    graph_model = value_trace(model, value_args=ref_kwargs)
+    EqualizeGraph(scale_computation_type=scale_computation_type).apply(graph_model)
