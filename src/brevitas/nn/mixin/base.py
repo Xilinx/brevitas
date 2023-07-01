914005edaddf773b86a486fa25a2126e0c914065
Giuseppe Franco <giuseppefranco4@gmail.com>
2023-07-01 21:37:03 +0000

    Correct handling of QuantTensor. No named tensors during tracing/export

--- src/brevitas/nn/mixin/base.py
+++ src/brevitas/nn/mixin/base.py
@@ -172,7 +172,8 @@ class QuantLayerMixin(ExportMixin):
                 cached_inp = _CachedIO(inp.detach(), self.cache_quant_io_metadata_only)
                 self._cached_inp = cached_inp
         # Remove any naming metadata to avoid dowmstream errors
-        inp.value.rename_(None)
+        if not torch._C._get_tracing_state():
+            inp.value.rename_(None)
         return inp
 
     def pack_output(self, quant_output: QuantTensor):
