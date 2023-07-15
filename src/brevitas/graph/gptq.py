c56cc5831d844eb18b1c55172fc8ede64ac5474c
Giuseppe Franco <giuseppefranco4@gmail.com>
2023-07-15 01:00:40 +0100

    Fix(gptq): fix for quant convtranspose1d/2d and conv1d

--- src/brevitas/graph/gptq.py
+++ src/brevitas/graph/gptq.py
@@ -190,7 +190,9 @@ class GPTQ():
 
         # By default, use groups = 1
         self.groups = 1
-        if isinstance(self.layer, qnn.QuantConv2d):
+        if isinstance(self.layer, SUPPORTED_CONV_OP):
+            if isinstance(self.layer, (qnn.QuantConvTranspose1d, qnn.QuantConvTranspose2d)):
+                weight = weight.transpose(1, 0)  # This performs a view
             weight = weight.flatten(1)
             self.groups = self.layer.groups
 
