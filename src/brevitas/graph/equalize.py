9a77649c7bbf15cec9e19273df6eba12e13c6f7e
Alessandro Pappalardo <alessand@xilinx.com>
2023-07-17 17:16:39 +0100

    fix syntax

--- src/brevitas/graph/equalize.py
+++ src/brevitas/graph/equalize.py
@@ -57,8 +57,8 @@ _scale_invariant_layers = (
     nn.AdaptiveAvgPool2d,
     nn.AdaptiveAvgPool3d,
     nn.Identity,
-    torch.nn.ReLU, 
-    torch.nn.LeakyReLU)
+    nn.ReLU, 
+    nn.LeakyReLU)
 
 _scale_invariant_op = (torch.mul, operator.mul, operator.imul, operator.__mul__, operator.__imul__)
 
