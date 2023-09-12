e4750ea82406c0151b542ca644506f66cc4c8ad4
Giuseppe Franco <giuseppefranco4@gmail.com>
2023-09-12 11:16:16 +0100

    Fix (test): ignore hypothesis health check

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
 # Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 
+import hypothesis
 import torch
 
 SEED = 123456
+DIFFERING_EXECUTOR_ENUM = 10  # Disabled since it fails with normal test setup
+
 torch.random.manual_seed(SEED)
+hypothesis.settings(suppress_health_check=hypothesis.HealthCheck(DIFFERING_EXECUTOR_ENUM))
