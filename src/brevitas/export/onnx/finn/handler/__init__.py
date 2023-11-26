de069f31c18e72dd1d7ee7cda5bd3f3fe87a3d55
Giuseppe Franco <giuseppefranco4@gmail.com>
2023-11-26 22:26:11 +0000

    Feat (export): deprecate FINN ONNX

--- src/brevitas/export/onnx/finn/handler/__init__.py
+++
@@ -1,2 +0,0 @@
-# Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
--- src/brevitas/onnx/__init__.py
+++ src/brevitas/onnx/__init__.py
@@ -5,7 +5,6 @@
 
 from brevitas.export import enable_debug  # noqa
 from brevitas.export import export_brevitas_onnx  # noqa
-from brevitas.export import export_finn_onnx  # noqa
 from brevitas.export import export_onnx_qcdq  # noqa
 from brevitas.export import export_onnx_qop  # noqa
 from brevitas.export import export_qonnx  # noqa
