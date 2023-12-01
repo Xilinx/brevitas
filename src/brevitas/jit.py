58936c495fd6e7f0514f84b8fb15d13c4bc2d7c2
Giuseppe Franco <giuseppefranco4@gmail.com>
2023-12-01 17:56:22 +0100

    Fix (jit): remove Injector patcher (#752)

--- src/brevitas/jit.py
+++ src/brevitas/jit.py
@@ -1,12 +1,9 @@
 # Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from packaging import version
 import torch
 
 from brevitas.config import JIT_ENABLED
-
-IS_ABOVE_110 = version.parse(torch.__version__) > version.parse('1.1.0')
 
 
 def _disabled(fn):
@@ -20,15 +17,9 @@ if JIT_ENABLED:
     ScriptModule = torch.jit.ScriptModule
     Attribute = torch.jit.Attribute
 
-    if not IS_ABOVE_110:
-        script_method_110_disabled = _disabled
-    else:
-        script_method_110_disabled = script_method
-
 else:
 
     script_method = _disabled
     script = _disabled
-    script_method_110_disabled = _disabled
     ScriptModule = torch.nn.Module
     Attribute = lambda val, type: val
