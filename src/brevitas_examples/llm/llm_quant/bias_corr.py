65360f4e01d6d80fb58e997bae9cb48fa1e316fd
nickfraser <icanlosh@gmail.com>
2024-02-09 09:48:33 +0000

    Fix (examples): typo in bias correction (#835)

--- src/brevitas_examples/llm/llm_quant/bias_corr.py
+++ src/brevitas_examples/llm/llm_quant/bias_corr.py
@@ -10,6 +10,6 @@ from brevitas.graph.calibrate import bias_correction_mode
 
 @torch.no_grad()
 def apply_bias_correction(model, dataloader):
-    with bias_correction_mode(curr_layer):
+    with bias_correction_mode(model):
         for inps in dataloader:
             model(**inps)
