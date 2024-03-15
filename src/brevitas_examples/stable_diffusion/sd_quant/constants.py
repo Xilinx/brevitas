3036b4d1c7b9774fc90a9ccfc06ac739a7c86dbe
Giuseppe Franco <giuseppefranco4@gmail.com>
2024-03-15 07:49:46 +0000

    Feat (examples): add support for Stable Diffusion XL

--- src/brevitas_examples/stable_diffusion/sd_quant/constants.py
+++ src/brevitas_examples/stable_diffusion/sd_quant/constants.py
@@ -4,3 +4,4 @@ SPDX-License-Identifier: MIT
 """
 
 SD_2_1_EMBEDDINGS_SHAPE = (77, 1024)
+SD_XL_EMBEDDINGS_SHAPE = (77, 2048)
--- src/brevitas_examples/stable_diffusion/sd_quant/export.py
+++ src/brevitas_examples/stable_diffusion/sd_quant/export.py
@@ -26,30 +26,8 @@ class UnetExportWrapper(nn.Module):
         return self.unet(*args, **kwargs, return_dict=False)
 
 
-def export_torchscript(pipe, trace_inputs, output_dir, export_manager):
-    with torch.no_grad(), brevitas_proxy_export_mode(pipe.unet, export_manager):
-        fx_g = make_fx(
-            UnetExportWrapper(pipe.unet),
-            decomposition_table=get_decompositions([
-                torch.ops.aten.embedding_dense_backward,
-                torch.ops.aten.native_layer_norm_backward,
-                torch.ops.aten.slice_backward,
-                torch.ops.aten.select_backward,
-                torch.ops.aten.norm.ScalarOpt_dim,
-                torch.ops.aten.native_group_norm,
-                torch.ops.aten.upsample_bilinear2d.vec,
-                torch.ops.aten.split.Tensor,
-                torch.ops.aten.split_with_sizes,]),
-        )(*tuple(trace_inputs.values()))
-        _force_requires_grad_false(fx_g)
-        jit_g = torch.jit.trace(_JitTraceExportWrapper(fx_g), tuple(trace_inputs.values()))
-        output_path = os.path.join(output_dir, 'unet.ts')
-        print(f"Saving unet to {output_path} ...")
-        torch.jit.save(jit_g, output_path)
-
-
 def export_onnx(pipe, trace_inputs, output_dir, export_manager):
     output_path = os.path.join(output_dir, 'unet.onnx')
     print(f"Saving unet to {output_path} ...")
     with torch.no_grad(), brevitas_proxy_export_mode(pipe.unet, export_manager):
-        torch.onnx.export(pipe.unet, args=tuple(trace_inputs.values()), f=output_path)
+        torch.onnx.export(pipe.unet, args=trace_inputs, f=output_path)
