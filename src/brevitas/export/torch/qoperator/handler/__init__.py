3364a92bc972b0f9c7dcdf6d0e1b5ff408004fd6
Timothy Costigan <156176839+costigt-dev@users.noreply.github.com>
2024-03-27 13:53:27 +0000

    Feat (export): Remove QOP Export (#917)

--- src/brevitas/export/torch/qoperator/handler/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-
-try:
-    import torch.nn.quantized.functional as qF
-
-# Skip for pytorch 1.1.0
-except:
-    qF = None
