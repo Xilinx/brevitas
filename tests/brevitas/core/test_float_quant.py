386c7c92fa742e9860948b1958cee8ad25c96dcf
Giuseppe Franco <giuseppefranco4@gmail.com>
2024-05-06 13:21:02 +0100

    Fix (tests): disable JIT with Mock

--- tests/brevitas/core/test_float_quant.py
+++ tests/brevitas/core/test_float_quant.py
@@ -61,6 +61,7 @@ def test_minifloat(minifloat_format):
 
 
 @given(inp=float_tensor_random_shape_st(), minifloat_format=random_minifloat_format())
+@jit_disabled_for_mock()
 def test_float_to_quant_float(inp, minifloat_format):
     bit_width, exponent_bit_width, mantissa_bit_width, signed, exponent_bias = minifloat_format
 
