975c9ee4e987e0862769bf9878b7bd0299578f52
Nick Fraser <icanlosh@gmail.com>
2024-06-26 10:15:50 +0100

    Fix (example/sdxl): print output directory.

--- src/brevitas_examples/stable_diffusion/main.py
+++ src/brevitas_examples/stable_diffusion/main.py
@@ -159,6 +159,7 @@ def main(args):
     str_ts = ts.strftime("%Y%m%d_%H%M%S")
     output_dir = os.path.join(args.output_path, f'{str_ts}')
     os.mkdir(output_dir)
+    print(f"Saving results in {output_dir}")
 
     # Dump args to json
     with open(os.path.join(output_dir, 'args.json'), 'w') as fp:
