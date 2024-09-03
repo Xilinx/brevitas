Xilinx/brevitas

commit c096d7b06e23505d4a39b195a032f0e937a21596

Fix (nn/conv): Fixed conversion of convolutions when `padding_mode='same'` (#1017)

src/brevitas/nn/quant_conv.py
@@ -46,7 +46,7 @@ class QuantConv1d(QuantWBIOL, Conv1d):
             dtype: Optional[torch.dtype] = None,
             **kwargs) -> None:
         # avoid an init error in the super class by setting padding to 0
-        if padding_mode == 'zeros' and padding == 'same' and stride > 1:
+        if padding_mode == 'zeros' and padding == 'same' and any(map(lambda x: x > 1, stride)):
             padding = 0
             is_same_padded_strided = True
         else:
@@ -132,7 +132,7 @@ class QuantConv2d(QuantWBIOL, Conv2d):
             dtype: Optional[torch.dtype] = None,
             **kwargs) -> None:
         # avoid an init error in the super class by setting padding to 0
-        if padding_mode == 'zeros' and padding == 'same' and stride > 1:
+        if padding_mode == 'zeros' and padding == 'same' and any(map(lambda x: x > 1, stride)):
             padding = 0
             is_same_padded_strided = True
         else:
@@ -220,7 +220,7 @@ class QuantConv3d(QuantWBIOL, Conv3d):
             dtype: Optional[torch.dtype] = None,
             **kwargs) -> None:
         # avoid an init error in the super class by setting padding to 0
-        if padding_mode == 'zeros' and padding == 'same' and stride > 1:
+        if padding_mode == 'zeros' and padding == 'same' and any(map(lambda x: x > 1, stride)):
             padding = 0
             is_same_padded_strided = True
         else:

commit 3887f3b8036a1de0f8e17acbae0103344058d121

fix (nn/conv): Fix regression introduced in #1017

src/brevitas/nn/quant_conv.py
@@ -46,7 +46,7 @@ class QuantConv1d(QuantWBIOL, Conv1d):
             dtype: Optional[torch.dtype] = None,
             **kwargs) -> None:
         # avoid an init error in the super class by setting padding to 0
-        if padding_mode == 'zeros' and padding == 'same' and any(map(lambda x: x > 1, stride)):
+        if padding_mode == 'zeros' and padding == 'same' and any(map(lambda x: x > 1, list(stride))):
             padding = 0
             is_same_padded_strided = True
         else:
@@ -132,7 +132,7 @@ class QuantConv2d(QuantWBIOL, Conv2d):
             dtype: Optional[torch.dtype] = None,
             **kwargs) -> None:
         # avoid an init error in the super class by setting padding to 0
-        if padding_mode == 'zeros' and padding == 'same' and any(map(lambda x: x > 1, stride)):
+        if padding_mode == 'zeros' and padding == 'same' and any(map(lambda x: x > 1, list(stride))):
             padding = 0
             is_same_padded_strided = True
         else:
@@ -220,7 +220,7 @@ class QuantConv3d(QuantWBIOL, Conv3d):
             dtype: Optional[torch.dtype] = None,
             **kwargs) -> None:
         # avoid an init error in the super class by setting padding to 0
-        if padding_mode == 'zeros' and padding == 'same' and any(map(lambda x: x > 1, stride)):
+        if padding_mode == 'zeros' and padding == 'same' and any(map(lambda x: x > 1, list(stride))):
             padding = 0
             is_same_padded_strided = True
         else: