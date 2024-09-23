5902061d89a4a73af1ac30c4c4adc723dedfe38a
Giuseppe Franco <giuseppefranco4@gmail.com>
2024-09-23 18:35:51 +0100

    fix

--- tests/brevitas/core/test_float_quant.py
+++ tests/brevitas/core/test_float_quant.py
@@ -99,7 +99,7 @@ def test_float_to_quant_float(inp, minifloat_format):
             float_clamp_impl=float_clamp)
         expected_out, *_ = float_quant(inp)
         scale = float_quant.scaling_impl(inp)
-        out_quant = float_quant.quantize(inp, scale)
+        out_quant, scale = float_quant.quantize(inp, scale)
         exponent_bit_width, mantissa_bit_width, exponent_bias  = torch.tensor(exponent_bit_width, dtype=torch.float), torch.tensor(mantissa_bit_width, dtype=torch.float), torch.tensor(exponent_bias, dtype=torch.float)
         out_quant, *_ = float_quant.float_clamp_impl(
             out_quant, exponent_bit_width, mantissa_bit_width, exponent_bias)
