5d5dfcec8e8b25e6af9875767072eb5aabc5c52e
Giuseppe Franco <giuseppefranco4@gmail.com>
2024-09-24 07:30:04 +0100

    Fix

--- src/brevitas/core/quant/float.py
+++ src/brevitas/core/quant/float.py
@@ -94,6 +94,6 @@ class FloatQuant(brevitas.jit.ScriptModule):
             # after quantizing, clamp to special cases like NaN/inf if they are set
             y, saturating, inf_values, nan_values = self.float_clamp_impl(
                 y, self.exponent_bit_width(), self.mantissa_bit_width(), self.exponent_bias())
-        y = self.dequantize(y, scale)
+            y = self.dequantize(y, scale)
         # This is to respect the current interface of proxies
         return y, scale, self.zero_point_impl(), self.exponent_bit_width(), self.mantissa_bit_width(), self.exponent_bias(), saturating, inf_values, nan_values
