475bd7f242a956252c2bf54d754bd63e401ef46a
Giuseppe Franco <giuseppefranco4@gmail.com>
2024-10-26 22:48:56 +0200

    Clean-up

--- src/brevitas/quant_tensor/float_quant_tensor.py
+++ src/brevitas/quant_tensor/float_quant_tensor.py
@@ -103,9 +103,9 @@ class FloatQuantTensor(FloatQuantTensorBase, QuantTensor):
             scale = self.scale.type(torch.float32)
         minifloat_value = value / scale
         fp_internal_scale = 1. - self.exponent_bias - self.mantissa_bit_width
-        eps = torch.finfo(self.scale.dtype).tiny
+        eps = torch.finfo(scale.dtype).tiny
         int_scale = float_internal_scale(
-            self.value / self.scale, self.mantissa_bit_width, fp_internal_scale, eps)
+            minifloat_value, self.mantissa_bit_width, fp_internal_scale, eps)
         minifloat_value = minifloat_value / int_scale
         return minifloat_value
 
@@ -138,12 +138,17 @@ class FloatQuantTensor(FloatQuantTensorBase, QuantTensor):
     def minifloat(self, float_datatype=True):
         # TODO: Check if OCP and cast to proper data-type if matching
         assert float_datatype, "Minifloat quant returns only higher precision dtype"
-
         if self.is_valid:
+            value = self.value
+            scale = self.scale
+            if self.scale.dtype == torch.bfloat16:
+                value = self.value.type(torch.float32)
+                scale = self.scale.type(torch.float32)
+            minifloat_value = value / scale
             fp_internal_scale = 1. - self.exponent_bias - self.mantissa_bit_width
-            eps = torch.finfo(self.scale.dtype).tiny
+            eps = torch.finfo(scale.dtype).tiny
             int_scale = float_internal_scale(
-                self.value / self.scale, self.mantissa_bit_width, fp_internal_scale, eps)
+                minifloat_value, self.mantissa_bit_width, fp_internal_scale, eps)
             float_value = torch.round(self._pre_round_float_value) * int_scale
             return float_value.type(self.scale.dtype)
         else:
