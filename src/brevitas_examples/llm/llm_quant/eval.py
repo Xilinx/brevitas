8f584a1f9d1886fddb6bd64249ee4314c9d7566c
Giuseppe Franco <giuseppefranco4@gmail.com>
2024-11-18 08:22:38 +0000

    fix import

--- src/brevitas_examples/llm/llm_quant/eval.py
+++ src/brevitas_examples/llm/llm_quant/eval.py
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from random import random
+import random
 from typing import Any, Dict, Iterable, List, Union
 
 import numpy as np
