c7d55fa343b499337c90b750f435b8a7928527d8
Nick Fraser <icanlosh@gmail.com>
2025-01-27 11:16:05 +0000

    Fix (solver/trunc): Added a ShiftRoundSaturate quantizer and update trunc solver

--- src/brevitas/quant/solver/trunc.py
+++ src/brevitas/quant/solver/trunc.py
@@ -2,12 +2,17 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from brevitas.core.quant import TruncIntQuant
+from brevitas.core.scaling import TruncMsbScaling
+from brevitas.core.scaling import TruncPowerOfTwoIntScaling
+from brevitas.core.scaling import TruncScalingWrapper
 from brevitas.inject import ExtendedInjector
 from brevitas.inject import value
 from brevitas.inject.enum import QuantType
+from brevitas.inject.enum import RestrictValueType
+from brevitas.inject.enum import TruncScalingImplType
 from brevitas.proxy import TruncQuantProxyFromInjector
-from brevitas.quant.solver.common import SolveBitWidthImplFromEnum
-from brevitas.quant.solver.common import SolveTensorQuantFloatToIntImplFromEnum
+from brevitas.quant.solver.act import *
+from brevitas.quant.solver.common import *
 
 
 class SolveTruncTensorQuantFromEnum(ExtendedInjector):
@@ -26,9 +31,44 @@ class SolveTruncTensorQuantFromEnum(ExtendedInjector):
             raise RuntimeError(f'{quant_type} not recognized.')
 
 
+class SolveTruncScalingImplFromEnum(ExtendedInjector):
+
+    @value
+    def trunc_scaling_impl(trunc_scaling_impl_type="msb"):
+        if trunc_scaling_impl_type == TruncScalingImplType.MSB:
+            return TruncMsbScaling
+        elif trunc_scaling_impl_type == TruncScalingImplType.WRAPPER:
+            return TruncScalingWrapper
+        else:
+            raise RuntimeError(f'trunc_scaling_impl_type={trunc_scaling_impl_type} not recognized.')
+
+
+class SolveTruncIntScalingImplFromEnum(ExtendedInjector):
+
+    @value
+    def trunc_int_scaling_impl(restrict_scaling_type):
+        if restrict_scaling_type == RestrictValueType.POWER_OF_TWO:
+            return TruncPowerOfTwoIntScaling
+        else:
+            raise RuntimeError(f'restrict_scaling_type={restrict_scaling_type} not recognized.')
+
+
 class TruncQuantSolver(SolveBitWidthImplFromEnum,
                        SolveTensorQuantFloatToIntImplFromEnum,
-                       SolveTruncTensorQuantFromEnum):
+                       SolveActScalingImplFromEnum,
+                       SolveIntScalingImplFromEnum,
+                       SolveScalingStatsOpFromEnum,
+                       SolveRestrictScalingImplFromEnum,
+                       SolveActScalingInitFromEnum,
+                       SolveStatsReduceDimFromEnum,
+                       SolveActScalingShape,
+                       SolveScalingStatsInputViewShapeImplFromEnum,
+                       SolveActScalingPerOutputChannelShape,
+                       SolveUpdateStateDictImplFromEnum,
+                       SolveInputViewImpl,
+                       SolveTruncTensorQuantFromEnum,
+                       SolveTruncScalingImplFromEnum,
+                       SolveTruncIntScalingImplFromEnum):
     """
     Translate enum directives to truncation-specific quantization core modules.
     It should be placed last in the list of classes a quantizer inherits from,
