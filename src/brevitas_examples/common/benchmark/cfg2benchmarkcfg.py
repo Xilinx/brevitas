fa709d482779cc75b942acc9a3d064b5b9b0dae4
Nick Fraser <icanlosh@gmail.com>
2025-02-13 15:09:49 +0000

    Feat (example/benchmark): Added script to convert YAML cfgs to "benchmark" configs

---
+++ src/brevitas_examples/common/benchmark/cfg2benchmarkcfg.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
+from argparse import ArgumentParser
+import yaml
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--config',
+        type=str,
+        default=None,
+        required=True,
+        help=
+        'Specify a configuration file (YAML) to use as a template for a benchmark config'
+    )
+    parser.add_argument(
+        '--benchmark-config',
+        type=str,
+        default=None,
+        required=True,
+        help=
+        'Specify the filename of the output benchmark config template'
+    )
+    args = parser.parse_args()
+    with open(args.config, 'r') as f:
+        defaults = yaml.safe_load(f)
+    benchmark_defaults = {}
+    for k in defaults.keys():
+        benchmark_defaults[k] = [defaults[k]]
+    with open(args.benchmark_config, 'w') as f:
+        yaml.dump(benchmark_defaults, f)
