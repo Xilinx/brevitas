ed8c13702e55bcf4692e83f0c484d800cd742c10
Giuseppe Franco <giuseppefranco4@gmail.com>
2025-03-10 10:21:45 +0000

    Per Row quantizer

--- tests/brevitas/proxy/test_compile.py
+++ tests/brevitas/proxy/test_compile.py
@@ -16,9 +16,16 @@ from brevitas.quant.experimental.mx_quant_ocp import MXFloat8e4m3Weight
 from brevitas.quant.experimental.mx_quant_ocp import MXInt8Act
 from brevitas.quant.experimental.mx_quant_ocp import MXInt8Weight
 from brevitas_examples.common.generative.quantize import Int8DynamicActPerTensorFloat
-from brevitas_examples.common.generative.quantizers import Fp8e4m3DynamicActPerGroupFloat
+from brevitas_examples.common.generative.quantizers import FP8e4m3OCPDynamicActPerRowFloat
 from tests.brevitas.hyp_helper import float_tensor_st
 from tests.marker import requires_pt_ge
+
+
+class Fp8PerRow(FP8e4m3OCPDynamicActPerRowFloat):
+    dynamic_scaling_broadcastable_fn = lambda x, shape: x.view(*shape[:-1], 1)
+    permute_dims = None
+    stats_reduce_dim = 1
+
 
 WEIGHT_QUANTIZERS = {
     'int8': Int8WeightPerTensorFloat,
@@ -32,7 +39,7 @@ ACT_QUANTIZERS = {
     'uint8': ShiftedUint8ActPerTensorFloat,
     'fp8': Fp8e4m3ActPerTensorFloat,
     'per_tensor_dynamic_int8': Int8DynamicActPerTensorFloat,
-    'per_group_dynamic_fp8': Fp8e4m3DynamicActPerGroupFloat,
+    'per_row_dynamic_fp8': Fp8PerRow,
     'mxint8': MXInt8Act,
     'mxfloat8': MXFloat8e4m3Act}
 
