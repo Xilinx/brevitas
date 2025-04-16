Fix (scaling/float): correct dtype for threshold

src/brevitas/core/scaling/float_scaling.py
@@ -25,6 +25,7 @@ class FloatScaling(brevitas.jit.ScriptModule):
         self.inf_values = inf_values
         self.nan_values = nan_values
         self.saturating = saturating
+        self.dtype = dtype
 
         if max_available_float:
             max_available_float = torch.tensor(max_available_float, device=device, dtype=dtype)
@@ -36,7 +37,8 @@ class FloatScaling(brevitas.jit.ScriptModule):
     def forward(
             self, exponent_bit_width: Tensor, pre_compute_max_mantissa: Tensor,
             exponent_bias: Tensor) -> Tensor:
-        max_value = max_float(exponent_bit_width, pre_compute_max_mantissa, exponent_bias)
+        max_value = max_float(exponent_bit_width, pre_compute_max_mantissa,
+                              exponent_bias).to(self.dtype)
         max_value = max_value if self.max_available_float is None else torch.min(
             max_value, self.max_available_float())
         return max_value