9a7475034591e24950aa104b7ba15ff6ed05899c
Nick Fraser <icanlosh@gmail.com>
2025-05-07 10:33:52 +0100

    test (ex/llm): Enabled eval for run args

--- tests/brevitas_examples/test_llm.py
+++ tests/brevitas_examples/test_llm.py
@@ -160,7 +160,7 @@ def default_run_args(request):
     args.seqlen = 2
     args.model = "hf-internal-testing/tiny-random-MistralForCausalLM"
     args.dataset = "c4"
-    args.eval = False
+    args.eval = True
     #args.checkpoint = ptid2pathname(request.node.nodeid) + ".pth" # Example filename which won't clash
     args.export_prefix = ptid2pathname(request.node.nodeid)
     args.weight_bit_width = 8
@@ -169,13 +169,6 @@ def default_run_args(request):
     args.act_calibration = True
     args.dtype = "float32"
     args.no_bos_preprocessing = True
-    return args
-
-
-@pytest_cases.fixture()
-def default_ppl_args(default_run_args):
-    args = default_run_args
-    args.eval = True
     return args
 
 
@@ -296,8 +289,8 @@ def test_small_models_toggle_run_args(caplog, toggle_run_args, small_models_with
             "quant_sdpa": True,
             "float_ppl": 52994.4296875,
             "quant_ppl": 52116.3359375},])
-def acc_args_and_acc(default_ppl_args, request):
-    args = default_ppl_args
+def acc_args_and_acc(default_run_args, request):
+    args = default_run_args
     run_dict = request.param
     float_ppl = run_dict["float_ppl"]
     quant_ppl = run_dict["quant_ppl"]
@@ -804,8 +797,8 @@ def test_small_models_torch_export(caplog, torch_export_args):
             "quant_ppl": 36821.664},])
             "float_ppl": 35292.54296875,
             "quant_ppl": 35014.25390625},])
-def learned_round_ppl_args_and_ppl(default_ppl_args, request):
-    args = default_ppl_args
+def learned_round_ppl_args_and_ppl(default_run_args, request):
+    args = default_run_args
     run_dict = request.param
     float_ppl = run_dict["float_ppl"]
     quant_ppl = run_dict["quant_ppl"]
@@ -901,8 +894,8 @@ def test_small_models_learned_round_ppl(caplog, learned_round_ppl_args_and_ppl):
             "rotation_layers_to_expand": ["down_proj"],
             "float_ppl": 32428.475,
             "quant_ppl": 32515.525,},])
-def rotation_ppl_args_and_ppl(default_ppl_args, request):
-    args = default_ppl_args
+def rotation_ppl_args_and_ppl(default_run_args, request):
+    args = default_run_args
     run_dict = request.param
     float_ppl = run_dict["float_ppl"]
     quant_ppl = run_dict["quant_ppl"]
@@ -1044,8 +1037,8 @@ def test_small_models_rotation_ppl(caplog, rotation_ppl_args_and_ppl):
                 "<class 'torch.nn.utils.parametrize.ParametrizedLinear'>": 1,
                 "<class 'torch.nn.utils.parametrize.ParametrizedEmbedding'>": 1,
                 "<class 'torch.nn.utils.parametrize.ParametrizedQuantLinear'>": 14,}},])
-def rotation_optimization_args_layer_count_and_ppl(default_ppl_args, request):
-    args = default_ppl_args
+def rotation_optimization_args_layer_count_and_ppl(default_run_args, request):
+    args = default_run_args
     run_dict = copy.deepcopy(request.param)
     extra_args = run_dict["extra_args"]
     float_ppl = run_dict["float_ppl"]
