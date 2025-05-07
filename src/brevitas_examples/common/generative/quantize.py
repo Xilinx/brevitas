c59bae13e48284aa201416794c6b46f9e3960261
Pablo Monteagudo Lago <44771380+pablomlago@users.noreply.github.com>
2025-05-07 20:32:19 +0100

    Update zero point condition

--- src/brevitas_examples/common/generative/quantize.py
+++ src/brevitas_examples/common/generative/quantize.py
@@ -382,7 +382,7 @@ def generate_quantizers(
     weight_quant = weight_quant.let(scaling_impl_type=weight_scaling_impl_type)
     # weight zero-point is converted to a standalone parameter
     # This is done already by default in the per_group quantizer
-    if weight_quant_type == 'asym' and weight_quant_granularity != 'per_group':
+    if weight_quant_type == 'asym' and weight_scaling_impl_type == 'parameter_from_stats':
         weight_quant = weight_quant.let(zero_point_impl=ParameterFromStatsFromParameterZeroPoint)
 
     if quant_attn_mode == 'sdpa':
