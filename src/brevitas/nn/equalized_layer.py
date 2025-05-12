22f691300329be819e659de05e4748b306a715e9
Giuseppe Franco <giuseppefranco4@gmail.com>
2025-05-12 13:05:49 +0100

    typo

--- src/brevitas/nn/equalized_layer.py
+++ src/brevitas/nn/equalized_layer.py
@@ -61,7 +61,7 @@ class RotatedModule(torch.nn.Module):
             had_mat=None,
             k=None,
             expansion_step=1,
-            expand_region: bool = False) -> None:
+            expand_input: bool = False) -> None:
         super().__init__()
         if had_mat is not None:
             self.had_mat = torch.nn.Parameter(had_mat).cpu()
@@ -70,11 +70,11 @@ class RotatedModule(torch.nn.Module):
         self.layer = layer
         self.k = k
         self.expansion_step = expansion_step
-        self.expand_region = expand_region
+        self.expand_input = expand_input
 
     def forward(self, inp, **kwargs):
         is_cuda = 'cuda' in str(inp.device) and torch.version.cuda is not None
-        if self.expansion_region:
+        if self.expand_input:
             # TODO: This only works for Linear layers. We have an assert in equalize.py to check for this
             featured_dim = inp.dim() - 1
             num_features = inp.shape[-1]
