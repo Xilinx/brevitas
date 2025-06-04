3db3e0d1bf8fa97875286f41acd5e95c645b95a8
Giuseppe Franco <giuseppefranco4@gmail.com>
2025-06-04 21:10:21 +0100

    update .bat file

--- docsrc/make.bat
+++ docsrc/make.bat
@@ -1,46 +1,60 @@
 @ECHO OFF
 
-pushd %~dp0
-
-REM Command file for Sphinx documentation
-
-if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-multiversion
-)
-set SOURCEDIR=source
-set BUILDDIR=build
-
-if "%1" == "" goto help
-
-if "%1" == "github" (
-    %SPHINXBUILD% -M html %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
-    robocopy %BUILDDIR%/html ../docs /E > nul
-    echo.Generated files copied to ../docs
-    goto end
-)
-
-if "%1" == "livehtml" (
-    sphinx-autobuild %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
-)
-
-%SPHINXBUILD% >NUL 2>NUL
-if errorlevel 9009 (
-	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to PATH.
-	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.http://sphinx-doc.org/
-	exit /b 1
-)
-
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
-goto end
-
-:help
-%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
-
-:end
-popd
+:: -----------------------------------------------------------------  
+:: Default variables (can be overridden in the environment)  
+:: -----------------------------------------------------------------  
+IF NOT DEFINED SPHINXOPTS    SET "SPHINXOPTS="  
+IF NOT DEFINED SPHINXBUILD   SET "SPHINXBUILD=sphinx-build"  
+IF NOT DEFINED MULTIVERSION  SET "MULTIVERSION=sphinx-multiversion"  
+IF NOT DEFINED SOURCEDIR     SET "SOURCEDIR=source"  
+IF NOT DEFINED BUILDDIR      SET "BUILDDIR=build"  
+IF NOT DEFINED BUILD_VERSION SET "BUILD_VERSION="  
+  
+:: -----------------------------------------------------------------  
+:: Dispatch to the selected target  
+:: -----------------------------------------------------------------  
+IF "%~1"==""        GOTO :help  
+IF /I "%~1"=="help" GOTO :help  
+IF /I "%~1"=="html" GOTO :html  
+IF /I "%~1"=="github" GOTO :github  
+IF /I "%~1"=="livehtml" GOTO :livehtml  
+  
+ECHO Unknown target "%~1" 1>&2  
+GOTO :help  
+  
+:: -----------------------------------------------------------------  
+:help  
+ECHO Available targets:  
+ECHO   html       Build documentation for all versions using sphinx-multiversion  
+ECHO   github     Build and copy HTML docs to ..\docs  
+ECHO   livehtml   Live-reload documentation (single version, not multiversion)  
+ECHO   help       Show this message  
+GOTO :EOF  
+  
+:: -----------------------------------------------------------------  
+:html  
+REM Propagate BUILD_VERSION to the child process via %VERSION%  
+SET "VERSION=%BUILD_VERSION%"  
+%MULTIVERSION% "%SOURCEDIR%" "%BUILDDIR%/html" %SPHINXOPTS%  
+GOTO :EOF  
+  
+:: -----------------------------------------------------------------  
+:github  
+REM First build the documentation (same as :html)  
+CALL :html  
+  
+REM Copy the generated HTML to ..\docs
+IF NOT EXIST "..\docs" MKDIR "..\docs"  
+XCOPY "%BUILDDIR%\html\*" "..\docs\" /E /I /Y >NUL  
+  
+REM Copy the top-level index.html from the source tree  
+COPY "%SOURCEDIR%\index.html" "..\docs\" >NUL  
+  
+REM Create .nojekyll markers 
+TYPE NUL > "..\docs\.nojekyll"  
+GOTO :EOF  
+  
+:: -----------------------------------------------------------------  
+:livehtml  
+sphinx-autobuild "%SOURCEDIR%" "%BUILDDIR%/livehtml" %SPHINXOPTS%  
+GOTO :EOF  
