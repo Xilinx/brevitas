65df0b87b4b40c5f591e0fcf8cc225ed78f73384
Giuseppe Franco <giuseppefranco4@gmail.com>
2025-06-04 11:12:34 +0100

    fix

--- src/brevitas/graph/hadamard.py
+++ src/brevitas/graph/hadamard.py
@@ -82,13 +82,14 @@ def find_closest_hadamard_number(starting_dim, steps=0):
     values_to_check = [172, 156, 152, 140, 108, 60, 52, 40, 36, 28, 20, 12]
 
     for step in range(steps):
+        best_value = None
         next_dim = starting_dim + 1
         for v in values_to_check:
             m = torch.tensor(math.ceil(next_dim / v))
             floor_po2_m = torch.pow(2, torch.log2(m).floor())
             ceil_po2_m = torch.pow(2, torch.log2(m).ceil())
             m = floor_po2_m * v if floor_po2_m > starting_dim else ceil_po2_m * v
-            if best_value == starting_dim:
+            if best_value is None:
                 best_value = m
             else:
                 best_value = m if (
