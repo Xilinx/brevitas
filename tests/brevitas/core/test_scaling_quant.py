e9f73496867e3b1f38315481fac41ade23f8c975
Ian Colbert <ian.colbert@amd.com>
2025-06-23 21:46:32 -0700

    Feat (tests): adding affine rescaled weight quantizer

--- tests/brevitas/core/test_scaling_quant.py
+++ tests/brevitas/core/test_scaling_quant.py
@@ -1,5 +1,9 @@
+# Copyright (C) 2025, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
 from dependencies import this
 from dependencies import value
+import pytest
 import torch
 
 from brevitas.core.quant.int import RescalingIntQuant
@@ -7,11 +11,13 @@ from brevitas.core.restrict_val import QuantRestrictValue
 from brevitas.core.stats.stats_wrapper import SCALAR_SHAPE
 from brevitas.core.zero_point import _ScaleShiftQuantZeroPoint
 from brevitas.inject.enum import ScalingPerOutputType
+from brevitas.nn import QuantLinear
 import brevitas.nn as qnn
 from brevitas.proxy.groupwise_int_parameter_quant import GroupwiseWeightQuantProxyFromInjector
 from brevitas.quant.base import ExtendedInjector
 from brevitas.quant.base import FloatRestrictValue
 from brevitas.quant.scaled_int import Int8WeightPerTensorFloat
+from brevitas.quant.shifted_scaled_int import ShiftedUint8WeightPerChannelFloat
 from brevitas.quant.shifted_scaled_int import ShiftedUint8WeightPerTensorFloat
 
 ZP_BIT_WIDTH = 6
@@ -129,3 +135,63 @@ def test_quant_scale():
             module.register_forward_pre_hook(hook_zp)
 
     linear(torch.randn(1, 512))
+
+
+class Uint8ScaledMinMaxPerChannelFloat(ShiftedUint8WeightPerChannelFloat):
+    """
+    Integer weight quantizer with per-channel scaling factors and zero-points that
+    map to a scaled min-max grid. The zero-point is calculated via NegativeMinOrZero,
+    the scale factor is calculated via AbsMinMax and normalized via 2^{bit_width} - 1
+    """
+    quantize_zero_point = False
+    scaling_affine_rescaling_init = 0.9
+    zero_point_affine_rescaling_init = this.scaling_affine_rescaling_init
+
+
+class Uint8ScaledMinMaxPerTensorFloat(Uint8ScaledMinMaxPerChannelFloat,
+                                      ShiftedUint8WeightPerTensorFloat):
+    pass
+
+
+scaled_weight_quant_map = {
+    "per_channel": Uint8ScaledMinMaxPerChannelFloat, "per_tensor": Uint8ScaledMinMaxPerTensorFloat}
+
+
+@pytest.mark.parametrize(
+    "scaled_weight_quant", scaled_weight_quant_map.values(), ids=scaled_weight_quant_map.keys())
+@pytest.mark.parametrize("quantize_zero_point", [True, False])
+@pytest.mark.parametrize("affine_rescaling_init", [0.9, 0.5])
+def test_affine_rescaling_per_channel(
+        scaled_weight_quant, quantize_zero_point, affine_rescaling_init, request):
+
+    test_id = request.node.callspec.id
+
+    torch.manual_seed(0)
+
+    scaled_weight_quant = scaled_weight_quant.let(quantize_zero_point=quantize_zero_point)
+    scaled_weight_quant = scaled_weight_quant.let(
+        scaling_affine_rescaling_init=affine_rescaling_init)
+    scaled_weight_quant = scaled_weight_quant.let(
+        zero_point_affine_rescaling_init=affine_rescaling_init)
+
+    quant_layer = QuantLinear(256, 32, weight_quant=scaled_weight_quant)
+    quant_layer.weight.data = torch.randn(32, 256)
+    quant_tensor = quant_layer.quant_weight()
+
+    w = quant_layer.weight.detach()
+    q = quant_tensor.value.detach()
+
+    if not quantize_zero_point:
+        z = (quant_tensor.zero_point * quant_tensor.scale).squeeze()
+        m = -w.min(dim=1).values  # NegativeMinOrZero
+        assert torch.allclose((z / m) - affine_rescaling_init, torch.zeros_like(m), atol=1e-3)
+
+    if "per_channel" in test_id:
+        q_range = q.max(dim=1).values - q.min(dim=1).values  # range of quant values
+        w_range = w.max(dim=1).values - w.min(dim=1).values  # range of float values
+    else:
+        q_range = q.max() - q.min()  # range of quant values
+        w_range = w.max() - w.min()  # range of float values
+    assert torch.allclose((q_range / w_range) - affine_rescaling_init,
+                          torch.zeros_like(q_range),
+                          atol=1e-3)
