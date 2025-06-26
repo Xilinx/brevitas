26cd0e72a483377193cd13d8d5563ae352bb58a1
Nick Fraser <icanlosh@gmail.com>
2025-06-26 14:55:06 +0100

    Tests (export/qonnx): Updated all tests to run with dynamo and torchscript

--- tests/brevitas_finn/brevitas/test_debug_export.py
+++ tests/brevitas_finn/brevitas/test_debug_export.py
@@ -4,17 +4,22 @@
 import torch
 
 from brevitas.export import enable_debug
-from brevitas.export import export_qonnx
 from brevitas_examples import bnn_pynq
+
+from ...export_fixture import qonnx_export_fn
+from ...export_fixture import rm_onnx
+
 
 REF_MODEL = 'CNV_2W2A'
 
 
-def test_debug_finn_onnx_export():
+def test_debug_finn_onnx_export(request, qonnx_export_fn):
     model, cfg = bnn_pynq.model_with_cfg(REF_MODEL, pretrained=False)
     model.eval()
     debug_hook = enable_debug(model)
     input_tensor = torch.randn(1, 3, 32, 32)
-    export_qonnx(model, input_t=input_tensor, export_path='finn_debug.onnx')
+    outfile = f'finn_debug_{request.node.callspec.id}.onnx'
+    qonnx_export_fn(model, input_t=input_tensor, export_path=outfile)
     model(input_tensor)
     assert debug_hook.values
+    rm_onnx(outfile)
--- tests/export_fixture.py
+++ tests/export_fixture.py
@@ -4,16 +4,27 @@
 from functools import partial
 import os
 
+from packaging.version import parse
 from pytest_cases import fixture
 from pytest_cases import parametrize
 
+from brevitas import torch_version
 from brevitas.export import export_qonnx
 
+def _get_qonnx_export_modes():
+    if parse("2.4") < torch_version:
+        export_fns = [export_qonnx]
+        export_ids = ["torchscript"]
+    else:
+        export_fns = [export_qonnx, partial(export_qonnx, dynamo=True, optimize=True)]
+        export_ids = ["torchscript", "dynamo"]
+    return export_fns, export_ids
+_qonnx_export_fns, _qonnx_export_ids = _get_qonnx_export_modes()
 
 @fixture
 @parametrize(
-    'export_fn', [export_qonnx, partial(export_qonnx, dynamo=True, optimize=True)],
-    ids=["torchscript", "dynamo"])
+    'export_fn', _qonnx_export_fns,
+    ids=_qonnx_export_ids)
 def qonnx_export_fn(export_fn):
     yield export_fn
 
