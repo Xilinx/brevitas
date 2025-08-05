Fix (core/scaling): handle edge cases with signed scale

src/brevitas/core/restrict_val.py
@@ -30,7 +30,8 @@ class _RestrictClampValue(brevitas.jit.ScriptModule):
     def __init__(
             self,
             scaling_min_val: Optional[float] = None,
-            restrict_value_impl: Optional[Module] = None):
+            restrict_value_impl: Optional[Module] = None,
+            is_unsigned=True):
         super(_RestrictClampValue, self).__init__()
         if scaling_min_val is not None and scaling_min_val != 0:
             self.clamp_min_ste = ScalarSignedClampMinSte(scaling_min_val)
@@ -41,8 +42,14 @@ class _RestrictClampValue(brevitas.jit.ScriptModule):
         else:
             self.restrict_value_impl = Identity()
 
+        if is_unsigned:
+            self.apply_abs = torch.abs
+        else:
+            self.apply_abs = Identity()
+
     @brevitas.jit.script_method
     def forward(self, x: Tensor):
+        x = self.apply_abs(x)
         x = self.restrict_value_impl(x)
         x = self.clamp_min_ste(x)
         return x

src/brevitas/core/scaling/standalone.py
@@ -13,6 +13,7 @@ from torch.nn import Parameter
 
 import brevitas
 import brevitas.config as config
+from brevitas.core.function_wrapper import Identity
 from brevitas.core.function_wrapper import OverBatchOverTensorView
 from brevitas.core.function_wrapper import TensorClamp
 from brevitas.core.restrict_val import _ClampValue
@@ -140,6 +141,7 @@ class ParameterScaling(brevitas.jit.ScriptModule):
     def __init__(
             self,
             scaling_init: Union[float, Tensor],
+            is_scale_unsigned: bool,
             scaling_shape: Optional[Tuple[int, ...]] = None,
             restrict_scaling_impl: Module = FloatRestrictValue(),
             restrict_threshold_impl: Optional[Module] = None,
@@ -167,7 +169,8 @@ class ParameterScaling(brevitas.jit.ScriptModule):
         if scaling_init.shape == SCALAR_SHAPE and scaling_shape is not None:
             scaling_init = torch.full(scaling_shape, scaling_init, dtype=dtype, device=device)
         self.value = Parameter(scaling_init)
-        self.restrict_clamp_scaling = _RestrictClampValue(scaling_min_val, restrict_scaling_impl)
+        self.restrict_clamp_scaling = _RestrictClampValue(
+            scaling_min_val, restrict_scaling_impl, is_scale_unsigned)
         self.restrict_clamp_threshold = _RestrictClampValue(
             restrict_value_impl=restrict_threshold_impl)
         self.restrict_threshold_pre = restrict_threshold_impl.restrict_init_module()
@@ -209,6 +212,7 @@ class ParameterFromStatsFromParameterScaling(brevitas.jit.ScriptModule):
             scaling_stats_input_concat_dim: int,
             tracked_parameter_list: List[torch.nn.Parameter],
             scaling_shape: Tuple[int, ...],
+            is_scale_unsigned: bool,
             force_parameter: bool = False,
             restrict_scaling_impl: Module = FloatRestrictValue(),
             restrict_threshold_impl: Optional[Module] = None,
@@ -238,7 +242,8 @@ class ParameterFromStatsFromParameterScaling(brevitas.jit.ScriptModule):
             scaling_affine_rescaling_init,
             scaling_affine_shifting_init,
             dtype,
-            device)
+            device,
+            is_scale_unsigned=is_scale_unsigned)
         self.restrict_threshold_pre = restrict_threshold_impl.restrict_init_module()
         self.restrict_inplace_scaling_pre = restrict_scaling_impl.restrict_init_inplace_module()
         self.clamp_scaling = _ClampValue(scaling_min_val)
@@ -348,6 +353,7 @@ class ParameterFromRuntimeStatsScaling(brevitas.jit.ScriptModule):
             self,
             collect_stats_steps: int,
             scaling_stats_impl: Module,
+            is_scale_unsigned: bool,
             scaling_stats_input_view_shape_impl: Module = OverBatchOverTensorView(),
             scaling_shape: Tuple[int, ...] = SCALAR_SHAPE,
             restrict_scaling_impl: Module = FloatRestrictValue(),
@@ -371,6 +377,10 @@ class ParameterFromRuntimeStatsScaling(brevitas.jit.ScriptModule):
             scaling_stats_momentum, Optional[float])
         self.register_buffer('buffer', torch.full(scaling_shape, 1.0, dtype=dtype, device=device))
         self.value = Parameter(torch.full(scaling_shape, 1.0, dtype=dtype, device=device))
+        if is_scale_unsigned:
+            self.apply_abs = torch.abs
+        else:
+            self.apply_abs = Identity()
         self.restrict_scaling = _RestrictValue(restrict_scaling_impl)
         self.restrict_threshold = _RestrictValue(restrict_threshold_impl)
         self.clamp_scaling = _ClampValue(scaling_min_val)
@@ -433,6 +443,7 @@ class ParameterFromRuntimeStatsScaling(brevitas.jit.ScriptModule):
             else:
                 out = self.value
             threshold = self.restrict_threshold(self.restrict_threshold_pre(threshold))
+            out = self.apply_abs(out)
             out = self.restrict_scaling(out)
             out = out / threshold
             # We can clamp after restrict val since the learned parameter is already in log-domain

src/brevitas/core/stats/stats_op.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 
+from abc import ABCMeta
 import math
 from typing import Optional
 from typing import Tuple
@@ -22,6 +23,20 @@ from brevitas.utils.torch_utils import kthvalue
 from .stats_wrapper import SCALAR_SHAPE
 
 DEFAULT_STD_DEV_EPSILON = 1e-8
+
+
+class UnsignedScaleStat(ABCMeta):
+
+    @classmethod
+    def is_scale_unsigned(cls):
+        True
+
+
+class SignedScaleStats(ABCMeta):
+
+    @classmethod
+    def is_scale_unsigned(cls):
+        False
 
 
 class NegativeMinOrZero(brevitas.jit.ScriptModule):
@@ -48,7 +63,7 @@ class NegativeMinOrZero(brevitas.jit.ScriptModule):
         return min_val
 
 
-class AbsPercentile(brevitas.jit.ScriptModule):
+class AbsPercentile(brevitas.jit.ScriptModule, UnsignedScaleStat):
     __constants__ = ['q', 'stats_reduce_dim', 'keepdim']
 
     def __init__(
@@ -116,7 +131,7 @@ class NegativePercentileOrZero(brevitas.jit.ScriptModule):
         return result
 
 
-class PercentileInterval(brevitas.jit.ScriptModule):
+class PercentileInterval(brevitas.jit.ScriptModule, UnsignedScaleStat):
     __constants__ = ['stats_reduce_dim', 'low_q', 'high_q', 'keepdim']
 
     def __init__(
@@ -159,7 +174,7 @@ class PercentileInterval(brevitas.jit.ScriptModule):
         return abs_interval
 
 
-class AbsMax(brevitas.jit.ScriptModule):
+class AbsMax(brevitas.jit.ScriptModule, UnsignedScaleStat):
     __constants__ = ['stats_reduce_dim']
 
     def __init__(self, stats_reduce_dim: Optional[int] = None, keepdim: bool = False) -> None:
@@ -175,7 +190,7 @@ class AbsMax(brevitas.jit.ScriptModule):
             return torch.max(torch.abs(x), dim=self.stats_reduce_dim, keepdim=self.keepdim)[0]
 
 
-class AbsMinMax(brevitas.jit.ScriptModule):
+class AbsMinMax(brevitas.jit.ScriptModule, UnsignedScaleStat):
     __constants__ = ['stats_reduce_dim', 'keepdim']
 
     def __init__(
@@ -202,7 +217,7 @@ class AbsMinMax(brevitas.jit.ScriptModule):
         return torch.abs(max_val - min_val)
 
 
-class AbsMaxAve(brevitas.jit.ScriptModule):
+class AbsMaxAve(brevitas.jit.ScriptModule, UnsignedScaleStat):
     __constants__ = ['stats_reduce_dim']
 
     def __init__(self, stats_reduce_dim: int) -> None:
@@ -214,7 +229,7 @@ class AbsMaxAve(brevitas.jit.ScriptModule):
         return torch.mean(torch.max(torch.abs(x), dim=self.stats_reduce_dim)[0])
 
 
-class AbsMaxL2(brevitas.jit.ScriptModule):
+class AbsMaxL2(brevitas.jit.ScriptModule, UnsignedScaleStat):
     __constants__ = ['stats_reduce_dim']
 
     def __init__(self, stats_reduce_dim: int) -> None:
@@ -229,7 +244,7 @@ class AbsMaxL2(brevitas.jit.ScriptModule):
         return out
 
 
-class AbsAve(brevitas.jit.ScriptModule):
+class AbsAve(brevitas.jit.ScriptModule, UnsignedScaleStat):
     __constants__ = ['stats_reduce_dim']
 
     def __init__(self, stats_reduce_dim: Optional[int] = None) -> None:
@@ -244,7 +259,7 @@ class AbsAve(brevitas.jit.ScriptModule):
             return torch.mean(torch.abs(x), dim=self.stats_reduce_dim)
 
 
-class MeanSigmaStd(brevitas.jit.ScriptModule):
+class MeanSigmaStd(brevitas.jit.ScriptModule, UnsignedScaleStat):
 
     def __init__(
             self,
@@ -289,7 +304,7 @@ class _MeanSigmaStdImpl(brevitas.jit.ScriptModule):
         return mean_val + sigma * std_val
 
 
-class MeanLearnedSigmaStd(brevitas.jit.ScriptModule):
+class MeanLearnedSigmaStd(brevitas.jit.ScriptModule, UnsignedScaleStat):
 
     def __init__(
             self,