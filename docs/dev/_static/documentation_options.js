28f1757d63a8cf09edc40fc356eeb9019cef8203
Giuseppe Franco <giuseppefranco4@gmail.com>
2025-08-07 12:06:30 +0100

    Rebuilt docs

--- docs/dev/_static/documentation_options.js
+++ docs/dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.12.1.dev20+g085e7846.d20250613',
+    VERSION: '0.12.1.dev61+g7e60590e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- docs/v0.10.0/_static/documentation_options.js
+++ docs/v0.10.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.12.1.dev10+g56c32c9c.d20250514',
+    VERSION: '0.12.1.dev61+g7e60590e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
