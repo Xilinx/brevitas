7f905e2853c5b1a429a3dfecd19bb7ccccd5bafa
Giuseppe Franco <giuseppefranco4@gmail.com>
2025-08-21 17:28:33 +0100

    fix

--- src/brevitas_examples/papers/expansion/benchmark.py
+++ src/brevitas_examples/papers/expansion/benchmark.py
@@ -9,7 +9,6 @@ class ExpansionBenchmark(LLMBenchmarkUtils):
     @staticmethod
     def validate(args, extra_args=None):
         super(LLMBenchmarkUtils, ExpansionBenchmark).validate(args, extra_args)
-        assert args.weight_group_size == args.input_group_size
         if len(args.rotation_layers_to_expand) == 0:
             assert args.expansion_step == 1
 
