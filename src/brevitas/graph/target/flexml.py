10196c3f03a41713b54b69131a15ee2048697356
Suraj Karki <suraj.karki@uni-bielefeld.de>
2025-11-02 11:03:20 +0100

    Feat (graph/flexml): add QuantHardSwish support for FlexML target
    Signed-off-by: Suraj Karki <suraj.karki@uni-bielefeld.de>

--- src/brevitas/graph/target/flexml.py
+++ src/brevitas/graph/target/flexml.py
@@ -114,6 +114,11 @@ FLEXML_QUANT_ACT_MAP = {
             'max_val': lambda module: module.max_val,
             'min_val': lambda module: module.min_val,
             'return_quant_tensor': True}),
+    nn.Hardswish: (
+        qnn.QuantHardSwish,
+        {
+            'act_quant': Uint8ActPerTensorFixedPoint,
+            'return_quant_tensor': True}),
     nn.Sigmoid: (
         qnn.QuantSigmoid, {
             'act_quant': Uint8ActPerTensorFixedPoint,
