38f160c910351ea4383fd19f50d1be4404847a2b
Nick Fraser <icanlosh@gmail.com>
2025-12-02 13:05:45 +0000

    Style (ex/llm/benchmark): remove unused imports

--- src/brevitas_examples/llm/benchmark/llm_rand_benchmark.py
+++ src/brevitas_examples/llm/benchmark/llm_rand_benchmark.py
@@ -1,22 +1,10 @@
 # Copyright (C) 2025, Advanced Micro Devices, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from argparse import ArgumentParser
-from argparse import Namespace
-import re
-import sys
-from typing import Any
-from typing import Dict
-from typing import List
-from typing import Optional
-from typing import Tuple
-
 from llm_benchmark import LLMBenchmarkUtilsMixin
 
 from brevitas_examples.common.benchmark.utils import benchmark
 from brevitas_examples.common.benchmark.utils import RandomSearchBenchmarkUtils
-from brevitas_examples.llm.llm_args import create_args_parser as create_llm_args_parser
-from brevitas_examples.llm.llm_args import validate as validate_llm_args
 
 
 class LLMRandomSearchBenchmarkUtils(RandomSearchBenchmarkUtils, LLMBenchmarkUtilsMixin):
