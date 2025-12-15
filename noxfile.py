7569d73e2af648d23b385f42c0fc0f78aea61592
Giuseppe Franco <giuseppefranco4@gmail.com>
2025-12-15 13:35:23 +0000

    fix fix marker

--- noxfile.py
+++ noxfile.py
@@ -148,7 +148,7 @@ def tests_brevitas_examples_llm(session, pytorch, jit_status):
     cmd += install_pytorch_cmd(pytorch)
     cmd += install_torchvision_cmd(pytorch)  # Optimum seems to require torchvision
     session.install('-e', '.[test, llm, export]', *cmd)
-    session.run('pytest', '-n', 'logical', '-k', 'llm', 'tests/brevitas_examples/test_llm.py')
+    session.run('pytest', '-n', 'logical', '-m', 'llm', 'tests/brevitas_examples/test_llm.py')
 
 
 @nox.session(python=PYTHON_VERSIONS)
@@ -162,7 +162,7 @@ def tests_brevitas_examples_llm_export(session, pytorch, jit_status):
     cmd += install_torchvision_cmd(pytorch)  # Optimum seems to require torchvision
     session.install('-e', '.[test, llm, export]', 'optimum[onnxruntime]', *cmd)
     session.run(
-        'pytest', '-n', 'logical', '-k', 'onnx_export', 'tests/brevitas_examples/test_llm.py')
+        'pytest', '-n', 'logical', '-m', 'onnx_export', 'tests/brevitas_examples/test_llm.py')
 
 
 @nox.session(python=PYTHON_VERSIONS)
@@ -176,7 +176,7 @@ def tests_brevitas_examples_llm_lighteval(session, pytorch, jit_status):
     cmd += install_torchvision_cmd(pytorch)  # Optim um seems to require torchvision
 
     session.install('-e', '.[test, llm, export]', *cmd, 'lighteval[math]')
-    session.run('pytest', '-n', 'logical', '-k', 'lighteval', 'tests/brevitas_examples/test_llm.py')
+    session.run('pytest', '-n', 'logical', '-m', 'lighteval', 'tests/brevitas_examples/test_llm.py')
 
 
 @nox.session(python=PYTHON_VERSIONS)
@@ -190,7 +190,7 @@ def tests_brevitas_examples_llm_lm_eval(session, pytorch, jit_status):
     cmd += install_torchvision_cmd(pytorch)  # Optim um seems to require torchvision
 
     session.install('-e', '.[test, llm, export]', *cmd, 'lm_eval')
-    session.run('pytest', '-n', 'logical', '-k', 'lm_eval', 'tests/brevitas_examples/test_llm.py')
+    session.run('pytest', '-n', 'logical', '-m', 'lm_eval', 'tests/brevitas_examples/test_llm.py')
 
 
 @nox.session(python=PYTHON_VERSIONS)
