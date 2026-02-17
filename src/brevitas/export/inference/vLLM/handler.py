4bfc1eba9c0b7a14f39130be1a808b4b9ab121b3
Giuseppe Franco <giuseppefranco4@gmail.com>
2026-02-17 11:30:32 +0000

    temp

--- src/brevitas/export/inference/vLLM/handler.py
+++ src/brevitas/export/inference/vLLM/handler.py
@@ -13,7 +13,7 @@ from ..handler import GroupwiseFloatInferenceHandler
 from ..handler import GroupwiseFloatWeightInferenceHandler
 from ..handler import IntInferencetHandler
 from ..handler import IntWeightInferencetHandler
-
+from vllm.config import ModelConfig
 class_mapping = {
     'GroupwiseFloatInferenceHandler': GroupwiseFloatInferenceHandler,
     'GroupwiseFloatWeightInferenceHandler': GroupwiseFloatWeightInferenceHandler,
@@ -103,70 +103,91 @@ class QuantLinear(LinearMethodBase):
         params_dtype: torch.dtype,
         **extra_weight_attrs,
     ):
+        from vllm.model_executor.parameter import ModelWeightParameter
+        breakpoint()
+        weight_loader = extra_weight_attrs.get("weight_loader")
+        self.input_size_per_partition = input_size_per_partition
+        self.output_partition_sizes = output_partition_sizes
         out_per_partition = sum(output_partition_sizes)
         w = torch.empty(
             (out_per_partition, input_size_per_partition),
             device="cuda",
             dtype=params_dtype,
         )
+        weight = ModelWeightParameter(
+            data=torch.empty(
+                sum(output_partition_sizes),
+                input_size_per_partition,
+                dtype=torch.float8_e4m3fn,
+            ),
+            input_dim=1,
+            output_dim=0,
+            weight_loader=weight_loader,
+             )
+        layer.register_parameter("weight", weight)
+        # layer.weight = torch.nn.Parameter(w, requires_grad=False)
 
-        layer.weight = torch.nn.Parameter(w, requires_grad=False)
+        # # Handling the packed weights for loading
+        # base_loader = extra_weight_attrs.get("weight_loader", None)
 
-        # Handling the packed weights for loading
-        base_loader = extra_weight_attrs.get("weight_loader", None)
+        # def packed_weight_loader(param, loaded_weight, loaded_shard_id=None, *args, **kwargs):
 
-        def packed_weight_loader(param, loaded_weight, loaded_shard_id=None, *args, **kwargs):
+        #     if loaded_shard_id is not None:
+        #         if isinstance(loaded_shard_id, int):
+        #             _loaded_shard_id = loaded_shard_id
+        #         else:
+        #             if loaded_shard_id == "q":
+        #                 _loaded_shard_id = 0
+        #             elif loaded_shard_id == "k":
+        #                 _loaded_shard_id = 1
+        #             elif loaded_shard_id == "v":
+        #                 _loaded_shard_id = 2
+        #             else:
+        #                 raise ValueError(f"Invalid loaded_shard_id: {loaded_shard_id}")
 
-            if loaded_shard_id is not None:
-                if isinstance(loaded_shard_id, int):
-                    _loaded_shard_id = loaded_shard_id
-                else:
-                    if loaded_shard_id == "q":
-                        _loaded_shard_id = 0
-                    elif loaded_shard_id == "k":
-                        _loaded_shard_id = 1
-                    elif loaded_shard_id == "v":
-                        _loaded_shard_id = 2
-                    else:
-                        raise ValueError(f"Invalid loaded_shard_id: {loaded_shard_id}")
+        #         logical_widths = list(output_partition_sizes)
+        #         start_idx = sum(logical_widths[:_loaded_shard_id])
+        #         end_idx = start_idx + logical_widths[_loaded_shard_id]
+        #         weight_quant = self.weight_quant[_loaded_shard_id]
+        #     else:
+        #         start_idx = 0
+        #         end_idx = out_per_partition
+        #         weight_quant = self.weight_quant
+        #     if not isinstance(weight_quant, torch.nn.Identity):
+        #         loaded_weight = weight_quant(loaded_weight.cuda())[0].cpu()
 
-                logical_widths = list(output_partition_sizes)
-                start_idx = sum(logical_widths[:_loaded_shard_id])
-                end_idx = start_idx + logical_widths[_loaded_shard_id]
-                weight_quant = self.weight_quant[_loaded_shard_id]
-            else:
-                start_idx = 0
-                end_idx = out_per_partition
-                weight_quant = self.weight_quant
-            if not isinstance(weight_quant, torch.nn.Identity):
-                loaded_weight = weight_quant(loaded_weight.cuda())[0].cpu()
+        #     if base_loader is not None:
+        #         return base_loader(param[start_idx:end_idx], loaded_weight, *args, **kwargs)
+        #     param[start_idx:end_idx].data.copy_(loaded_weight)
 
-            if base_loader is not None:
-                return base_loader(param[start_idx:end_idx], loaded_weight, *args, **kwargs)
-            param[start_idx:end_idx].data.copy_(loaded_weight)
+        # setattr(layer.weight, "weight_loader", packed_weight_loader)
 
-        setattr(layer.weight, "weight_loader", packed_weight_loader)
+        # # If this layer has bias, allocate it
+        # if getattr(layer, "bias", None) is not None:
+        #     b = torch.empty((out_per_partition,), device="cuda", dtype=params_dtype)
+        #     layer.bias = torch.nn.Parameter(b, requires_grad=False)
+        #     base_bias_loader = extra_weight_attrs.get("bias_loader", None)
 
-        # If this layer has bias, allocate it
-        if getattr(layer, "bias", None) is not None:
-            b = torch.empty((out_per_partition,), device="cuda", dtype=params_dtype)
-            layer.bias = torch.nn.Parameter(b, requires_grad=False)
-            base_bias_loader = extra_weight_attrs.get("bias_loader", None)
+        #     def packed_bias_loader(param, loaded_bias, *args, **kwargs):
+        #         if isinstance(loaded_bias, (list, tuple)):
+        #             loaded_bias = torch.cat(list(loaded_bias), dim=0)
+        #         if base_bias_loader is not None:
+        #             return base_bias_loader(param, loaded_bias, *args, **kwargs)
+        #         param.data.copy_(loaded_bias)
 
-            def packed_bias_loader(param, loaded_bias, *args, **kwargs):
-                if isinstance(loaded_bias, (list, tuple)):
-                    loaded_bias = torch.cat(list(loaded_bias), dim=0)
-                if base_bias_loader is not None:
-                    return base_bias_loader(param, loaded_bias, *args, **kwargs)
-                param.data.copy_(loaded_bias)
+        #     setattr(layer.bias, "bias_loader", packed_bias_loader)
 
-            setattr(layer.bias, "bias_loader", packed_bias_loader)
+        # # Preserve attrs that vLLM weight loaders may attach
+        # for k, v in extra_weight_attrs.items():
+        #     if k in ("weight_loader", "bias_loader"):
+        #         continue
+        #     setattr(layer.weight, k, v)
 
-        # Preserve attrs that vLLM weight loaders may attach
-        for k, v in extra_weight_attrs.items():
-            if k in ("weight_loader", "bias_loader"):
-                continue
-            setattr(layer.weight, k, v)
+    def process_weights_after_loading(
+        model: torch.nn.Module, model_config: ModelConfig, target_device: torch.device
+    ) -> None:
+        breakpoint()
+        pass
 
     def apply(
         self,
