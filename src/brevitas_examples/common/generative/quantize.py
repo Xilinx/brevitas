0064125b86b82baef97686ae2ef17c9262261666
Pablo Monteagudo Lago <pablo.monteagudolago@amd.com>
2026-03-03 11:15:27 +0000

    Fix and test

--- src/brevitas_examples/common/generative/quantize.py
+++ src/brevitas_examples/common/generative/quantize.py
@@ -394,6 +394,7 @@ def generate_quantizers(
         # Enable signed scale if specified in the attention scale precision format
         k_transposed_quant = maybe_inject_signed_scale_kwargs(
             k_transposed_quant, attn_scale_precision, attn_scale_quant_kwargs)
+
         if attn_quant_config == "qkvs" or attn_quant_config == 'qkv':
             q_scaled_quant = k_transposed_quant  # later we define attn_output_weights_quant=q_scaled_quant, so don't instantiate it here
         elif attn_quant_config == "kv":
