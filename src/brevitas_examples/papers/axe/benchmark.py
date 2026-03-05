b6f89e034f1631b84eccb90190f02e1a337aadb9
Ian Colbert <ian.colbert@amd.com>
2026-03-05 22:23:04 +0000

    Feat (axe): adding examples

---
+++ src/brevitas_examples/papers/axe/benchmark.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2026, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
+import sys
+
+from brevitas_examples.common.benchmark.utils import benchmark
+from brevitas_examples.llm.benchmark.llm_benchmark import LLMBenchmarkUtils
+
+
+class AXEBenchmark(LLMBenchmarkUtils):
+
+    @staticmethod
+    def validate(args, extra_args=None):
+        super(LLMBenchmarkUtils, AXEBenchmark).validate(args, extra_args)
+        # TODO: add support for accumulator-aware Qronos
+        assert (int(args.gptq) + int(args.gpfq) + int(args.qronos)) == 1
+
+
+if __name__ == "__main__":
+    benchmark(AXEBenchmark, sys.argv[1:])
