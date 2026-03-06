56ca8d21ce5c376dfbfcc4decc75ae6f9ad694b2
Pablo Monteagudo Lago <pablo.monteagudolago@amd.com>
2026-03-06 17:54:58 +0000

    Add header to learned round benchmark

--- src/brevitas_examples/papers/learned_round/benchmark.py
+++ src/brevitas_examples/papers/learned_round/benchmark.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2026, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
 import sys
 
 from brevitas_examples.common.benchmark.utils import benchmark
