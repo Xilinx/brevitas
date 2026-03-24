e010eb4c35e636ee980eaf8eee10e3e984458a45
Giuseppe Franco <giuseppefranco4@gmail.com>
2026-03-24 14:56:00 +0000

    Feat (quant_tensor): convert from NamedTuple to Tensor subclass

--- src/brevitas/quant_tensor/__init__.py
+++ src/brevitas/quant_tensor/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from .base_quant_tensor import *
 from .base_quant_tensor import _unpack_quant_tensor
+from .base_quant_tensor import QuantTensor
 from .float_quant_tensor import *
 from .groupwise_float_quant_tensor import *
 from .groupwise_int_quant_tensor import *
