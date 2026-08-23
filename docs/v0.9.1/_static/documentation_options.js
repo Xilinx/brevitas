7f1b32d60e61be5ac1c6f29814b225b8d47a319d
Giuseppe Franco <giuseppefranco4@gmail.com>
2026-08-23 20:21:06 +0100

    Update

--- docs/v0.9.1/_static/documentation_options.js
+++ docs/v0.9.1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.12.1.dev61+g7e60590e',
+    VERSION: '0.9.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
