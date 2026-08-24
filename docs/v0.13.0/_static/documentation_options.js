2019cf78da4f36354e446385f773d45880996f55
Giuseppe Franco <giuseppefranco4@gmail.com>
2026-08-24 19:29:49 +0200

    Docs: regenerate docs (#1589)

--- docs/v0.13.0/_static/documentation_options.js
+++ docs/v0.13.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.12.2.dev116+g43fd8f294',
+    VERSION: '0.13.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
