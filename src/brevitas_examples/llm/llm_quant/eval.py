f9d6bf0da542bd60744cc3cc7b08457873257155
Ian Colbert <ian.colbert@amd.com>
2026-09-04 01:41:07 +0000

    Feat: updating header

--- src/brevitas_examples/llm/llm_quant/eval.py
+++ src/brevitas_examples/llm/llm_quant/eval.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2026, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
 """
 Adapted from https://github.com/huggingface/optimum-amd, released under the following LICENSE:
 
