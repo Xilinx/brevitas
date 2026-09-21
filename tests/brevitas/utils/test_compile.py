9376c8dc288ad7cbd92d1d52e225066f112facf7
nickfraser <icanlosh@gmail.com>
2026-09-21 15:25:16 +0200

    Fix (tests): skip groupwise dequant compile test on torch 2.4.1 Windows (#1620)

--- tests/brevitas/utils/test_compile.py
+++ tests/brevitas/utils/test_compile.py
@@ -2,10 +2,13 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import math
+import platform
 
+from packaging import version
 import pytest
 import torch
 
+from brevitas import torch_version
 from brevitas.utils.quant_utils import groupwise_dequant_expand
 from brevitas.utils.torch_utils import padding_to_multiple
 from tests.marker import requires_pt_ge
@@ -41,6 +44,10 @@ def test_compile_padding_to_multiple(shape, dim, multiple, expected_shape):
 @requires_pt_ge('2.2')
 @requires_torch_compile()
 def test_compile_groupwise_dequant_expand(group_dim, scalar_metadata, expand_metadata):
+    if torch_version == version.parse('2.4.1') and platform.system() == 'Windows':
+        pytest.skip(
+            'Skip compile + Windows on torch 2.4.1: dynamo fails to inline its own '
+            'list_cmp polyfill (trace_rules SKIP_DIRS)')
     value = torch.arange(16, dtype=torch.float32).reshape(2, 2, 4)
     if scalar_metadata:
         scale = torch.tensor(2.)
